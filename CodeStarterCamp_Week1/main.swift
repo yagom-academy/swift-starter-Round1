39d956f12f06b42979df1bee35c424e479742529
Quokkaaa <rktrkdml@gmail.com>
2021-12-06 12:23:42 +0900

    docs: 첫 commit 및 -t 의미알아보기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,9 @@
 
 import Foundation
 
-print("Hello, World!")
-
+print("git clone성공")
+"""
+git checkout -t origin/2_Quokkaaa
+여기서 t는 track이라는 의미이며 -track은 set upstream info for new branch 새로운 분기정보를 가져온다.
+git checkout에대한 명령어를 알고싶을떈 git checkout -help를 입력하면 사용할 수 있는 명령어와 뜻을 알수있따.
+"""
