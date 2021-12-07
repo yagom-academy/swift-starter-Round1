b61f480c456c7a7148e85bfa82a33db66186f87b
Jungmo Yu <zzoo789@naver.com>
2021-12-08 01:22:16 +0900

    Step2: 정해진 모양과 길이의 아이스크림 만들기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawIceCream(body: String) {
+    for _ in 1...8 {
+        print(body)
+    }
+}
+
+func drawStick(body: String, length: Int) {
+    let freeSpace: Int = (body.count / 2) - 1
+    for _ in 1...length {
+        for _ in 1...freeSpace {
+            print(" ", terminator: "")
+        }
+        print("|","|")
+    }
+}
+
+var myBody: String = "***********"
+
+
+drawIceCream(body: myBody)
+drawStick(body: myBody, length: 4)
+
 
