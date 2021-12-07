545d6a46d69fa899a7b432737b35fd70a8acc8bd
Mino777 <jomino7777@gmail.com>
2021-12-07 16:40:19 +0900

    feat: add IceCreamMarket & add IceCreamOption, StickOption

---
+++ CodeStarterCamp_Week1/IceCreamMarket.swift
@@ -0,0 +1,20 @@
+//
+//  IceCreamMarket.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 조민호 on 2021/12/07.
+//
+
+import Foundation
+
+struct IceCreamMarket {
+    private enum IceCreamOption {
+        static let size = 8
+        static let shape = "***********"
+    }
+    
+    private enum StickOption {
+        static let size = 4
+        static let shape = "    | |    "
+    }
+}
