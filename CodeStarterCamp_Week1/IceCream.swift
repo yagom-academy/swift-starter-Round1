17a21148a777d10a23e31caa4727d678f8f508f4
Mino777 <jomino7777@gmail.com>
2021-12-07 16:36:28 +0900

    feat: add IceCream Model

---
+++ CodeStarterCamp_Week1/IceCream.swift
@@ -0,0 +1,13 @@
+//
+//  IceCream.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 조민호 on 2021/12/07.
+//
+
+import Foundation
+
+struct IceCream {
+    private(set) var size: Int
+    private(set) var shape: String
+}
