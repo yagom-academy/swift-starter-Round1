a73f95470d3834a7d0aa9aed42e6f10693fd8481
Mino777 <jomino7777@gmail.com>
2021-12-07 16:37:52 +0900

    feat: add IceCreamFactory & add makeIceCream

---
+++ CodeStarterCamp_Week1/IceCreamFactory.swift
@@ -0,0 +1,15 @@
+//
+//  IceCreamFactory.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 조민호 on 2021/12/07.
+//
+
+import Foundation
+
+struct IceCreamFactory {
+    func makeIceCream(size: Int, shape: String) -> IceCream {
+        let iceCream = IceCream(size: size, shape: shape)
+        return iceCream
+    }
+}
