d2d56efc4dccc52d8d5709d61d65bffc01a109a9
malrang <rue>
2021-12-09 01:29:59 +0900

    for->while,세부사항수정.

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,25 +1,41 @@
-func iceCream(){
-    for _ in 1...8{
-        for _ in 1...11{
-            print("*", terminator: "")
-        }
-        print("")
-    }
-}
+//iceCream 을 만들어보자!
 
-func iceCreamBar(){
-    for _ in 1...4{
-        for _ in 1...4{
-            print(" ", terminator: "")
-        }
-        print("| |")
-}
+func drawIceCream(){ // *를 11개 만드는함수
+    var width1 : Int = 1
+    let iceCreamWidth : Int = 11
+    while iceCreamWidth >= width1 {
+        print("*", terminator: "")
+        width1 += 1
+    }
     print("")
 }
 
-iceCream()
-iceCreamBar()
+func drawEmpty(){ //공백4칸을 만드는 함수
+    var empty : Int = 1
+    let emptyMax : Int = 4
+    while emptyMax >= empty {
+        print(" ", terminator: "")
+        empty += 1
+    }
+}
 
+func drawIceCreamBar(){ //막대모양을 출력하는 함수
+    print("| |")
+}
 
-
-
+drawIceCream()
+drawIceCream()
+drawIceCream()
+drawIceCream()
+drawIceCream()
+drawIceCream()
+drawIceCream()
+drawIceCream()
+drawEmpty()
+drawIceCreamBar()
+drawEmpty()
+drawIceCreamBar()
+drawEmpty()
+drawIceCreamBar()
+drawEmpty()
+drawIceCreamBar()
