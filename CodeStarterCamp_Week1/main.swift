eed3d03959b4005543eec6b08a86f2dc0a1ab38e
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-08 20:42:42 +0900

    코드 수정및  플로우 차트 새로작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,13 +10,13 @@ import Foundation
 
  // Step 2 : 아이스크림 그리기
 func icecream() {
-    for cream in 1...8{
+    for icecreamcount in 1...8{
         print("***********")
     }
 }
 
 func stick() {
-    for cone in 1...4{
+    for conecount in 1...4{
         print("    | |    ")
     }
 }
