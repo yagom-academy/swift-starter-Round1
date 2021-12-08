012221cb553c99559deec75ad981a990d537ca43
박세웅 <park@bagse-ung-ui-MacBookAir.local>
2021-12-08 23:31:33 +0900

    docs: Mission1 Step2 Modify FlowChart

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -29,4 +29,3 @@ func printStick() {
         print("    ||")
     }
 }
-
--- CodeStarterCamp_Week1/step3.swift
+++
@@ -1,8 +0,0 @@
-//
-//  step3.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by 박세웅 on 2021/12/08.
-//
-
-import Foundation
