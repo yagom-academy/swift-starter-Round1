9e3187dad61a7b51b761ad1763f068b0099fe90d
hogang1223 <hyogangster1@gmail.com>
2021-12-08 23:41:14 +0900

    막대그리기 함수 수정, 순서도 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,69 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Step2: 아이스크림 그리기
 
+// 일단 보자마자 내가 생각한대로
+print("내맘대로")
+for i in 1...12 {
+    if (i < 9){
+        print("***********")
+    } else {
+        print("    | |")
+    }
+}
+print("")
+
+
+// 과제
+print("Week1 - STEP02 과제1")
+
+// 아이스크림 Body
+let iceCreamWidth = 11
+let iceCreamHeight = 8
+
+// 막대 변수
+let barHeight = 4
+
+printIcecream()
+printBar()
+
+
+
+// 아이스크림 그리기
+func printIcecream(){
+    if (iceCreamWidth < 5 || iceCreamHeight < 1){
+        print("error - printIcecream : width is under 5 or height is under 1")
+        return
+    }
+    
+    for _ in 1...iceCreamHeight{
+        for _ in 1...iceCreamWidth{
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
+
+
+// 막대 그리기
+func printBar(){
+    if (iceCreamWidth < 5 || barHeight < 1){
+        print("error - printBar : width is under 5 or height is under 1")
+        return
+    }
+    
+    for _ in 1...barHeight{
+        for width in 1...iceCreamWidth{
+            
+
+            var halfWidth = iceCreamWidth / 2
+            if (width == halfWidth || width == halfWidth + 2 ){
+                print("|", terminator: "")
+            }else {
+                print(" ", terminator: "")
+            }
+        }
+        print("")
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # 코드 스타터 캠프 1주차 미션을 위한 저장소입니다.
 
+## [WEEK1 - STEP2]
+### 아이스크림 그리기
+- 아이스크림 순서도<br>
+![week1-step2](https://github.com/hogang1223/swift-starter-week1/blob/2_hogang1223/FlowChart/fix2-week1-step2.png?raw=true)
+
+- swift의 print()
+스위프트의 print()는 java의 println()과 같이 기본적으로 줄바꿈을 지원해 준다.<br>
+만약 줄바꿈 없이 print()를 실행하고 싶다면 terminator을 함께 사용
+```swift
+print("*", terminator: "")
+```
+
+
+## [WEEK1 - STEP1]
 ### 1. 캠퍼 본인이 앞으로 컴퓨터를 구매할 때, 가장 중요하게 생각할 컴퓨터의 구성요소는 무엇인가요?
 -> OS. MAC OS!!
 
@@ -8,3 +22,4 @@
 컴퓨터에는 굉장히 많은 스위치(트렌지스터)가 있고 on/off 상태를 통해 0과 1을 표현합니다.<br>
 십진법은 0~9까지의 경우의 수가 있는 것에 비해 이진법은 0 혹은 1 두가지의 경우의 수밖에 없기 때문에<br>
 컴퓨터에게는 이진법이 훨씬 더 효율적이라고 생각합니다.<br>
+
