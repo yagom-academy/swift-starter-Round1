9e68d215e2e18474184f2817917ca1e87ec8ba94
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-09 00:36:44 +0900

    step3 플로우 차트랑 코드 만들어서 올렸습니다

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,21 +8,45 @@
 
 import Foundation
 
- // Step 2 : 아이스크림 그리기
-func drawIcecream() {
-    for _ in 1...8{
-        print("***********")
+ // Step 3 : 주문에 따른 빼뺴로 그리기
+
+func inFormation(){
+    let name: String = " < 정보 > "
+    print(name)
+    
+    let length : Int = 10
+    print("길이:",length)
+    
+    let body: String = "***"
+    print("몸통: ",body)
+    
+    let topping: String = ""
+    print("토핑: ", topping)
+    
+    let stickLength: String = "4"
+    print("막대길이: ",stickLength)
+    
+}
+func drawpepero() {
+    for _ in 1...10{
+        print("   ***   \n")
     }
 }
 
 func drawStick() {
     for _ in 1...4{
-        print("    | |    ")
+        print("   | |    \n")
     }
 }
-
-drawIcecream()
+inFormation()
+drawpepero()
 drawStick()
+//실행  예시2
+
+
+  
+   
+
 
 
 
