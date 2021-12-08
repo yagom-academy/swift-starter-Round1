d434dba4cdf320155587f7ea7788a443c4bac452
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-08 20:21:54 +0900

    코드수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,20 +8,18 @@
 
 import Foundation
 
-
  // Step 2 : 아이스크림 그리기
-
-for cream in 1...8{
-    icecream(cream:cream)
-    func icecream(cream: Int){
+func icecream() {
+    for cream in 1...8{
         print("***********")
     }
 }
-for cone in 1...4{
-    stick(cone: cone)
-    func stick(cone: Int){
+
+func stick() {
+    for cone in 1...4{
         print("    | |    ")
     }
 }
 
-
+icecream()
+stick()
