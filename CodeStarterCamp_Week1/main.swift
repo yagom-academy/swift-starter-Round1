72c0dd67a32762af1f07d875b8b77d49ffa2158e
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-09 09:45:47 +0900

    코드수정및 pull 다시 요청

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,25 +8,34 @@
 
 import Foundation
 
- // Step 3 : 주문에 따른 빼뺴로 그리기
-
-func inFormation(){
+// Step 3 : 주문에 따른 빼뺴로 그리기
+func inFormation() {
     let name: String = " < 정보 > "
     print(name)
-    
+}
+
+func length() {
     let length : Int = 10
     print("길이:",length)
-    
+}
+
+func body() {
     let body: String = "***"
     print("몸통: ",body)
-    
+}
+
+func topping() {
     let topping: String = ""
     print("토핑: ", topping)
-    
-    let stickLength: String = "4"
-    print("막대길이: ",stickLength)
-    
 }
+
+func stickLength() {
+    let stickLength: String = "4"
+    print("막대길이: ",stickLength,"\n")
+}
+
+
+
 func drawpepero() {
     for _ in 1...10{
         print("   ***   \n")
@@ -39,6 +48,10 @@ func drawStick() {
     }
 }
 inFormation()
+length()
+body()
+topping()
+stickLength()
 drawpepero()
 drawStick()
 //실행  예시2
