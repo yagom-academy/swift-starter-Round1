baba1ed0793a06270352f04a0c32e146b5f277f7
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-09 10:31:49 +0900

    코드 요청 사항 추가 및 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,50 +9,46 @@
 import Foundation
 
 // Step 3 : 주문에 따른 빼뺴로 그리기
-func inFormation() {
-    let name: String = " < 정보 > "
-    print(name)
+func mainName(name: String) {
+    print("\(name)")
 }
 
-func length() {
-    let length : Int = 10
-    print("길이:",length)
+func lengthOrder(length: Int) {
+    print(" 길이:", "\(length)")
 }
 
-func body() {
-    let body: String = "***"
-    print("몸통: ",body)
+func bodyOrder(body: String) {
+    print(" 몸통: ","\(body)")
 }
 
-func topping() {
-    let topping: String = ""
-    print("토핑: ", topping)
+func toppingOrder(topping: String) {
+    print(" 토핑: ", "\(topping)")
 }
 
-func stickLength() {
-    let stickLength: String = "4"
-    print("막대길이: ",stickLength,"\n")
+func stickLengthOrder(stickLength: Int) {
+    print(" 막대길이: ","\(stickLength)\n")
 }
 
 
 
 func drawpepero() {
     for _ in 1...10{
-        print("   ***   \n")
+        print("  ***   ")
     }
+    print("\n")
 }
 
 func drawStick() {
     for _ in 1...4{
-        print("   | |    \n")
+        print("  | |    \n")
     }
 }
 
-inFormation()
-length()
-body()
-topping()
-stickLength()
+mainName(name: " < 정보 > ")
+lengthOrder(length: 3)
+bodyOrder(body: "***")
+toppingOrder(topping: "")
+stickLengthOrder(stickLength: 4)
 drawpepero()
 drawStick()
 //실행  예시2
