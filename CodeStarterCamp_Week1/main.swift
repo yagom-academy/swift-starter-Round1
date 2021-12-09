b26c0e3139217416ef0e1b0ee3406a41176650c4
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-09 21:50:56 +0900

    토핑과 몸통 그리기에서 length 함수 없이 출력이 안되서 넣어서 일단  올리겠습니다

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,46 +7,53 @@
 //
 
 import Foundation
-
 // Step 3 : 주문에 따른 빼뺴로 그리기
-func orderName(name: String) {
+
+// 정보 및  주문
+func Order(name: String, length: Int , body: String ,
+           topping: String,stickLength:Int){
     print("\(name)")
+    print("길이:", "\(length)")
+    print("몸통:","\(body)")
+    print("토핑:", "\(topping)")
+    print("막대길이:","\(stickLength)\n")
 }
 
-func lengthOrder(length: Int) {
-    print(" 길이:", "\(length)")
-}
 
-func bodyOrder(body: String) {
-    print(" 몸통: ","\(body)")
-}
-
-func toppingOrder(topping: String) {
-    print(" 토핑: ", "\(topping)")
-}
-
-func stickLengthOrder(stickLength: Int) {
-    print(" 막대길이:","\(stickLength)\n")
-}
-
-func drawPepero(bodyOrder: String , toppingOrder:String, lengthOrder: Int) {
-    for _ in 1...lengthOrder{
-        print("   \(toppingOrder)\(bodyOrder)")
-        print("    \(bodyOrder)\(toppingOrder)")
+//길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
+func drawStick(stickLength:Int){
+    //막대 그리기
+    for _ in 1...stickLength{
     }
 }
 
-func drawStick(stickLengthOrder:Int, stick: String) {
-    for _ in 1...stickLengthOrder{
-        print("    \(stick)")
+//몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+func drawToppingBody(body: String , topping: String, length:Int){
+    //토핑과 몸통 그리기
+    for _ in 1...length{
+        print("  \(topping)\(body)")
+        print("   \(body)\(topping)")
     }
 }
 
-orderName(name: " < 정보 > ")
-lengthOrder(length: 10)
-bodyOrder(body: "***")
-toppingOrder(topping: "")
-stickLengthOrder(stickLength:6)
-drawPepero(bodyOrder:"***", toppingOrder: "#", lengthOrder: 5)
-drawStick(stickLengthOrder: 6,stick:  "| |")
-//실행  예시2
+
+
+//길이(Int)에 따라 몸통을 그리는 함수
+func drawBodyLength(bodyLength:Int){
+    for _ in 1...bodyLength{
+        print("   | |")
+    }
+}
+//각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+func drawPepro(drawstick:Int , drawbody:String,
+               drawtopping:String, Length:Int,drawbodylength:Int) {
+    drawStick(stickLength:drawstick)
+    drawToppingBody(body: drawbody, topping: drawtopping,length: Length)
+    drawBodyLength(bodyLength: drawbodylength)
+    
+}
+Order(name: " < 정보 > ", length: 12, body:"***",
+      topping: "#", stickLength: 6)
+drawPepro(drawstick: 12, drawbody: "***", drawtopping: "#", Length: 12/2, drawbodylength: 6)
+
+
