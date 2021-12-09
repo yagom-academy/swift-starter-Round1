532572b21390145b15bb53ae0cd2ef51fec8d97a
malrang <rue>
2021-12-09 20:07:53 +0900

    규칙을 가지 고있는 함수를 반복하는 함수로 지정,주석제거

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,6 +1,6 @@
 //iceCream 을 만들어보자!
 
-func drawIceCream(){ // *를 11개 만드는함수
+func drawIceCream(){
     var width1 : Int = 1
     let iceCreamWidth : Int = 11
     while iceCreamWidth >= width1 {
@@ -10,7 +10,7 @@ func drawIceCream(){ // *를 11개 만드는함수
     print("")
 }
 
-func drawEmpty(){ //공백4칸을 만드는 함수
+func drawEmpty(){
     var empty : Int = 1
     let emptyMax : Int = 4
     while emptyMax >= empty {
@@ -19,23 +19,29 @@ func drawEmpty(){ //공백4칸을 만드는 함수
     }
 }
 
-func drawIceCreamBar(){ //막대모양을 출력하는 함수
+func drawIceCreamBar(){
     print("| |")
 }
 
-drawIceCream()
-drawIceCream()
-drawIceCream()
-drawIceCream()
-drawIceCream()
-drawIceCream()
-drawIceCream()
-drawIceCream()
-drawEmpty()
-drawIceCreamBar()
-drawEmpty()
-drawIceCreamBar()
-drawEmpty()
-drawIceCreamBar()
-drawEmpty()
-drawIceCreamBar()
+func makeIceCream(){
+    drawIceCream()
+    drawIceCream()
+    drawIceCream()
+    drawIceCream()
+    drawIceCream()
+    drawIceCream()
+    drawIceCream()
+    drawIceCream()
+}
+func makeIceCreamBar(){
+    drawEmpty()
+    drawIceCreamBar()
+    drawEmpty()
+    drawIceCreamBar()
+    drawEmpty()
+    drawIceCreamBar()
+    drawEmpty()
+    drawIceCreamBar()
+}
+makeIceCream()
+makeIceCreamBar()
