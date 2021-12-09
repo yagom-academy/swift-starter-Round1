9e432017f06e15a2ba10a157c3d97d20d05acdaf
brandx-j2su <j2su@brandxcorp.com>
2021-12-09 22:14:01 +0900

    feat: 아이스크림 막대 몸통 아이스크림 전체 함수 생성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,11 +8,25 @@
 
 import Foundation
 
-drawIcecream()
+completeIcecream()
 
+//아이스크림 몸통
 func drawIcecream() {
     for _ in 1...8 {
         print(String(repeating: "*", count: 11))
     }
 }
 
+//아이스크림 막대
+func drawBar() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+//아이스크림 완전체
+func completeIcecream() {
+    drawIcecream()
+    
+    drawBar()
+}
