9ff61f22cf9f214f708a561ee64fb267a2a3fee0
Quokkaaa <rktrkdml@gmail.com>
2021-12-09 17:42:09 +0900

    feat: drawPepero함수 및 메뉴정보담는 변수 구현
    * bodyLengthValue
    * bodyValue
    * toppingValue
    * stickLengthValue
    * bodyInsideValue
    * stickValue
    * toppingPossitionChanger
    
    * drawPepero함수내 if문 구현

--- CodeStarterCamp_Week1/OrderPepero.swift
+++ CodeStarterCamp_Week1/OrderPepero.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+var bodyLengthValue = 6
+var bodyValue = "\(bodyInsideValue)"
+var toppingValue = " "
+var stickLengthValue = 4
+
+var bodyInsideValue = "|0|"
+var stickValue = " | |"
+
+var toppingPositionChanger = 0
+
+// 몸통, 토핑
+func drawPepero(body: String, topping: String) {
+    if toppingPositionChanger % 2 == 0 {
+        print("\(topping)\(body)")
+        toppingPositionChanger += 1
+    } else {
+        print("\(body)\(topping)")
+        toppingPositionChanger += 1
+    }
+}
