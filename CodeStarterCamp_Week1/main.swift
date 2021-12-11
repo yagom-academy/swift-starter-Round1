04610dbea9f15dce61f9740e58f6de0be3a7cec3
박세웅 <park@bagse-ung-ui-MacBookAir.local>
2021-12-12 02:03:53 +0900

    docs: Mission1 Step3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/*
 printIceCream()     // 아이스크림 부분을 그리는 함수
 printStick()        // 막대기(손잡이) 부분을 그리는 함수
 
@@ -29,3 +30,11 @@ func printStick() {
         print("    ||")
     }
 }
+*/
+
+let length: Int = 10
+let body: String = "***"
+let topping: String = "#"
+let stickLength: Int = 4
+    
+let myPepero = MyPepero(length: length, body: body, topping: topping, stickLength: stickLength)
---
+++ CodeStarterCamp_Week1/step3.swift
@@ -0,0 +1,93 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 박세웅 on 2021/12/10.
+//
+import Foundation
+
+class MyPepero {
+    
+    let length: Int, body: String, topping: String, stickLength: Int
+    
+    init(length: Int, body: String, topping: String, stickLength: Int) {
+        self.length = length
+        self.body = body
+        self.topping = topping
+        self.stickLength = stickLength
+        
+        printInfo(self.length, self.body, self.topping, self.stickLength)
+        printMyPepero(length: self.length, body: self.body, topping: self.topping, stickLength: self.stickLength)
+    }
+    
+    /**
+        정보를 출력하는 함수
+     */
+    func printInfo(_ length: Int, _ body: String, _ topping: String, _ stickLength: Int) {
+        print("<정보>")
+        print("길이: \(length)")
+        print("몸통: \(body)")
+        print("토핑: \(topping)")
+        print("막대길이: \(stickLength)\n")
+    }
+
+    /**
+        출력 함수
+     */
+    func printMyPepero(length: Int, body: String, topping: String, stickLength: Int) {
+        var result: String = ""
+        
+        result += makeBody(length, body, topping)
+        result += makeStick(stickLength)
+        
+        print(result)
+    }
+    
+    /**
+        막대를 만드는 함수
+     */
+    func makeStick(_ stickLength: Int) -> String {
+        var result: String = ""
+        for _ in 1...stickLength {
+            result += " | |\n"
+        }
+        return result
+    }
+
+    /**
+        빼빼로 만드는 함수
+     */
+    func makeBody(_ length: Int, _ body: String, _ topping: String) -> String{
+        var result:String = ""
+        if body == "|0|" && topping == "" {     // 누드 빼빼로의 조건: 몸통 "|0|" 토핑 ""
+            for step in 1...length {
+                if step % 2 == 1 {
+                    result += makePiece(topping, "| |", "")
+                } else {
+                    result += makePiece("", body, topping)
+                }
+            }
+        } else {
+            for step in 1...length {
+                if step % 2 == 1 {
+                    result += makePiece(topping, body, "")
+                } else {
+                    result += makePiece("", body, topping)
+                }
+            }
+        }
+        return result
+    }
+
+    /**
+        빼빼로 몸체 한 칸씩 만드는 함수
+     */
+    func makePiece(_ leftTopping: String, _ body: String, _ rightTopping: String) -> String {
+        var result: String = ""
+        if leftTopping == "" {
+            result += " "
+        }
+        result += leftTopping + body + rightTopping + "\n"
+        return result
+    }
+}
