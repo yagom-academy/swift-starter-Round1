edb70a5e881e3d22add043e2db90a4a001250f8f
051kid <94177308+051kid@users.noreply.github.com>
2021-12-12 18:20:12 +0900

    빼빼로 기능구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -22,3 +22,38 @@ for i in 1...8 {
 for i in 1...4 {
     stick()
 }
+func stick(a:Int) {
+    for i in 1...a {
+        print( "   | | ")
+    }
+}
+func makeSome(body:String,length:Int,sticklength:Int) {
+    for b in 1...length {
+        print("   \(body)")
+    }
+    stick(a:sticklength)
+}
+func makeSomeTopping(body:String,topping:String,length:Int) {
+    for my in 1...length/2 {
+        
+    
+    print("  \(topping)\(body)")
+    print("   \(body)\(topping)")
+    }
+    
+}
+
+func makeEnd(body:String,topping:String,length:Int,sticklength:Int) {
+    makeSomeTopping(body: body, topping: topping, length: length)
+    stick(a: sticklength)
+    
+}
+
+makeSome(body: "***", length: 10, sticklength: 4)
+
+makeEnd(body: "***", topping: "&", length: 12, sticklength: 4)
+
+makeEnd(body: "***", topping: "#", length: 12, sticklength: 4)
+
+makeSome(body:"| | \n   |0|", length: 3,sticklength: 4)
+
