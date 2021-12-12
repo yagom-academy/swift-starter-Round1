b97e6eeb74f2091b2585cbe0ffc393af3a3714b8
NAM <nvm@NAMui-MacBookAir.local>
2021-12-12 17:58:16 +0900

    Week1 [Step 2] yunilnam

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+// 반복문을 함수 내에 포함하지 않는 경우
+func iceCream() {
+    print("***********")
+}
 
+func stick() {
+    print("    | |")
+}
+
+// 반복문을 사용할 때 for 앞부분에
+for _ in 1...8 {
+    iceCream()
+}
+
+for _ in 1...4 {
+    stick()
+}
+
+// 반복문을 함수 내에 포함하는 경우
+func iceCream2() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func stick2() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+iceCream2()
+stick2()
