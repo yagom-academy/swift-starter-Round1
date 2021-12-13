4d95b6280034be1d505bb332307f00724687d5e6
vayne77 <sky891@naver.com>
2021-12-13 22:04:59 +0900

    refactor: drawPeperoBody 함수 수정 및 drawPeperoBodyShape 함수 생성

--- CodeStarterCamp_Week1/makePePero.swift
+++ CodeStarterCamp_Week1/makePePero.swift
@@ -8,17 +8,16 @@
 import Foundation
 
 
-let peperoStickImage = "    | |"
+let peperoStickImage = "| |"
 
-
-func drawPeperoBody(peperoLegnth: Int) {
-    for _ in 1...peperoLegnth {
-        print(drawPeperoBodyShape())
-    }
+func drawPeperoBodyShape(peperoBody: String, peperoTopping: String) {
+ 
 }
 
-func drawPeperoBodyShape() {
-    
+func drawPeperoBody(peperoLength: Int, peperoBody: String, peperoTopping: String) {
+    for _ in 1...peperoLength {
+        print(drawPeperoBodyShape(peperoBody: peperoBody, peperoTopping: peperoTopping))
+    }
 }
 
 func drawPeperoStick(stickLength: Int) {
@@ -26,6 +25,7 @@ func drawPeperoStick(stickLength: Int) {
         print(peperoStickImage)
     }
 }
+
 
 
 
