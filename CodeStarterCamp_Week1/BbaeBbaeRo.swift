ccf17861f6a34c04a9f7ff2820673fe4c00b9165
Baek <baekjihoon@Baekui-MacBook-Pro.local>
2021-12-13 21:06:01 +0900

    1주차 미션 Step 2

---
+++ CodeStarterCamp_Week1/BbaeBbaeRo.swift
@@ -0,0 +1,11 @@
+//
+//  bbaebbaero.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Baek on 2021/12/13.
+//
+
+import Foundation
+
+
+print("Hello")
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func printb() -> Void{
+    
+    for ice_t in 1...8{
+        for ice_e in 1...11{
+            print("*",terminator: "")
+        }
+        print()
+    }
+        
+}
+
+func printf() -> Void{
+    for ice_s in 1...4{
+        print("    | |     ")
+    }
+}
+
+//아이스 크림 몸통,막대
+printb()
+printf()
+
 
