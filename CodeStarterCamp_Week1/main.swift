bb2240130b243121c206f18aebc3995b28f1e221
ironReal <menu34@paran.com>
2021-12-13 20:03:49 +0900

    STEP2 코드 작성과 순서도 이미지 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+// STEP2 : 아이스크림 그리기
+func iceCream() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func stick() {
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+}
+
+iceCream()
+stick()
 
