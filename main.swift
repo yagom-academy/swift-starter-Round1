92c0d1398ffa9b4830ffc4c6b4c93e78da751e48
Taeangel <wngks2050@gmail.com>
2021-12-14 13:55:02 +0900

    step3Correction

--- main.swift
+++ main.swift
@@ -1,55 +1,56 @@
 import Foundation
 
 
-
-func makeBar (barLenth:Int){
+func makeBar(barLenth: Int){
     for _ in 1...barLenth{
         print(" | |")
     }
-}//막대부분만들기
+}
 
-func makeChocoleteTopping(chocolete: String, topping: String, chocoleteLenth:Int) -> String {
-    if chocolete == "|0|"{
+
+func setPeperoBody(chocolete: String, topping: String, chocoleteLenth: Int) -> String {
+    if chocolete == "|0|" {
         
-        if chocoleteLenth % 2 == 1 {
-            return " | |"
-        }
-        else
-        {
-            return " "+chocolete
-        }
+     return  NudepeperoBody(chocolete: chocolete, chocoleteLenth: chocoleteLenth)
+    } else {
+ 
+     return PeperoBody(chocolete: chocolete, topping: topping, chocoleteLenth: chocoleteLenth)
+}
+}
+
+
+func NudepeperoBody(chocolete: String, chocoleteLenth: Int) -> String {
+    if chocoleteLenth % 2 == 1 {
+        return " | |"
+    } else {
+        return " "+chocolete
     }
+}
+
+
+func PeperoBody(chocolete: String, topping: String, chocoleteLenth: Int) -> String {
     if chocoleteLenth % 2 == 1 {
         return topping+chocolete
-    }
-    else
-    {
+    } else {
         return " "+chocolete+topping
     }
-}//초콜릿몸통부분만들기
+}
 
 
-func makeBody(chocolete: String, topping: String, chocoleteLenth:Int){
+func makeBody(chocolete: String, topping: String, chocoleteLenth: Int) {
     for chocoleteLenth in 1...chocoleteLenth{
-        
-        
-     let result = makeChocoleteTopping(chocolete: chocolete, topping: topping,        chocoleteLenth:chocoleteLenth)
-        
+     let result = setPeperoBody(chocolete: chocolete, topping: topping, chocoleteLenth: chocoleteLenth)
         print(result)
-        
-        
     }
-}//초콜릿몸통 전체부분만들기
+}
 
-func makePepero(chocolete: String, topping: String, chocoleteLenth:Int ,barLenth:Int){
-    makeBody(chocolete: chocolete, topping: topping, chocoleteLenth:chocoleteLenth)
-    makeBar (barLenth:barLenth)
-}//호출함수 만들기
+
+func makePepero(chocolete: String, topping: String, chocoleteLenth:Int, barLenth:Int) {
+    makeBody(chocolete: chocolete, topping: topping, chocoleteLenth: chocoleteLenth)
+    makeBar(barLenth:barLenth)
+    print("길이: \(chocoleteLenth) 몸통: \(chocolete) 토핑: \(topping) 막대길이: \(barLenth)")
+}
 
 
 makePepero(chocolete: "***", topping: "&", chocoleteLenth: 12, barLenth: 5)
-
-//makeIcecreamBody()
-//makeIcecreamBar()
-
 
