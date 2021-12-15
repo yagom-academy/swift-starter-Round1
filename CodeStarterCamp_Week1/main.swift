be9151fae99e6be1bf14ab5dad8f6db9a4f35371
brandx-j2su <j2su@brandxcorp.com>
2021-12-15 23:59:36 +0900

    feat: 빼빼로 예시 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-let pepero: PeperoRecipe = .init(length: 8, bodyShape: "%%%", topping: "#", stickLength: 4)
+let pepero: PeperoRecipe = .init(length: 8, bodyShape: "***", stickLength: 4)
+let amondPepero: PeperoRecipe = .init(length: 7, bodyShape: "%%%", topping: "#", stickLength: 4)
 let nudepepero: PeperoRecipe = .init(length: 8, bodyShape: "|0|", stickLength: 4)
 
-peperoRecipe(pepero: nudepepero)
+peperoRecipe(pepero: amondPepero)
