e266d0d5aab301ad043f1d0c5c08031ab944016d
ParkHanSoo <mnl124@naver.com>
2021-12-15 18:55:37 +0900

    Feat:Step3추가

---
+++ Step3.swift
@@ -0,0 +1,56 @@
+//
+//  Step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 박한수 on 2021/12/14.
+//
+
+import Foundation
+
+func makeBar (barLength:Int){
+    for _ in 1...barLength{
+        print("| |")
+    }
+}
+
+
+
+func makeBodyType(body: String, topping: String, bodyLength:Int) -> String {
+    if body == "|0|" {
+        
+        if bodyLength % 2 == 1 {
+            return "| |"
+        }
+        else
+        {
+            return ""+body
+        }
+    }
+    if bodyLength % 2 == 1{
+        return topping+body
+    }
+    else
+    {
+        return ""+body+topping
+    }
+}
+
+func makeBody(body: String, topping: String, bodyLength:Int){
+    for bodyLength in 1...bodyLength{
+        
+        let pepero = makeBodyType(body: body, topping: topping, bodyLength:bodyLength)
+        print(pepero)
+        
+    }
+    
+}
+
+func generatorPepero(body: String, topping: String,bodyLength:Int, barLength:Int){
+    makeBody(body: body, topping: topping, bodyLength: bodyLength)
+    makeBar(barLength: barLength)
+}
+
+
+
+
+
--- main.swift
+++ main.swift
@@ -22,3 +22,13 @@ func printIceCreamBar() {
 printIceCreamTop()
 printIceCreamBar()
 
+print("<정보>\n길이: 10\n몸통:***\n토핑: \n막대길이: 4")
+generatorPepero(body: "***", topping: "", bodyLength: 10, barLength: 4)
+print("<정보>\n길이: 12\n몸통: ***\n토핑:&\n막대길이: 4")
+generatorPepero(body: "***", topping: "&", bodyLength: 12, barLength: 4)
+print("<정보>\n길이: 12\n몸통: ***\n토핑:#\n막대길이: 6")
+generatorPepero(body: "***", topping: "#", bodyLength: 12, barLength: 6)
+print("<정보>\n길이: 6\n몸통: |0|\n토핑: \n막대길이: 4")
+generatorPepero(body: "|0|", topping: "", bodyLength: 6, barLength: 4)
+print("나만의 빼빼로(꼬깔콘빼뺴로)\n<정보>\n길이: 6\n몸통: |0|\n토핑: \n막대길이: 4")
+generatorPepero(body: "**", topping: "^", bodyLength: 11, barLength: 4)
