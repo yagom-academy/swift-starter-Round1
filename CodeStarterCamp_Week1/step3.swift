68a4b663ac5fe4c66824ed52430fcfb878c3012b
Minseong-yagom <msb0987@naver.com>
2021-12-15 17:25:23 +0900

    Feat 길이에 따른 막대 그리기 함수 추가
    길이에 따라 막대를 다르게 그리는 기능의 함수이다

---
+++ CodeStarterCamp_Week1/step3.swift
@@ -0,0 +1,17 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 백민성 on 2021/12/15.
+//
+
+import Foundation
+
+class Pepero {
+    //길이에 따른 막대 그리기
+    func drawLengthStick(length: Int) {
+        for _ in 1...length {
+            print(" | |")
+        }
+    }
+}
