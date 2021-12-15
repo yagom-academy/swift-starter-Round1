52c426278d90ae913131249b375c1f1ebbf9f82e
NAM <nvm@NAMui-MacBookAir.local>
2021-12-15 16:54:44 +0900

    docs:Change func name

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,36 +8,40 @@
 
 import Foundation
 
-// 반복문을 함수 내에 포함하지 않는 경우
-func iceCream() {
+
+func printIceCreamBody() {
     print("***********")
 }
 
-func stick() {
+func printIceCreamStick() {
     print("    | |")
 }
 
-// 반복문을 사용할 때 for 앞부분에
-for _ in 1...8 {
-    iceCream()
+
+ for _ in 1...8 {
+    printIceCreamBody()
 }
 
 for _ in 1...4 {
-    stick()
+    printIceCreamStick()
 }
 
-// 반복문을 함수 내에 포함하는 경우
-func iceCream2() {
+
+
+
+
+func printWholeIceCreamBody() {
     for _ in 1...8 {
         print("***********")
     }
 }
 
-func stick2() {
+func printWholeIceCreamStick() {
     for _ in 1...4 {
         print("    | |")
     }
 }
 
-iceCream2()
-stick2()
+printWholeIceCreamBody()
+printWholeIceCreamStick()
+
