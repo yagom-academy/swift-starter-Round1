ea0873a35b037869da319f4cc8f8fdb6d135ef34
051kid <94177308+051kid@users.noreply.github.com>
2021-12-15 16:13:15 +0900

    아이스크림 순서도 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,17 +8,4 @@
 
 import Foundation
 
-func body() {
-    print(" ***********")
-}
 
-func stick() {
-    print("     | |  ")
-}
-
-for i in 1...8 {
-    body()
-}
-for i in 1...4 {
-    stick()
-}
