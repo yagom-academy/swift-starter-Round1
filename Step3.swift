429eae1c815351412c963d4a4f353f104bd0ca66
ParkHanSoo <mnl124@naver.com>
2021-12-16 21:43:03 +0900

    Refactor:Step3수정

--- Step3.swift
+++ Step3.swift
@@ -7,41 +7,40 @@
 
 import Foundation
 
-func makeBar (barLength:Int){
-    for _ in 1...barLength{
-        print("| |")
+func createBar (barLength:Int) {
+    for _ in 1...barLength {
+        print(" | | ")
     }
 }
 
-func makeBodyType(body: String, topping: String, bodyLength:Int) -> String {
+func createBodyType(body: String, topping: String, bodyLength:Int) -> String {
     if body == "|0|" {
         if bodyLength % 2 == 1 {
-            return "| |"
+            return " | | "
         }
-        else
-        {
-            return ""+body
+        else {
+            return " " + body
         }
     }
-    if bodyLength % 2 == 1{
-        return topping+body
+    if bodyLength % 2 == 1 {
+        return topping + body
     }
-    else
-    {
-        return ""+body+topping
+    else {
+        return " " + body + topping
     }
 }
 
-func makeBody(body: String, topping: String, bodyLength:Int){
-    for bodyLength in 1...bodyLength{
-        let pepero = makeBodyType(body: body, topping: topping, bodyLength:bodyLength)
+func createBody(body: String, topping: String, bodyLength:Int) {
+    for bodyLength in 1...bodyLength {
+        let pepero = createBodyType(body: body, topping: topping, bodyLength:bodyLength)
         print(pepero)
     }
 }
 
-func generatorPepero(body: String, topping: String,bodyLength:Int, barLength:Int){
-    makeBody(body: body, topping: topping, bodyLength: bodyLength)
-    makeBar(barLength: barLength)
+func generatorPepero(body: String, topping: String, bodyLength:Int, barLength:Int) {
+    print("<정보> \n길이: \(bodyLength) \n몸통: \(body) \n토핑: \(topping) \n막대길이: \(barLength)")
+    createBody(body: body, topping: topping, bodyLength: bodyLength)
+    createBar(barLength: barLength)
 }
 
 
