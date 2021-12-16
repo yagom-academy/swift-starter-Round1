2f097e2db860a54163c1afafea2d02fe2770ec17
051kid <94177308+051kid@users.noreply.github.com>
2021-12-16 19:54:47 +0900

    Add: 빼빼로 기능구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -22,3 +22,38 @@ for iceCreamBody in 1...8 {
 for iceCreamStick in 1...4 {
     makeIceCreamStick()
 }
+
+func makePeperoStick(a:Int) {
+    for peperoStick in 1...a {
+        print( "   | | ")
+    }
+}
+func makePepero(body:String,length:Int,sticklength:Int) {
+    for peperoBody in 1...length {
+        print("   \(body)")
+    }
+    makePeperoStick(a:sticklength)
+}
+func makeToppingPepero(body:String,topping:String,length:Int) {
+    for toppingPeperoBody in 1...length/2 {
+        
+    
+    print("  \(topping)\(body)")
+    print("   \(body)\(topping)")
+    }
+    
+}
+
+func makeToppingPeperoEnd(body:String,topping:String,length:Int,sticklength:Int) {
+    makeToppingPepero(body: body, topping: topping, length: length)
+    makePeperoStick(a: sticklength)
+    
+}
+
+makePepero(body: "***", length: 10, sticklength: 4)
+
+makeToppingPeperoEnd(body: "***", topping: "&", length: 12, sticklength: 4)
+
+makeToppingPeperoEnd(body: "***", topping: "#", length: 12, sticklength: 4)
+
+makePepero(body:"| | \n   |0|", length: 3,sticklength: 4)
