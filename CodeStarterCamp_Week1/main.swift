9a51d94182c7e9001f82ef856e050d70ef749252
051kid <94177308+051kid@users.noreply.github.com>
2021-12-17 23:45:19 +0900

    Feat: 빼빼로 정보 기능구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,52 +8,59 @@
 
 import Foundation
 
-func makeIceCreamBody() {
-    print(" ***********")
-}
 
-func makeIceCreamStick() {
-    print("     | |  ")
-}
 
-for iceCreamBody in 1...8 {
-    makeIceCreamBody()
-}
-for iceCreamStick in 1...4 {
-    makeIceCreamStick()
-}
 
-func makePeperoStick(a:Int) {
-    for peperoStick in 1...a {
-        print( "   | | ")
+func makePeperoStick(length:Int) {
+    for peperoStick in 1...length {
+        print( " | | ")
+        
     }
 }
 func makePepero(body:String,length:Int,sticklength:Int) {
     for peperoBody in 1...length {
-        print("   \(body)")
+        print(" \(body)")
     }
-    makePeperoStick(a:sticklength)
+    makePeperoStick(length:sticklength)
 }
-func makeToppingPepero(body:String,topping:String,length:Int) {
+
+func makeToppingPeperoStick(length:Int) {
+    for toppingPeperoStick in 1...length{
+            print( "  | |") }
+}
+
+
+func makeToppingPepero(body:String,topping:String,length:Int,sticklength:Int) {
     for toppingPeperoBody in 1...length/2 {
         
     
-    print("  \(topping)\(body)")
-    print("   \(body)\(topping)")
+    print(" \(topping)\(body)")
+    print("  \(body)\(topping)")
     }
+    makeToppingPeperoStick(length: sticklength)
     
 }
-
-func makeToppingPeperoEnd(body:String,topping:String,length:Int,sticklength:Int) {
-    makeToppingPepero(body: body, topping: topping, length: length)
-    makePeperoStick(a: sticklength)
-    
+func makeNudePepero(body:String,body2:String,length:Int,sticklength:Int) {
+    for nudePepero in 1...length {
+        print(" \(body)\n\(body2)")
+    }
+            makePeperoStick(length: sticklength)
 }
 
+func peperoRecipe(body:String,length:Int,topping:String,sticklength:Int) {
+    print("""
+          <정보>
+          길이: \(length)
+          몸통: \(body)
+          토핑: \(topping)
+          막대길이: \(sticklength)
+          """)
+}
+peperoRecipe(body: "***", length: 10, topping: "", sticklength: 4)
 makePepero(body: "***", length: 10, sticklength: 4)
-
-makeToppingPeperoEnd(body: "***", topping: "&", length: 12, sticklength: 4)
-
-makeToppingPeperoEnd(body: "***", topping: "#", length: 12, sticklength: 4)
-
-makePepero(body:"| | \n   |0|", length: 3,sticklength: 4)
+peperoRecipe(body: "***", length: 12, topping: "&", sticklength: 4)
+makeToppingPepero(body: "***", topping: "&", length: 12, sticklength: 4)
+peperoRecipe(body: "***", length: 12, topping: "#", sticklength: 4)
+makeToppingPepero(body: "***", topping: "#", length: 12, sticklength: 4)
+peperoRecipe(body: "|0|", length: 6, topping: "", sticklength: 4)
+makeNudePepero(body: "| |",body2:" |0|", length: 3,sticklength: 4)
