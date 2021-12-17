642dd23b70b21dc3a0ccb03e696f7dc96427bc8d
vayne77 <sky891@naver.com>
2021-12-17 13:11:13 +0900

    refactor: 아이스크림을 그리는 함수 main에서 분리해 새 파일 생성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,26 +6,7 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
-let iceCreamImage = "***********"
-let stickImage = "    | |"
-var iceCreamLength = 8
-var stickLength = 4
-
-func drawIceCreamImage() {
-    for _ in 1...iceCreamLength {
-        print("\(iceCreamImage)")
-    }
-}
-
-func drawStickImage() {
-    for _ in 1...stickLength {
-        print("\(stickImage)")
-    }
-}
-
-//drawIceCreamImage()
 //drawStickImage()
+//drawIceCreamImage()
 
 printPeperoInfo(peperoLength: 6, peperoBody: "|0|", peperoTopping: "", stickLength: 4)
