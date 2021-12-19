43275f509b079e692a6c7d48917b77d99db71bc9
Seokjune Hong <seokjunehong@hongseogjun-ui-MacBookPro.local>
2021-12-19 11:00:07 +0900

    refactor: Step 3 코드 수정

--- CodeStarterCamp_Week1/DrawingPepero.swift
+++ CodeStarterCamp_Week1/DrawingPepero.swift
@@ -15,29 +15,41 @@ var peperoTopping: String = ""
 
 func drawPeperoBar(bar: Int) {
     for _ in 1...bar {
-        print("\(peperoBarShape)")
+        print(peperoBarShape)
     }
-    print("\n") //order end
 }
 
-func addToppingOnPeperoBody(body : String, topping : String){
-    if(topping == ""){
+func addToppingOnPeperoBody(body : String, topping : String) {
+    if body == " |0|" {
+        leftSideTopping = body
+        rightSideTopping = body
+    } else if topping == "" {
         leftSideTopping = " " + body
         rightSideTopping = " " + body
-    } else if(topping != ""){
+    } else {
         leftSideTopping = topping + body
         rightSideTopping = " " + body + topping
     }
 }
 
-func drawPeperoBody(length : Int){
-    for count in 1...length{
+func drawPeperoBody(length : Int) {
+    for count in 1...length {
         count % 2 == 1 ? print(leftSideTopping) : print(rightSideTopping)
     }
 }
 
-func orderPepero(length : Int, body : String, topping : String, bar : Int){
+func printPeperoInformation (length : Int, body : String, topping : String, bar : Int) {
+    print("<정보>\n")
+    print("길이: \(length)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이: \(bar)\n")
+}
+
+func orderPepero(length : Int, body : String, topping : String, bar : Int) {
+    printPeperoInformation(length: length, body: body, topping: topping, bar: bar)
     addToppingOnPeperoBody(body: body, topping: topping)
     drawPeperoBody(length: length)
     drawPeperoBar(bar: bar)
+    print("\n")
 }
