2ce1e76f340cd0ebec48d043c0dd93b2c2851e8c
Woo Jong <dnwhd0112@naver.com>
2021-12-19 21:33:21 +0900

    refactor : 일부 항목 수정 함수 매개변수 수정 및 불필요한 코드 삭제

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -11,19 +11,15 @@ import Foundation
 
 
 // stetp 3 코드
-makePaParow(body: "***", topping: " ", size: 12, barSize: 6)
-print("1번째 뺴빼로")
-makePaParow(body: "***", topping: "&", size: 12, barSize: 4)
-print("2번째 뺴빼로")
-makePaParow(body: "***", topping: "#", size: 12, barSize: 6)
-print("3번째 뺴빼로")
-makePaParow(body: "|0|", topping: " ", size: 12, barSize: 6)
-print("4번째 뺴빼로")
-makePaParow(body: "*_*", topping: "0", size: 12, barSize: 6)
-print("5번째 뺴빼로")
+makePaParow(body: "***", topping: " ", bodySize: 12, stickSize: 6)
 
-var isTrue = true
-print("\(isTrue)")
+makePaParow(body: "***", topping: "&", bodySize: 12, stickSize: 4)
+
+makePaParow(body: "***", topping: "#", bodySize: 12, stickSize: 6)
+
+makePaParow(body: "|0|", topping: " ", bodySize: 12, stickSize: 6)
+
+
 
 
 
--- CodeStarterCamp_Week1/makePaparow.swift
+++ CodeStarterCamp_Week1/makePaparow.swift
@@ -7,32 +7,38 @@
 
 import Foundation
 
-func makeTwoChocoBody(body: String,topping: String) -> Void{
-    //print("\(topping)\(body)")
-    //print(" \(body)\(topping)")
+func makeTwoChocoBody(body: String,topping: String) {
+    
     print("""
                \(topping)\(body)
                 \(body)\(topping)
                """)
 }
 
-func printChocoBody(body: String, topping: String, size: Int) {
-    let halfChcoSize = size / 2
+func printChocoBody(body: String, topping: String, bodySize: Int) {
+    let halfChocoSize = bodySize / 2
     
-    for _ in 1...halfChcoSize{
+    for _ in 1...halfChocoSize{
         makeTwoChocoBody(body: body, topping: topping)
     }
  }
 
-func printBar (barSize: Int){
-    for _ in 1...barSize{
+func printBar (stickSize: Int){
+    for _ in 1...stickSize{
         print(" | | ")
     }
 }
     
-func makePaParow (body: String, topping: String, size: Int, barSize: Int){
-    printChocoBody(body: body, topping: topping, size:size)
-    printBar(barSize: barSize)
+func makePaParow (body: String, topping: String, bodySize: Int, stickSize: Int){
+    print("""
+        길이: \(bodySize)
+        몸통: \(body)
+        토핑: \(topping)
+        막대길이: \(stickSize)
+        
+        """)
+    printChocoBody(body: body, topping: topping, bodySize: bodySize)
+    printBar(stickSize: stickSize)
 }
 
 
