8a8a774c3e5907d2ef1e153c7b16571bc183e2ac
malrang <rue>
2021-12-19 21:36:33 +0900

    Feat: 잘못된 정보를 입력시 번환값 추가
    기존 빼빼로 만들기 몸통을 만드는 함수에서 잘못된 바디를 선택하게되면 "잘못된 정보를 입력하였습니다" 를 출력 하게끔 수정됨

--- CodeStarterCamp_Week1/repeatStep3.swift
+++ CodeStarterCamp_Week1/repeatStep3.swift
@@ -21,28 +21,34 @@ func makePrinrtInfo(barLength : Int = 4, bodyLength : Int = 10, body : String = 
     print("빼뺴로 토핑 : \(topping)")
     print("############")
     
-    //빼빼로 막대 만드는 함수
     func drawpeperoBar(barLength: Int){
-        for _ in 1...barLength{
-            print(" | |")
+        if makeBody(body: body, topping: topping) == "잘못된 정보" {
+        } else {
+            for _ in 1...barLength{
+                print(" | |") }
         }
     }
     
-    //빼빼로 몸통 만드는 함수
     func makeBody(body: String, topping: String) -> String {
-        switch body {
-        case "|0|" :
-            return " \(body)\(topping)\n, \(topping)| |\n"
-        default :
+        if body == "|0|" {
+            return " \(body)\(topping)\n\(topping)| |"
+        } else if body == "***" {
             return " \(body)\(topping)\n\(topping)\(body)"
+        } else {
+            return "잘못된 정보"
         }
+        
     }
     
     func drawBody(body: String, topping: String, bodyLength: Int) {
         let peperoBody: String = makeBody(body: body, topping: topping)
         let bodyArray: Array<String> = peperoBody.components(separatedBy: "\n")
-        for count in 1...bodyLength {
-            print(bodyArray[count % 2])
+        if makeBody(body: body, topping: topping) == "잘못된 정보" {
+            print("정보를 잘못 입력 하였습니다")
+        } else {
+            for count in 1...bodyLength {
+                print(bodyArray[count % 2])
+            }
         }
     }
     
