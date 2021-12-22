d60978280a012adb17a1457052cdb3e253ae575f
Tak <77046882+Tak95@users.noreply.github.com>
2021-12-22 11:26:27 +0900

    Revert "Week1 [step 3] malrang"

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,43 +1,12 @@
-//iceCream 을 만들어보자!
-func drawIceCream(){
-    var width1 : Int = 1
-    let iceCreamWidth : Int = 11
-    while iceCreamWidth >= width1 {
-        print("*", terminator: "")
-        width1 += 1
-    }
-    print("")
-}
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
 
-func drawEmpty(){
-    var empty : Int = 1
-    let emptyMax : Int = 4
-    while emptyMax >= empty {
-        print(" ", terminator: "")
-        empty += 1
-    }
-}
+import Foundation
 
-func drawIceCreamBar(){
-    print("| |")
-}
-
-func makeIceCream(){
-    for _ in 1...8 {
-        drawIceCream()
-
-    }
-}
-
-func makeIceCreamBar(){
-    for _ in 1...4 {
-        drawEmpty()
-        drawIceCreamBar()
-    }
-}
-
-//빼빼로 정보 를 입력할때 주의할점
-//몸통부분은 "***", "|0|" 만 선택가능합니다
-makePrinrtInfo(barLength: 4, bodyLength: 4, body: "|0|", topping: "&")
-
+print("Hello, World!")
 
