d300774eebb9d7f5d0e1d7e7c7cccee0bcb6a0ab
yeeton37 <lyeeun37@naver.com>
2022-02-10 01:20:20 +0900

    step2 함수명 변경

---
+++ icecream/makingicecream.swift
@@ -0,0 +1,13 @@
+func makingIcecream() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+makingIcecream()
+
+func makingIcecreamStick() {
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+}
+makingIcecreamStick()
