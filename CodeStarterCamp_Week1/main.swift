8ef482c3bca8b77902b268e237a36e159e4de283
themoon <leeyejik99@gmail.com>
2022-02-09 22:24:30 +0900

    [step 2] code update

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,8 +5,19 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+// 별 8줄 막대 4줄
 
 import Foundation
 
-print("Hello, World!")
-
+func star() -> Void{ // Void 생략가능
+    for _ in 1...8{
+        print("***********")
+    }
+}
+func bar() -> Void{
+    for _ in 1...4{
+        print("    | |")
+    }
+}
+star()
+bar()
