a93e6e1c688040d4da251e1baad85cd26465bc99
Makseong2 <wshstjddl@gmail.com>
2022-02-10 13:45:48 +0900

    code for icecream

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+func icecreamBody(numberOfRows : Int) {
+    for _ in 1...numberOfRows {
+        print("***********")
+    }
+}
+
+func icecreamStick(numberOfRows : Int) {
+    
+    for i in 1...numberOfRows { //와일드 패턴을 썻지만, 순서도에 표현 쉽게 하기 위해  i, j 부여 하였습니다.
+        
+        for j in 1...4 {    //공백(space) 갯수
+            print(" ",terminator: "")
+        }
+        
+        print("|",terminator: "")
+        print(" ",terminator: "")
+        print("|")
+        
+    }
+}
+
+icecreamBody(numberOfRows: 8)
+icecreamStick(numberOfRows: 4)
 
