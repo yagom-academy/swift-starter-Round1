6cc9a2a18eac7cc6c8a56c62cd787fa5bdb6353c
JeeeeSangRyul <wltkdfbf@gmail.com>
2022-02-11 00:22:47 +0900

    Step2과제입니다

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,6 +7,50 @@
 //
 
 import Foundation
-
 print("Hello, World!")
 
+
+//첫번째 방법
+let totalIcecream = 8
+var layerIcecream = 0
+
+func makeLayerIcecream (){
+    for i in 1...11{
+        print("*" , terminator: "")
+    }
+    print("")
+}
+
+func makeLayerStick(){
+    for i in 1...4{
+        print("    |", terminator: " ")
+        print("|    ")
+    }
+}
+
+while layerIcecream < totalIcecream{
+    makeLayerIcecream()
+    layerIcecream += 1
+}
+
+makeLayerStick()
+
+// 두번째 방법
+//func makeLayerIcecream(){
+//for count in 1...8{
+//for i in 1...11{
+//    print("*" , terminator: "")
+//}
+//    print("")
+//}
+//}
+//func makeLayerStick(){
+//    for i in 1...4{
+//            print("    |", terminator: " ")
+//            print("|    ")
+//        }
+//        print("")
+//}
+//
+//makeLayerIcecream()
+//makeLayerStick()
