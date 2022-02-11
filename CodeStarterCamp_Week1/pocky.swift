851f4cf8cdb4e2ad10857c86c10fb097338c5de2
cpp2swift <dkseotn123@gmail.com>
2022-02-11 18:24:06 +0900

    pocky.swift file added

---
+++ CodeStarterCamp_Week1/pocky.swift
@@ -0,0 +1,8 @@
+//
+//  pocky.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Kay Ahn on 2022/02/11.
+//
+
+import Swift
---
+++ pocky/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  pocky
+//
+//  Created by 안대수 on 2022/02/11.
+//
+
+import Foundation
+
+print("Hello, World!")
+
