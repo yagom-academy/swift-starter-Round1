948ddb7b22d8cc9a06adc9e38f60a23c2e4ce496
소영 <sso_0022@naver.com>
2022-02-11 15:17:45 +0900

    [WEEK1 - STEP3] feat : step 3 빼빼로 그리기 함수 구현

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+func drawPepero(bodyLength: Int, body: String, topping: String, barLength: Int) {
+  let ingredient = makeIngredient(body: body, topping: topping)
+  drawPeperoBody(bodyLength: bodyLength, ingredient: ingredient)
+  drawPeperoBar(barLength: barLength)
+}
+
+func makeIngredient(body: String, topping: String) -> String {
+  return topping == "" ? " \(body)" : "\(topping)\(body)\(topping)"
+}
+
+func drawPeperoBody(bodyLength: Int, ingredient: String) {
+  for _ in 0..<bodyLength {
+    print(ingredient)
+  }
+}
+
+func drawPeperoBar(barLength: Int) {
+  for _ in 0..<barLength {
+    print(" | |")
+  }
+}
