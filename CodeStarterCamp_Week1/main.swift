f011606155cb9f85c50b4fc1841cc0bc28f5e2d0
TaeSeong <ts4683@gmail.com>
2022-02-11 22:42:59 +0900

    feat : makeBodyAndTopping func 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -12,3 +12,24 @@ func makeStick(stickLength: Int) {
     }
 }
 
+func makeBodyAndTopping(bodyLength: Int , body: String , topping: String ) {
+    
+    if body == " |0|" {                 //누드빼빼로일 경우
+        if bodyLength % 2 == 1 {        // 누드빼빼로이고 bodyLength가 홀수일때
+            print(" | |")
+            for _ in 1...bodyLength/2 {
+                print("\(topping)\(body)\(topping)")
+                print(" | |")
+            }
+        } else {                         //누드빼빼로이고 bodyLength가 짝수일때
+            for _ in 1...bodyLength/2 {
+                print(" | |")
+                print("\(topping)\(body)\(topping)")
+            }
+        }
+    } else {                             //누드빼빼로가 아닐경우
+        for _ in 1...bodyLength {
+            print("\(topping)\(body)\(topping)")
+        }
+    }
+}
