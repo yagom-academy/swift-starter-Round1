dcccc76bc7b56d83b1047c65195ad2bb4c0c5d50
소영 <sso_0022@naver.com>
2022-02-11 19:55:30 +0900

    [WEEK1 - STEP3] feat : 실행 예시의 정보 출력 코드 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,19 +9,17 @@
 import Foundation
 
 // MARK: - STEP 2 아이스크림 그리기
-drawIceCreamTop()
-drawIceCreamBar()
+//drawIceCreamTop()
+//drawIceCreamBar()
 
 // MARK: - STEP 3 뺴뺴로 그리기
 // 예시 1
-print("\n1️⃣번 빼빼로\n")
 drawPepero(bodyLength: 10, body: "***", topping: "", barLength: 4)
 // 예시 2
-print("\n2️⃣번 빼빼로\n")
 drawPepero(bodyLength: 12, body: "***", topping: "&", barLength: 4)
 // 예시 3
-print("\n3️⃣번 빼빼로\n")
 drawPepero(bodyLength: 12, body: "***", topping: "#", barLength: 6)
 // 예시 4
-print("\n4️⃣번 빼빼로\n")
 drawPepero(bodyLength: 6, body: "|0|", topping: "", barLength: 4)
+// 나만의 빼빼로
+drawPepero(bodyLength: 8, body: "***", topping: "@", barLength: 5)
--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 func drawPepero(bodyLength: Int, body: String, topping: String, barLength: Int) {
+  print("\n<정보>")
+  print("길이: \(bodyLength)")
+  print("몸통: \(body)")
+  print("토핑: \(topping)")
+  print("막대길이: \(barLength)\n")
+  
   if body == "|0|" {
     drawNudePepero(bodyLength: bodyLength, barLength: barLength)
   } else {
@@ -17,24 +23,25 @@ func drawPepero(bodyLength: Int, body: String, topping: String, barLength: Int) 
   }
 }
 
-func makeIngredient(body: String, topping: String) -> String {
+private func makeIngredient(body: String, topping: String) -> String {
   return topping == "" ? " \(body)" : "\(topping)\(body)\(topping)"
 }
 
-func drawPeperoBody(bodyLength: Int, ingredient: String) {
+private func drawPeperoBody(bodyLength: Int, ingredient: String) {
   for _ in 0..<bodyLength {
     print(ingredient)
   }
 }
 
-func drawPeperoBar(barLength: Int) {
+private func drawPeperoBar(barLength: Int) {
   for _ in 0..<barLength {
     print(" | |")
   }
 }
 
-func drawNudePepero(bodyLength: Int, barLength: Int) {
+private func drawNudePepero(bodyLength: Int, barLength: Int) {
   let length: Int = bodyLength/2
   drawPeperoBody(bodyLength: length, ingredient: " | |\n |0|")
   drawPeperoBar(barLength: barLength)
 }
+
