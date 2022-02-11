b7d3c73b503239ce63fa40d93ea457971e305db3
소영 <sso_0022@naver.com>
2022-02-11 15:03:38 +0900

    [WEEK1 - STEP3] refactor : main과 step2 파일 분리

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,17 +10,3 @@ import Foundation
 
 drawIceCreamTop()
 drawIceCreamBar()
-
-// 1. 아이스크림 부분을 그리는 함수
-func drawIceCreamTop() {
-  for _ in 0..<8 {
-    print("***********\n")
-  }
-}
-
-// 2. 막대기(손잡이) 부분을 그리는 함수
-func drawIceCreamBar() {
-  for _ in 0..<4 {
-    print("    | |\n")
-  }
-}
