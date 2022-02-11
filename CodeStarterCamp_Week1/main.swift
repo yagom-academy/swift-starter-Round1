88adaa774a56879b809712a29dc669d15957ba7f
SEO-YJ <tjdbwns19@naver.com>
2022-02-11 19:44:00 +0900

    finish1-week1-step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,9 +6,27 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-print("Hello, World!")
 
-print("git push test - another folder")
+func printIceCream(width: Int, height: Int) -> Void {
+    for _ in 1...height {
+        for _ in 1...width {
+            print("*", terminator: "")
+        }
+        print()
+    }
+}
+
+func printStick(width: Int, height: Int) -> Void {
+    let space = width / 2 - 1
+    for _ in 1...height{
+        for _ in 1...space{
+            print(" ", terminator: "")
+        }
+        print("| |")
+    }
+}
+
+printIceCream(width: 11, height: 8)
+printStick(width: 11, height: 4)
 
