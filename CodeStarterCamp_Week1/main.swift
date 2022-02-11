997c92a18ed71861b6ca39ad49e16ee9ad99fde2
Dino <dinojun12@naver.com>
2022-02-11 21:49:48 +0900

    Week1 [STEP 2] Dino 피드백 답안 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/* 첫 번째 답안
 func makeIceCream() {
     for rowOfIceCream in 1...8 {
         print("***********")
@@ -19,4 +20,28 @@ func makeStick() {
     }
 }
 makeIceCream()
+makeStick()
+*/
+
+// 피드백 후의 답안
+var blank = ""
+func makeElevenStars() {
+            for count in 1...11 {
+                if count > 10 {
+                    blank = "\n"
+                } else if count == 1 {
+                    blank = ""
+                }
+                print("*", terminator: blank)
+            }
+        }
+func makeStick() {
+    for count in 1...4 {
+        print("    | |", terminator: blank)
+    }
+}
+
+for rowOfIceCream in 1...8 {
+   makeElevenStars()
+}
 makeStick()
