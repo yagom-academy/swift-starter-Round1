036a78fc12c017febdaeb31fa741ad8f43dc720d
Ares Jinhwan Kim <aresjinhwankim@Ares-MacBook-Pro-2020.local>
2022-02-11 12:38:11 +0900

    1st

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func icemain(iceheight: Int) { // Icecream
+    for count in 1...iceheight {
+        print("***********")
+    }
 
+}
+
+func icebar(icebarheight: Int) { // Icebar
+    for count in 1...icebarheight {
+        print("    | |     ")
+    }
+}
+
+icemain(iceheight: 8) // Icecream 길이 조정
+icebar(icebarheight: 4) // Icebar 길이 조정
