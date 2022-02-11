710f574d54d0831419026a3ad41cfa2ba6e5889d
Hangga99 <qkrrlxo418@naver.com>
2022-02-11 13:37:39 +0900

    Week1 Setp2 커밋

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,20 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+iceTop()
+iceBar()
+
+// 1. 아이스크림을 그리는 함수
+func iceTop() {
+    for _ in 1...8 {
+       print("***********")
+     }
+}
+
+// 2. 아이스크림 막대기를 그리는 함수
+func iceBar() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
 
