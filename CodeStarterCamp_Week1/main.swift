3f4957b0103a790812b6bf1cbcaea1b72ec3c62a
your_name <sondaehyeon@sondaehyeon-ui-MacBookAir.local>
2022-02-12 22:15:52 +0900

    Week1 [STEP3]

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,20 +8,47 @@
 
 import Foundation
 
-func iceCreamBar() {
-    for count in 1 ... 11 {
-        print("***********")
+
+ 
+
+var peperoBody: String = "***"
+var peperoToping: String = "&"
+
+func makePeperoOneTwo(oneTwoBodyLength: Int, oneTwoStickLength: Int) {
+    let peperoOneBody = " ***"
+    for count in 1 ... oneTwoBodyLength {
+        if oneTwoBodyLength  == 10  {
+            print(peperoOneBody)
+        } else {
+            print("\(peperoToping)\(peperoBody)\(peperoToping)")
+        }
     }
-    
+    for count in 1 ... oneTwoStickLength {
+        print(" | |")
+    }
+}
+func makePeperoThreeFour(threeFourBodyLength: Int, threeFourStickLength: Int) {
+    peperoToping = "#"
+    let nudePeperoBody = """
+                       | |
+                       |0|
+                      """
+    for count in 1 ... threeFourBodyLength {
+        if threeFourBodyLength == 12 {
+            print("\(peperoToping)\(peperoBody)\(peperoToping)")
+        } else  {
+            print(nudePeperoBody)
+        }
+    }
+    for count in 1 ... threeFourStickLength {
+        print(" | |")
+    }
 }
 
-func iceCreamStick() {
-    for count in 1 ... 4 {
-        print("    | |      ")
-    }
-}
+makePeperoOneTwo(oneTwoBodyLength: 10, oneTwoStickLength: 4)
+makePeperoOneTwo(oneTwoBodyLength: 12, oneTwoStickLength: 4)
+makePeperoThreeFour(threeFourBodyLength: 12, threeFourStickLength: 6)
+makePeperoThreeFour(threeFourBodyLength: 3, threeFourStickLength: 4)
 
 
-iceCreamBar()
-iceCreamStick()
 
