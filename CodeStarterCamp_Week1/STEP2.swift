e593fd8d587d7a2499cb91b7fae3e6c7ea6565c4
lia388 <lia388@naver.com>
2022-02-14 00:39:38 +0900

    [Week1-step3] : main과 STEP2 파일 분리

---
+++ CodeStarterCamp_Week1/STEP2.swift
@@ -0,0 +1,20 @@
+//
+//  Week1_STEP2.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Yelin Hong on 2022/02/13.
+//
+
+import Foundation
+
+func drawIceCream(){
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func drawBar(){
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+}
