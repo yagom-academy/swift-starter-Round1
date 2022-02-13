6e6538fc8357311d02bba4ac97b4919b32025d8e
Makseong2 <wshstjddl@gmail.com>
2022-02-13 21:32:26 +0900

    [Week1-step3] : step3 함수호출

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,3 +10,13 @@
 //drawIceCreamBody(numberOfBodyRows: 8)
 //drawIceCreamStick(numberOfStickRows: 4)
 
+// MARK: - STEP 3
+
+//예시1
+customedPepero(numberOfBodyRows: 10, bodyShape: "***", toppingShape: "", numberOfStickRows: 4)
+//예시2
+customedPepero(numberOfBodyRows: 12, bodyShape: "***", toppingShape: "&", numberOfStickRows: 4)
+//예시3
+customedPepero(numberOfBodyRows: 12, bodyShape: "***", toppingShape: "#", numberOfStickRows: 6)
+//예시4
+customedPepero(numberOfBodyRows: 6, bodyShape: "|0|", toppingShape: "", numberOfStickRows: 4)
