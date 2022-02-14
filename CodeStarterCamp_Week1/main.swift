b56bb67958c49b8062bfdfa286a4304de3715e7f
hi2102 <hi2102@naver.com>
2022-02-14 22:53:39 +0900

    main.swift Step 2: 아이스크림 그리기 commit

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+// Step 2: 아이스크림 그리기
+
+// 아이스크림 몸통
+func iceCreamBody() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+// 아이스크림 바
+func iceCreamBar() {
+    for _ in 1...4 {
+        print(" "," ","|"," |")
+    }
+}
+
+// 아이스크림 완성
+func iceCream() {
+    iceCreamBody()
+    iceCreamBar()
+}
+
+iceCream()
 
