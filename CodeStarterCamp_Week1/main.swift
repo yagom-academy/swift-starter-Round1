af738cc575eb666b687f1019fd9eeba53385b124
your_name <sondaehyeon@sondaehyeon-ui-MacBookAir.local>
2022-02-15 00:00:27 +0900

    Week1[STEP3]

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,6 +10,18 @@ import Foundation
 
 var peperoBody: String = "***"
 var peperoToping: String = "&"
+
+func inputPeperoInformation(length: String, body: String, topping: String, secondLength: String) {
+    print("""
+    
+    <정보>
+    길이: \(length)
+    몸통: \(body)
+    토핑: \(topping)
+    길이2: \(secondLength)
+    
+    """)
+}
 func makePeperoOneTwo(oneTwoBodyLength: Int, oneTwoStickLength: Int) {
     let peperoOneBody = " ***"
     for _ in 1 ... oneTwoBodyLength {
@@ -40,9 +52,12 @@ func makePeperoThreeFour(threeFourBodyLength: Int, threeFourStickLength: Int) {
         print(" | |")
     }
 }
+inputPeperoInformation(length: "10", body: "", topping: "", secondLength: "4")
 makePeperoOneTwo(oneTwoBodyLength: 10, oneTwoStickLength: 4)
+inputPeperoInformation(length: "12", body: "***", topping: "&", secondLength: "4")
 makePeperoOneTwo(oneTwoBodyLength: 12, oneTwoStickLength: 4)
+inputPeperoInformation(length: "12", body: "***", topping: "#", secondLength: "6")
 makePeperoThreeFour(threeFourBodyLength: 12, threeFourStickLength: 6)
+inputPeperoInformation(length: "6", body: "|0|", topping: "", secondLength: "4")
 makePeperoThreeFour(threeFourBodyLength: 3, threeFourStickLength: 4)
-
 
