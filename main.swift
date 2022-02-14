174bfac273aade4119e63cf82ce39b72a3f72d4d
JeeeeSangRyul <wltkdfbf@gmail.com>
2022-02-14 18:00:08 +0900

    step3 commit

---
+++ main.swift
@@ -0,0 +1,55 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 지상률 on 2022/02/14.
+//
+
+import Foundation
+
+//길이에 따른 막대를 그리는 함수
+func layerStick(stick : Int){
+    for _ in 1...stick{
+    print(" | | ")
+    }
+}
+
+
+//몸통(string) , 토핑(string)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+func layerBody(body: String , topping: String) -> String{
+    if body == "|0|"{
+        return (" | | \n |0| ")
+    }else{
+    return ("\(topping)\(body)\(topping)")
+    }
+}
+
+
+//길이에 따라 몸통을 그리는 함수
+func totalBody(total: Int,body: String , topping: String ){
+    if body == "|0|"{
+        for _ in 1...total/2{
+        print(layerBody(body: body, topping: topping))
+        }
+    }else{
+    for _ in 1...total{
+    print(layerBody(body: body, topping: topping))
+    }
+    }
+}
+
+
+func makeBBeBBeRo(total: Int,body: String , topping: String, stick : Int){
+    totalBody(total: total, body: body, topping: topping)
+    layerStick(stick: stick)
+}
+
+makeBBeBBeRo(total: 10, body: "***", topping: " ", stick: 4)
+print("")
+makeBBeBBeRo(total: 12, body: "***", topping: "&", stick: 4)
+print("")
+makeBBeBBeRo(total: 12, body: "***", topping: "#", stick: 6)
+print("")
+makeBBeBBeRo(total: 6, body: "|0|", topping: " ", stick: 4)
+print("")
+makeBBeBBeRo(total: 8, body: "|$|", topping: "^", stick: 4)
