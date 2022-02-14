3c11283d5a4a8a9ab0ea461413ac75ac60daf434
yeeton37 <lyeeun37@naver.com>
2022-02-15 00:27:07 +0900

    step2 순서도와 코드 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,13 +1,4 @@
-func makingIcecreamBody() {
-    for _ in 1...8 {
-        print("***********")
-    }
-}
 makingIcecreamBody()
-
-func makingIcecreamStick() {
-    for _ in 1...4 {
-        print("    | |    ")
-    }
-}
 makingIcecreamStick()
+
+
---
+++ CodeStarterCamp_Week1/step2 icecream.swift
@@ -0,0 +1,11 @@
+func makingIcecreamBody() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func makingIcecreamStick() {
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+}
