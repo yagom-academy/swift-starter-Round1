00306ca64f04b0702fa358b188920441a346bcd1
uni <miju@Uui-MacBookAir.local>
2022-02-15 18:45:47 +0900

    drawPepero

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,20 +9,47 @@
 import Foundation
 
 
-func printIceCream(iceCreamLength: Int, iceCreamWidth: Int) {
-    for i in 1...iceCreamLength {
-        for j in 1...iceCreamWidth {
-            print("*", terminator: "")
+func drawStick(stickLength: Int) {
+    for _ in 1...stickLength{
+        print(" | |")
+    }
+}
+
+func drawPeperoBody(peperoBody: String, topping: String )-> String  {
+    if topping.isEmpty {
+        return " "+peperoBody
+    }
+    else{
+    return topping+peperoBody+topping
+    }
+}
+
+func drawPeperoLength(peperoLength: Int, peperoBody: String, topping: String) {
+    for count in 1...peperoLength {
+        if peperoBody == "|0|" {
+            if count % 2 == 0 {
+                print(" |0|")
+            }
+            else {
+                print(" | |")
+            }
         }
-        print("")
+        else {
+            print(drawPeperoBody(peperoBody: peperoBody, topping: topping))
+        }
     }
 }
 
-func printStick(stickLength: Int) {
-    for i in 1...stickLength {
-        print("    | |")
-    }
+func drawPeperoParameter(peperoLength: Int, peperoBody: String, topping: String, stickLength: Int) {
+    print("<정보>")
+    print("길이: \(peperoLength)" )
+    print("몸통: \(peperoBody)")
+    print("토핑: \(topping)")
+    print("막대길이: \(stickLength)")
+
+    drawPeperoLength(peperoLength: peperoLength, peperoBody: peperoBody, topping: topping)
+    drawStick(stickLength: stickLength)
 }
 
-printIceCream(iceCreamLength: 8, iceCreamWidth: 11)
-printStick(stickLength: 4)
+
+ drawPeperoParameter(peperoLength: 12, peperoBody: "***", topping: "&", stickLength: 4)
