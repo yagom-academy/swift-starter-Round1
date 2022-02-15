0efbe38ed403f87c25c9ef9c09073ac363284ac3
Dino <dinojun12@naver.com>
2022-02-15 16:02:08 +0900

    Week1 [STEP 3] Dino

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,9 +7,33 @@
 
 import Foundation
 
-func makeStick(lengthOfStick: Int) {
-    for count in 1...lengthOfStick {
-        print(" | |")
+func chooseBodyAndToppingOfIceCream(body: String, topping: String) {
+    print(" \(topping)\(body)\(topping)")
+}
+
+func makeIceCreamPart(lengthOfIceCream: Int, body: String, topping: String) {
+    for count in 1...lengthOfIceCream {
+        chooseBodyAndToppingOfIceCream(body: body, topping: topping)
     }
 }
-makeStick(lengthOfStick: 4)
+
+func makeStickPart(lengthOfStick: Int) {
+    for count in 1...lengthOfStick {
+        print("  | |")
+    }
+}
+
+func makeWholeIceCream(lengthOfIceCream: Int, body: String, topping: String, lengthOfStick: Int) {
+    print("<정보>\n 길이: \(lengthOfIceCream)\n 몸통: \(body)\n 토핑: \(topping)\n 막대길이: \(lengthOfStick)\n")
+    makeIceCreamPart(lengthOfIceCream: lengthOfIceCream, body: body, topping: topping)
+    makeStickPart(lengthOfStick: lengthOfStick)
+}
+
+//실행 예시 1
+makeWholeIceCream(lengthOfIceCream: 10, body: " ***", topping: "", lengthOfStick: 4)
+//실행 예시 2
+makeWholeIceCream(lengthOfIceCream: 12, body: "***", topping: "&", lengthOfStick: 4)
+//실행 예시 3
+makeWholeIceCream(lengthOfIceCream: 12, body: "***", topping: "#", lengthOfStick: 6)
+//실행 예시 4
+makeWholeIceCream(lengthOfIceCream: 6, body: " |0|", topping: "", lengthOfStick: 4)
