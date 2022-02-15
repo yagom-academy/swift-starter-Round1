a029de980992ce2a7460d12d1fb7dc07c06ed846
mskaang <mskaang@gmail.com>
2022-02-15 18:56:01 +0900

    [UPDATE] Week 1 Step 3 Flowchart 작성 완료, 함수 일부 작성 완료 (drawBar, decideBody 함수)

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,12 @@
 
 import Foundation
 
-drawIcecreamPart() //step2_drawIcecream.swift에서 함수 정의
-drawBarPart() //step2_drawIcecream.swift에서 함수 정의
+/* [Step2 과제 코드]
+ drawIcecreamPart() //step2_drawIcecream.swift에서 함수 정의
+ drawBarPart() //step2_drawIcecream.swift에서 함수 정의
+ */
+
+// [Step3 과제 코드] 일부 테스트
+print(decideBody(bodyShape: "***", toppingShape: "&"))
+print(decideBody(bodyShape: " |0|", toppingShape: ""))
+drawBar(barLength: 4)
