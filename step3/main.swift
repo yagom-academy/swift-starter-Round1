6296e79c86963889a43ae86564206eba12001e25
sozohoy <sozohoy@gmail.com>
2022-02-15 17:29:32 +0900

    [IMPROVE] step3 기능 수정 및 리네이밍, 순서도 수정

--- step3/main.swift
+++ step3/main.swift
@@ -7,47 +7,70 @@
 
 import Foundation
 
-let iceLength = Int(readLine()!)!
-let iceShape = readLine()!
-let toppingChoice = readLine() ?? " "
-let barSize = Int(readLine()!)!
+print("길이를 입력하세요 !")
+let iceLengthTypeStr = readLine() ?? "0"
+print("몸통을 입력하세요 !")
+var iceShape: String = readLine() ?? ""
+print("토핑을 입력하세요 !")
+var toppingChoice: String = readLine() ?? ""
+print("막대길이를 입력하세요 !")
+let barSizeTypeStr = readLine() ?? "0"
+
+let iceLength: Int = Int(iceLengthTypeStr) ?? 0
+let barSize: Int = Int(barSizeTypeStr) ?? 0
+
 var toppingCount = 0 // 토핑의 갯수를 세줌. 비어있어도 2
-checkTopping()
+var iceCount = 0
+
+countTopping()
+countIceSize()
 
 let barShape = ["| |", "||", "|"]
-let checkIceSize = iceShape.count + toppingCount
+let checkIceSize = iceCount + toppingCount
+
+let printInfo = """
+<정보>
+길이 : \(iceLength)\n몸통 : \(iceShape)\n토핑 : \(toppingChoice)\n막대길이 : \(barSize)
+"""
+print(printInfo)
+createShape(countLength: iceLength)
+createBar(countLength: barSize)// (checkIceSize - 3) / 2 = 앞에 필요한 여백
 
 
-print("<정보>")
-print("길이 : \(iceLength)\n몸통 : \(iceShape)\n토핑 : \(toppingChoice)\n막대길이 : \(barSize)")
-createShape(x: iceLength)
-createBar(y: barSize)// (checkIceSize - 3) / 2 = 앞에 필요한 여백
-
-
-func createShape(x: Int){
+func createShape(countLength: Int){
     if toppingChoice == "" {
-        for _ in 0...x-1{
+        for _ in 0...countLength-1{
             print(" \(iceShape) ")
         }
     } else {
-        for _ in 0...x-1{ // iceLength
-        print("\(toppingChoice)\(iceShape)\(toppingChoice)")
+        if iceShape == "|0|"{
+            for i in 0...countLength-1{
+                if i % 2 == 0{
+                    print("\(toppingChoice)| |\(toppingChoice)")
+                } else {
+                    print("\(toppingChoice)\(iceShape)\(toppingChoice)")
+                }
+            }
+        } else {
+            for _ in 0...countLength-1{ // iceLength
+            print("\(toppingChoice)\(iceShape)\(toppingChoice)")
+            }
         }
     }
 }
 
-func createBar(y: Int){
+func createBar(countLength: Int){
     if checkIceSize % 2 == 1{ // 아이스크림 너비가 홀수
         if checkIceSize - 3 > 0{ // 몸통 크기가 3보다 클 때
             let barSpace = (checkIceSize - 3) / 2 // 앞의 여백 count
-            for _ in 0...y-1{
+            for _ in 0...countLength-1{
                 for _ in 0...barSpace-1{
                 print(" ", terminator: "")
                 }
                 print(barShape[0])
             }
         } else { // 몸통 크기가 3 이하일 때.
-            for _ in 0...y-1{ // barSize
+            for _ in 0...countLength-1{ // barSize
                 for _ in 0...(toppingCount / 2) - 1{
                     print(" ", terminator: "")
                 }
@@ -56,7 +79,7 @@ func createBar(y: Int){
         }
     } else { // ice size가 짝수일 때
         let barSpace = (checkIceSize - 2) / 2
-        for _ in 0...y-1{
+        for _ in 0...countLength-1{
             for _ in 0...barSpace-1{
                 print(" ", terminator: "")
             }
@@ -65,10 +88,21 @@ func createBar(y: Int){
     }
 }
 
-func checkTopping(){
+func countIceSize() {
+    if iceShape == "" {
+        iceCount = 1
+        iceShape = " "
+    } else {
+        iceCount = iceShape.count
+    }
+}
+
+func countTopping() {
     if toppingChoice == "" {
+        toppingChoice = " "
         toppingCount = 2
     } else {
         toppingCount = toppingChoice.count * 2
     }
 }
+
