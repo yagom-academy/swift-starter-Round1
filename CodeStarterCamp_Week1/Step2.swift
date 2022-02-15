76dad80ba44f34141ee12b791c056271f092e48a
Ares Jinhwan Kim <aresjinhwankim@Ares-MacBook-Pro-2020.local>
2022-02-15 22:54:40 +0900

    wk1_step3

---
+++ CodeStarterCamp_Week1/Step2.swift
@@ -0,0 +1,22 @@
+//
+//  Step2.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Ares Jinhwan Kim on 2022/02/15.
+//
+
+import Foundation
+
+
+func icemain(iceheight: Int) { // Icecream
+    for _ in 1...iceheight {
+        print("***********")
+    }
+
+}
+
+func icebar(icebarheight: Int) { // Icebar
+    for _ in 1...icebarheight {
+        print("    | |     ")
+    }
+}
