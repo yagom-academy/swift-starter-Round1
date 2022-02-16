4dde970c015ee667dd16f737a8efd4f60e201477
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-02-17 01:19:01 +0900

    Refactor : 함수 네이밍 수정

--- CodeStarterCamp_Week1/Step3/main.swift
+++ CodeStarterCamp_Week1/Step3/main.swift
@@ -8,34 +8,34 @@
 
 import Foundation
 
-func drawStick(stickLength: Int, stick: String) {
+func makePeperoStick(stickLength: Int, stick: String) {
     for _ in 1...stickLength {
             print(" ", " ", separator: "\(stick)")
     }
 }
 
-func drawBody(body: String, topping: String) {
+func makeBodyPiece(body: String, topping: String) {
     print(topping + body + topping)
 }
 
-func drawFullBody(body: String, topping: String, bodyLength: Int) {
+func makePeperoBody(body: String, topping: String, bodyLength: Int) {
     if body.hasPrefix("|") {
         let halfLength: Int = bodyLength / 2
         for _ in 1...halfLength {
-            drawBody(body: "| |", topping: topping)
-            drawBody(body: body, topping: topping)
+            makeBodyPiece(body: "| |", topping: topping)
+            makeBodyPiece(body: body, topping: topping)
         }
         if bodyLength % 2 == 1 {
-            drawBody(body: "| |", topping: topping)
+            makeBodyPiece(body: "| |", topping: topping)
         }
     } else {
         for _ in 1...bodyLength {
-            drawBody(body: body, topping: topping)
+            makeBodyPiece(body: body, topping: topping)
         }
     }
 }
 
-func drawPepero(bodyLength: Int, body: String, orderedTopping: String, stickLength: Int) {
+func makeOrderedPepero(bodyLength: Int, body: String, orderedTopping: String, stickLength: Int) {
     var topping: String = orderedTopping
     if topping.isEmpty {
        topping = " "
@@ -46,9 +46,9 @@ func drawPepero(bodyLength: Int, body: String, orderedTopping: String, stickLeng
     토핑: \(orderedTopping)
     막대길이: \(stickLength)\n
     """)
-    drawFullBody(body: body, topping: topping, bodyLength: bodyLength)
-    drawStick(stickLength: stickLength, stick: "| |")
+    makePeperoBody(body: body, topping: topping, bodyLength: bodyLength)
+    makePeperoStick(stickLength: stickLength, stick: "| |")
 }
 
-drawPepero(bodyLength: 12, body: "000", orderedTopping: "$", stickLength: 4)
-drawPepero(bodyLength: 13, body: "|0|", orderedTopping: "", stickLength: 4)
+makeOrderedPepero(bodyLength: 12, body: "000", orderedTopping: "$", stickLength: 4)
+makeOrderedPepero(bodyLength: 13, body: "|0|", orderedTopping: "", stickLength: 4)
