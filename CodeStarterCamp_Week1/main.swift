520b3569b802060601db3136998d1ed581e91eef
Juho Jung <jooho293@gmail.com>
2022-02-16 17:07:39 +0100

    Refactor: change code for step3 1. -print 구문 함수로 빼내어 출력 -오타수정 -컨벤션 수정 -step2 주석처리
    -

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-func makeBodyOfIcecream(icecreamBodyWidth: Int, icecreamBodyLonger: Int) {
+/*func makeBodyOfIcecream(icecreamBodyWidth: Int, icecreamBodyLonger: Int) {
     let bodyOfIcecream = "*"
     for _ in 0...icecreamBodyLonger {
         for _ in 0...icecreamBodyWidth {
@@ -27,60 +27,65 @@ func makeTailOfIcecream(icecreamTailLonger: Int) {
 }
 
 makeBodyOfIcecream(icecreamBodyWidth: 10, icecreamBodyLonger: 7)
-makeTailOfIcecream(icecreamTailLonger: 3)
+makeTailOfIcecream(icecreamTailLonger: 3)*/
 
 
 
-let tailOfPpero = " | | "
+let tailOfPpepero = " | | "
 
-func makeStickOfPpero(stickHeigth: Int) {
+func makeStickOfPpepero(stickHeigth: Int) {
     for _ in 1...stickHeigth {
-        print(tailOfPpero)
+        print(tailOfPpepero)
     }
 }
 
-func makeBodyOfPpero(bodyForm: String, topping: String) {
+func makeBodyOfPpepero(bodyForm: String, topping: String) {
     print(topping + bodyForm + topping)
 }
 
-func makeHeightOfPpero(bodyHeigth: Int, bodyForm: String, topping: String) {
+func makeHeightOfPpepero(bodyHeigth: Int, bodyForm: String, topping: String) {
     if bodyForm == "***" {
         for _ in 1...bodyHeigth {
-            makeBodyOfPpero(bodyForm: bodyForm, topping: topping)
+            makeBodyOfPpepero(bodyForm: bodyForm, topping: topping)
         }
     } else {
         for count in 1...bodyHeigth {
             if count%2 != 0 {
                 print(" |0|")
-            }else {
-                print(tailOfPpero)
+            } else {
+                print(tailOfPpepero)
             }
         }
     }
 }
-func orderPpero(stickHeigth: Int, bodyHeigth: Int, bodyForm: String, topping: String) {
-    print("""
-길이:\(bodyHeigth)
-몸통:\(bodyForm)
-토핑:\(topping)
-막대길이:\(stickHeigth)
-""")
-    makeHeightOfPpero(bodyHeigth: bodyHeigth, bodyForm: bodyForm, topping: topping)
-    makeStickOfPpero(stickHeigth: stickHeigth)
+
+
+func orderPpepero(stickHeigth: Int, bodyHeigth: Int, bodyForm: String, topping: String) {
+    func formationOrder() {
+        print("""
+    길이:\(bodyHeigth)
+    몸통:\(bodyForm)
+    토핑:\(topping)
+    막대길이:\(stickHeigth)
+    """)
+    }
+    formationOrder()
+    makeHeightOfPpepero(bodyHeigth: bodyHeigth, bodyForm: bodyForm, topping: topping)
+    makeStickOfPpepero(stickHeigth: stickHeigth)
     
 }
 
 //first Ppero order
-orderPpero(stickHeigth: 4, bodyHeigth: 10, bodyForm: "***", topping: " ")
+orderPpepero(stickHeigth: 4, bodyHeigth: 10, bodyForm: "***", topping: " ")
 
 //second Ppero order
-orderPpero(stickHeigth: 4, bodyHeigth: 12, bodyForm: "***", topping: "&")
+orderPpepero(stickHeigth: 4, bodyHeigth: 12, bodyForm: "***", topping: "&")
 
 //third Ppero order
-orderPpero(stickHeigth: 6, bodyHeigth: 12, bodyForm: "***", topping: "#")
+orderPpepero(stickHeigth: 6, bodyHeigth: 12, bodyForm: "***", topping: "#")
 
 //forth Ppero order
-orderPpero(stickHeigth: 4, bodyHeigth: 6, bodyForm: "| |", topping: " ")
+orderPpepero(stickHeigth: 4, bodyHeigth: 6, bodyForm: "| |", topping: " ")
 
 //my own Ppero order
-orderPpero(stickHeigth: 4, bodyHeigth: 6, bodyForm: "***", topping: "!")
+orderPpepero(stickHeigth: 4, bodyHeigth: 6, bodyForm: "***", topping: "!")
