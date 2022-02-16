8c6cd66e05c2e76b11fb77b94a1da7f024026d95
Dino <dinojun12@naver.com>
2022-02-16 16:09:36 +0900

    Week1 [STEP 2] Dino 불필요한 주석 삭제

--- CodeStarterCamp_Week1/Week1_Step2.swift
+++ CodeStarterCamp_Week1/Week1_Step2.swift
@@ -8,22 +8,6 @@
 
 import Foundation
 
-/* 첫 번째 답안
-func makeIceCream() {
-    for rowOfIceCream in 1...8 {
-        print("***********")
-    }
-}
-func makeStick() {
-    for rowOfStick in 1...4 {
-        print("    | |")
-    }
-}
-makeIceCream()
-makeStick()
-*/
-
-// 피드백 후의 답안
 var blankOrReturn = ""
 func makeStarsOfOneRow(numberOfStars: Int) {
     for count in 1...numberOfStars {
