a72dc7a757ca0fa872ce8f98ecb9d815fecb8d77
hi2102 <hi2102@naver.com>
2022-02-16 19:04:59 +0900

    실행예시3번 추가 커밋

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,4 +1,4 @@
-//
+
 //  main.swift
 //  CodeStarterCamp_Week1
 //
@@ -8,37 +8,8 @@
 
 import Foundation
 
-// Step 2: 아이스크림 그리기
-
-// 아이스크림 몸통
-func iceCreamBody() {
-    for _ in 1...8 {
-        print("***********")
-    }
-}
-
-// 아이스크림 바
-func iceCreamBar() {
-    for _ in 1...4 {
-        print(" "," ","|"," |")
-    }
-}
-
-// 아이스크림 완성
-func iceCream() {
-    iceCreamBody()
-    iceCreamBar()
-}
-
-iceCream()
-
-
-// Step 3: 주문에 따른 빼뺴로 그리기
+₩// Step 3: 주문에 따른 빼뺴로 그리기 - 수정된 코드
 // 조건에 따른 기본 코드들
-
-// 위의 실행 예시와 같은 빼빼로를 그리기 위해 
-// 다음과 같은 함수를 나누어 만들어봅시다.
-
 // 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
 
 func bar(length: String) -> String {
@@ -46,7 +17,7 @@ func bar(length: String) -> String {
     return stick
 }
 
-let stick = bar(length: " | |")
+let stick = bar(length: " " + " " + "| |")
 
 func drawBar(length: Int) {
     for _ in 1...length {
@@ -71,13 +42,13 @@ func makeBody(outside: String, inside: String) -> String {
     let toppings = topping(shape: outside)
     let bodies = body(astesisk: inside)
     
-    let pepero = toppings + bodies + toppings
+    let pepero = " " + toppings + bodies + toppings
     return pepero
 }
 
 // 길이(Int)에 따라 몸통을 그리는 함수
 
-let bodySet = makeBody(outside: " ", inside: "***")
+let bodySet = makeBody(outside: " ", inside: "|0|")
 
 func drawBody(column: Int) {
     for _ in 1...column {
@@ -96,11 +67,6 @@ func draw(length: Int, stick: Int) {
 
 }
 
-// 실행예시 1
-draw(length: 10, stick: 4)
-
-// 실행예시 2
 draw(length: 12, stick: 4)
-// 길이(Int)에 따라 몸통을 그리는 함수 부분 코드에 토핑 추가
-// line 80: let bodySet = makeBody(outside: "&", inside: "***")
+
 
