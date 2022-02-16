2dd446db94ec2a1a3a767bd100bec9b8e4ed4f04
Dino <dinojun12@naver.com>
2022-02-16 19:40:34 +0900

    Week1 [STEP 3] Dino makeIceCreamPart(lengthOfIceCream:)->makeIceCream(length:) makeStickPart(lengthOfStick:)->makeStickPart(length:)

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -11,22 +11,22 @@ func chooseOptionsOfIceCream(body: String, topping: String) {
     print(" \(topping)\(body)\(topping)")
 }
 
-func makeIceCreamPart(lengthOfIceCream: Int, body: String, topping: String) {
-    for rowOfIceCream in 1...lengthOfIceCream {
+func makeIceCreamPart(length: Int, body: String, topping: String) {
+    for rowOfIceCream in 1...length {
         chooseOptionsOfIceCream(body: body, topping: topping)
     }
 }
 
-func makeStickPart(lengthOfStick: Int) {
-    for rowOfStick in 1...lengthOfStick {
+func makeStickPart(length: Int) {
+    for rowOfStick in 1...length {
         print("  | |")
     }
 }
 
 func makeWholeIceCream(lengthOfIceCream: Int, body: String, topping: String, lengthOfStick: Int) {
     print("<정보>\n 길이: \(lengthOfIceCream)\n 몸통: \(body)\n 토핑: \(topping)\n 막대길이: \(lengthOfStick)\n")
-    makeIceCreamPart(lengthOfIceCream: lengthOfIceCream, body: body, topping: topping)
-    makeStickPart(lengthOfStick: lengthOfStick)
+    makeIceCreamPart(length: lengthOfIceCream, body: body, topping: topping)
+    makeStickPart(length: lengthOfStick)
 }
 
 //실행 예시 1
