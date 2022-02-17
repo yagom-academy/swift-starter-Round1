a540d3f38ad018120e5d1da3dc9a79e78333fb0e
jun <jun@junui-MacBook-Pro.local>
2022-02-17 19:16:02 +0900

    refactor : 함수와 반복문의 시작{ }의 공간 통일 및 재배치

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-func icecream(){
+func icecream() {
     for count1 in 1...8 {
         print("***********")
     }
 }
 
-func stick(){
+func stick() {
     for count2 in 1...4 {
         print("    ", terminator: "")
         print("| |")
