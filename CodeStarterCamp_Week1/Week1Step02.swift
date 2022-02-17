8e812bf6448770d188c816d854f6909842459a91
iwonman <tjdnjswls3@naver.com>
2022-02-18 03:48:19 +0900

    Step03 Code commit

---
+++ CodeStarterCamp_Week1/Week1Step02.swift
@@ -0,0 +1,20 @@
+//
+//  WEEK1 Step 02
+
+import Foundation
+
+func iceCreamDraw(){
+    for _ in 1...11{
+        print("***********")
+    }
+}
+
+func stickDraw(){
+    for _ in 1...4{
+        print("    ",terminator: "")
+        print("| |")
+    }
+}
+
+iceCreamDraw()
+stickDraw()
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,20 +8,48 @@
 
 import Foundation
 
-func drawIceCream(){
-    for _ in 1...11{
-        print("***********")
-    }
-}
-
-func drawStick(){
-    for _ in 1...4{
-        print("    ",terminator: "")
+func drawUnder(length: Int){
+    for _ in 1...length{
+        print(" ",terminator: "")
         print("| |")
     }
+
+}
+func drawBodyChange(height: Int , body: String, topping: String){
+    if body == "|0|" && topping==""{
+        for _ in 1...height/2{
+            print(" | |")
+            print("",topping+body+topping)
+        }
+    }
+    else{
+        for _ in 1...height{
+            print(topping+body+topping,terminator: "")
+            print()
+        }
+    }
+}
+func drawBody(height: Int,body: String){
+    for _ in 1...height{
+        print(" ",terminator: "")
+        print("\(body)")
+    }
 }
 
-iceCreamDraw()
-stickDraw()
+func orderBBR(height: Int ,body: String, topping:String,length: Int){
 
-//
+    print("<정보>","\n길이: ", height,"\n몸통: \(body) \n토핑: \(topping)","\n막대길이: ",length)
+
+    
+    if topping != "" || body == "|0|" {
+        drawBodyChange(height: height,body: body,topping: topping)
+    }
+    else{
+        drawBody(height: height,body: body)
+    }
+    drawUnder(length: length)
+}
+
+
+orderBBR(height: 6, body: "|0|", topping: "", length: 4)
+
