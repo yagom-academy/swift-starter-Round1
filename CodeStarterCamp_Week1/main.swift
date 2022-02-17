7ccd709fe3214b53ca6dd503c0ab500f139b97f7
your_name <sondaehyeon@sondaehyeon-ui-MacBookAir.local>
2022-02-18 01:11:30 +0900

    Week1 [STEP3] 다시 해봤습니다

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,58 +6,44 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
-var peperoBody: String = "***"
-var peperoToping: String = "&"
-
-func inputPeperoInformation(length: String, body: String, topping: String, secondLength: String) {
-    print("""
-    
-    <정보>
-    길이: \(length)
-    몸통: \(body)
-    토핑: \(topping)
-    길이2: \(secondLength)
-    
-    """)
-}
-func makePeperoOneTwo(oneTwoBodyLength: Int, oneTwoStickLength: Int) {
-    let peperoOneBody = " ***"
-    for _ in 1 ... oneTwoBodyLength {
-        if oneTwoBodyLength  == 10  {
-            print(peperoOneBody)
-        } else {
-            print("\(peperoToping)\(peperoBody)\(peperoToping)")
-        }
-    }
-    for _ in 1 ... oneTwoStickLength {
+// 길이 따른 막대기 함수
+func makePeperoStick(stickLength: Int) {
+    for _ in 1 ... stickLength {
         print(" | |")
     }
 }
-func makePeperoThreeFour(threeFourBodyLength: Int, threeFourStickLength: Int) {
-    peperoToping = "#"
-    let nudePeperoBody = """
-                       | |
-                       |0|
-                      """
-    for _ in 1 ... threeFourBodyLength {
-        if threeFourBodyLength == 12 {
-            print("\(peperoToping)\(peperoBody)\(peperoToping)")
-        } else  {
-            print(nudePeperoBody)
+func makePeperoBody(body: String, topping: String, length: Int) {
+    for i in 1 ... length {
+        if body == " ***" {
+            print("\(body)")
+        } else if topping.isEmpty == false {
+            print("\(topping)\(body)\(topping)")
+        } else if (i % 2 == 1) {
+            print(" | | ")
+        } else if (i % 2 == 0) {
+            print(" |0|")
         }
     }
-    for _ in 1 ... threeFourStickLength {
-        print(" | |")
-    }
 }
-inputPeperoInformation(length: "10", body: "", topping: "", secondLength: "4")
-makePeperoOneTwo(oneTwoBodyLength: 10, oneTwoStickLength: 4)
-inputPeperoInformation(length: "12", body: "***", topping: "&", secondLength: "4")
-makePeperoOneTwo(oneTwoBodyLength: 12, oneTwoStickLength: 4)
-inputPeperoInformation(length: "12", body: "***", topping: "#", secondLength: "6")
-makePeperoThreeFour(threeFourBodyLength: 12, threeFourStickLength: 6)
-inputPeperoInformation(length: "6", body: "|0|", topping: "", secondLength: "4")
-makePeperoThreeFour(threeFourBodyLength: 3, threeFourStickLength: 4)
+func makeMyPepero(bodyLength: Int, bodyshape: String, toppingShape: String, myStickLength: Int) {
+   
+    let peperoInformation = """
+        
+        <정보>
+        길이: \(bodyLength)
+        몸통: \(bodyshape)
+        토핑: \(toppingShape)
+        길이2: \(myStickLength)
+
+        """
+    print(peperoInformation)
+    makePeperoBody(body: bodyshape, topping: toppingShape, length: bodyLength)
+    makePeperoStick(stickLength: myStickLength)
+}
+makeMyPepero(bodyLength: 10, bodyshape: " ***", toppingShape: "", myStickLength: 4)
+makeMyPepero(bodyLength: 12, bodyshape: "***", toppingShape: "&", myStickLength: 4)
+makeMyPepero(bodyLength: 12, bodyshape: "***", toppingShape: "#", myStickLength: 6)
+makeMyPepero(bodyLength: 6, bodyshape: " |0|", toppingShape: "", myStickLength: 4)
+// 나만의 잭팟 뺴빼로
+makeMyPepero(bodyLength: 9, bodyshape: "777", toppingShape: "$", myStickLength: 5)
 
