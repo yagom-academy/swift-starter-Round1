aef13ec7066a50d5acf5f5129627dad31525bcea
jun <jun@junui-MacBook-Pro.local>
2022-02-18 23:57:35 +0900

    Update : drawStick함수 업데이트

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 func drawStick(stickLength: Int) {
-    print(" | |")
+    for _ in 1...stickLength {
+        print(" | |")
+    }
 }
 
 func drawTopping(body: String, topping: String) {
@@ -16,9 +18,12 @@ func drawTopping(body: String, topping: String) {
 }
 
 func drawBody(bodyLength: Int) {
-    
+
 }
 
 func drawPepero(stickLength: Int, body: String, topping: String, bodyLength: Int) {
-    
+    print("<정보>", "길이: \(bodyLength)", "몸통: \(body)", "토핑: \(topping)", "막대길이: \(stickLength)", " ")
+    drawStick(stickLength: stickLength)
 }
+
+drawPepero(stickLength: 4, body: "***", topping: " ", bodyLength: 10)
