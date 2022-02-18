285471485ee213d033ce49dfc5875b63342c23ca
uni <miju@Uui-MacBookAir.local>
2022-02-18 11:23:50 +0900

    빼빼로 몸통출력하는 함수

--- CodeStarterCamp_Week1/drawPepero.swift
+++ CodeStarterCamp_Week1/drawPepero.swift
@@ -22,3 +22,17 @@ func makePeperoBody(Body: String, topping: String ) -> String {
     return topping + Body + topping
     }
 }
+
+func printPeperoBody(Length: Int, Body: String, topping: String) {
+    for count in 1...Length {
+        if Body == "|0|" {
+            if count % 2 == 0 {
+                print(" |0|")
+            } else {
+                print(" | |")
+            }
+        } else {
+            print(makePeperoBody(Body: Body, topping: topping))
+        }
+    }
+}
