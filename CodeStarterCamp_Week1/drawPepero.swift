04652e8913736414cd90110f509a44395c972e1a
uni <miju@Uui-MacBookAir.local>
2022-02-18 11:19:49 +0900

    막대 그리는 함수 구현

---
+++ CodeStarterCamp_Week1/drawPepero.swift
@@ -0,0 +1,15 @@
+//
+//  drawPepero.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by MIJU on 2022/02/18.
+//
+
+import Foundation
+
+
+func drawStick(length: Int) {
+    for _ in 1...length {
+        print(" | |")
+    }
+}
