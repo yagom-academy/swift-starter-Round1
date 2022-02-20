af0ad85011f11f0c70db6a8c6f49a3fd07c7b9ed
iwonman <tjdnjswls3@naver.com>
2022-02-20 19:36:29 +0900

    Step03 Code Edit

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -4,43 +4,35 @@
 //
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
 func drawUnder(length: Int){
     for _ in 1...length{
-        print(" ",terminator: "")
-        print("| |")
+        print(" ",terminator: "| |\n")
     }
-
 }
+
 func drawBodyChange(height: Int , body: String, topping: String){
-    if body == "|0|" && topping==""{
+    if body == "|0|" && topping == "" {
         for _ in 1...height/2{
-            print(" | |")
-            print("",topping+body+topping)
+            print(" | |\n",topping+body+topping)
         }
     }
     else{
         for _ in 1...height{
-            print(topping+body+topping,terminator: "")
-            print()
+            print(topping+body+topping,terminator: "\n")
         }
     }
 }
+
 func drawBody(height: Int,body: String){
     for _ in 1...height{
-        print(" ",terminator: "")
-        print("\(body)")
+        print(" ",terminator: "\(body)\n")
     }
 }
 
-func orderBBR(height: Int ,body: String, topping:String,length: Int){
-
+func orderBBR(height: Int ,body: String, topping:String, length: Int){
     print("<정보>","\n길이: ", height,"\n몸통: \(body) \n토핑: \(topping)","\n막대길이: ",length)
-
-    
     if topping != "" || body == "|0|" {
         drawBodyChange(height: height,body: body,topping: topping)
     }
@@ -50,6 +42,5 @@ func orderBBR(height: Int ,body: String, topping:String,length: Int){
     drawUnder(length: length)
 }
 
-
-orderBBR(height: 6, body: "|0|", topping: "", length: 4)
+orderBBR(height: 6, body: "***", topping: "", length: 4)
 
