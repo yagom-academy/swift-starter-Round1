da291e216e93a31f8a6afaa06e1840e4db3c7c9c
jiwoo99 <jiwoo9757@naver.com>
2022-02-21 00:49:14 +0900

    week1[step2]아이스크림 그리기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
+//------------step2 아이스크림 그리기 --------------
+//아이스크림 부분을 그리는 함수
+func iceCream(iceCreamNumber: Int) {
+    print("***********")
+}
+
+for iceCreamNumber in 1...8 {
+    iceCream(iceCreamNumber: iceCreamNumber)
+}
+
+//막대기(손잡이) 부분을 그리는 함수
+func iceCreamBar(iceCreamBarNumber : Int) {
+    print("    | |    ")
+}
+
+for iceCreamBarNumber in 1...4 {
+    iceCreamBar(iceCreamBarNumber: iceCreamBarNumber)
+}
 
