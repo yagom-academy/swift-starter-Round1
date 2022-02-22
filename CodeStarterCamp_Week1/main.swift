6935ad02030ca336dc2f069cb1ea7945a330b368
starting1231 <starting1231@naver.com>
2022-02-22 12:26:21 +0900

    Refactor : 불필요한 문자 제거, 매개변수명 수정, 줄바꿈 수정 - 13, 20, 30, 39, 40, 48번 줄 공백제거 - Height 매개변수 -> height 수정 - 21, 31번 else 문 줄바꿈 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,29 +8,27 @@
 import Foundation
 
 //길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
-func drawStick(Height: Int) {
-    for _ in 1...Height{
-        print(" | | ")
+func drawStick(height: Int) {
+    for _ in 1...height{
+        print(" | |")
     }
 }
 
 //몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 리턴하는 함수
 func makeTopBody(body: String, topping: String) -> String {
     if topping == "" {
-        return (" \(body) ")
-    }
-    else {
-        return ("\(topping)\(body)\(topping)")
+        return " \(body)"
+    } else {
+        return "\(topping)\(body)\(topping)"
     }
 }
 
 //길이(Int)에 따라 몸통을 그리는 함수
-func drawBody(Height: Int, body: String, nudeBebero: Bool) {
-    for count in 1...Height {
+func drawBody(height: Int, body: String, nudeBebero: Bool) {
+    for count in 1...height {
         if nudeBebero && count%2 != 0 {
-            print(" | | ")
-        }
-        else{
+            print(" | |")
+        } else{
             print(body)
         }
     }
@@ -38,8 +36,8 @@ func drawBody(Height: Int, body: String, nudeBebero: Bool) {
 
 //각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
 func makeBebero (bodyHeight: Int, bodyType: String, toppingType: String, stickHeight: Int) {
-    let nude : Bool
-    let BeberoBody : String
+    let nude: Bool
+    let BeberoBody: String
     print("<정보>\n길이: \(bodyHeight)\n몸통: \(bodyType)\n토핑: \(toppingType)\n막대길이: \(stickHeight)\n")
     if bodyType == "|0|" {
         nude = true
@@ -47,8 +45,8 @@ func makeBebero (bodyHeight: Int, bodyType: String, toppingType: String, stickHe
         nude = false
     }
     BeberoBody = makeTopBody(body: bodyType, topping: toppingType)
-    drawBody(Height: bodyHeight, body: BeberoBody,  nudeBebero: nude)
-    drawStick(Height: stickHeight)
+    drawBody(height: bodyHeight, body: BeberoBody, nudeBebero: nude)
+    drawStick(height: stickHeight)
 }
 
 makeBebero(bodyHeight: 6, bodyType: "|0|", toppingType: "", stickHeight: 4)
