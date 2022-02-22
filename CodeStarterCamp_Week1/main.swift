a1782dfd144049ae81b477a0ea9c14b30ee46168
Ares Jinhwan Kim <aresjinhwankim@Ares-MacBook-Pro-2020.local>
2022-02-22 20:46:31 +0900

    1. Week1 Step 3 과제 제출 2. Step 3 Flowchart 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,20 +9,42 @@
 import Foundation
 
 
-func makeIce(iceHeight: Int) { // Icecream
-    for _ in 1...iceHeight {
-        print("***********")
-    }
+let iceLength: Int = 5 // 숫자 입력
+let iceBody: String = "&*&" // 문자 3자 입력
+let iceTopping: String = " " // 문자 1자 입력, 토핑 불필요시 공백 한칸 유지 " "
+let iceBarLength: Int = 5 // 숫자 입력
 
+    
+func icecreamInfo () {                                                 // icecream 정보 출력 함수
+    print ("길이: \(iceLength)")
+    print ("몸통: \(iceBody)")
+    print ("토핑: \(iceTopping)")
+    print ("막대길이: \(iceBarLength)")
+    print ()
 }
 
-func makeIcebar(icebarHeight: Int) { // Icebar
-    for _ in 1...icebarHeight {
-        print("    | |     ")
+func formIcecream (iceBodyIn: String, iceToppingIn: String) {         // iceLength에 따라 몸통 그리는 함수
+    print ("\(iceToppingIn)", "\(iceBodyIn)", "\(iceToppingIn)", separator:"")
+    }
+
+
+func makeIceBar () {                                                  // ice막대 그리는 함수
+    for _ in 1...iceBarLength {
+            print (" | |")
     }
 }
 
+func makeIcecream () {                                                // icecream 그리는 함수
+    for _ in 1...iceLength {
+        formIcecream(iceBodyIn: iceBody, iceToppingIn: iceTopping)
+    }
+}
 
+icecreamInfo()
+makeIcecream()
+makeIceBar()
 
-makeIce(iceHeight: 8) // Icecream 길이 조정
-makeIcebar(icebarHeight: 4) // Icebar 길이 조정
+    
+
+//makeIce(iceHeight: 8) // Icecream 길이 조정
+//makeIcebar(icebarHeight: 4) // Icebar 길이 조정
