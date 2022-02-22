e0d97465713aa77b6017d7328bf40854f548509c
Hangga99 <qkrrlxo418@naver.com>
2022-02-22 23:28:13 +0900

    코드 및 순서도 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -3,21 +3,49 @@ import Foundation
 drawIceTop()
 drawIceBar()
 
+func drawPeperoBarLength(BarLength: Int) {
+    for _ in 1...BarLength {
+        print(" | | ")
+    }
+}
+
+func drawPeperoBody(body: String, topping: String) {
+    print(topping + body + topping)
+}
+
+func drawPeperoBar(lenght: Int, topping: String, body: String) {
+    if body == "***" {
+        for _ in 1...lenght {
+            drawPeperoBody(body: body, topping: topping)
+        }
+    }
+    else {
+        for i in 1...lenght {
+            if (i % 2) != 0{
+                print(" | |")
+            }
+            else {
+                print(body)
+            }
+        }
+    }
+}
+
+func makePepero(lenght: Int, topping: String, body: String, barLength: Int, bar: String) {
+    drawPeperoBody(body: body, topping: topping)
+    drawPeperoBar(lenght: lenght, topping: topping, body: body)
+    drawPeperoBarLength(BarLength: barLength)
+}
+
+// 실행 예시 1
 print("<정보>")
 print("길이: 10")
 print("몸통: ***")
 print("토핑:")
 print("막대길이: 4")
-func drawExample1Pepero(length: Int, bady: String,topping: String, barLength: Int) {
-    for _ in 1...length {
-        print(bady)
-    }
-    for _ in 1...barLength{
-        print(" | |")
-    }
-    print("\n")
-}
-drawExample1Pepero(length: 10, bady: " ***", topping:" ", barLength: 4)
+
+makePepero(lenght: 10, topping: " ", body: "***", barLength: 4, bar: " | |")
+
 
 // 실행 예시 2
 print("<정보>")
@@ -25,16 +53,8 @@ print("길이: 12")
 print("몸통: ***")
 print("토핑: &")
 print("막대길이: 4")
-func drawExample2Pepero(length: Int, bady: String,topping: String, barLength: Int) {
-    for _ in 1...length {
-        print(topping, bady, topping, separator: "")
-    }
-    for _ in 1...barLength{
-        print(" | |")
-    }
-    print("\n")
-}
-drawExample2Pepero(length: 12, bady: "***", topping:"&", barLength: 4)
+
+makePepero(lenght: 12, topping: "&", body: "***", barLength: 4, bar: " | |")
 
 // 실행 예시 3
 print("<정보>")
@@ -42,16 +62,8 @@ print("길이: 12")
 print("몸통: ***")
 print("토핑: #")
 print("막대길이: 6")
-func drawExample3Pepero(length: Int, bady: String,topping: String, barLength: Int) {
-    for _ in 1...length {
-        print(topping, bady, topping, separator: "")
-    }
-    for _ in 1...barLength{
-        print(" | |")
-    }
-    print("\n")
-}
-drawExample3Pepero(length: 12, bady: "***", topping:"#", barLength: 6)
+
+makePepero(lenght: 12, topping: "#", body: "***", barLength: 6, bar:  " | |")
 
 // 실행 예시 4
 print("<정보>")
@@ -59,35 +71,15 @@ print("길이: 6")
 print("몸통: |0|")
 print("토핑: ")
 print("막대길이: 4")
-func drawExample4Pepero(length: Int, bady: String,topping: String, barLength: Int) {
-    for i in 1...length {
-        if (i % 2) != 0{
-            print(" | |")
-        }
-        else {
-            print(bady)
-        }
-    }
-    for _ in 1...barLength{
-        print(" | |")
-    }
-    print("\n")
-}
-drawExample4Pepero(length: 6, bady: " |0|", topping:"", barLength: 4)
+
+makePepero(lenght: 6, topping: "", body: " |0|", barLength: 4, bar:  " | |")
+
 
 // 나만의 빼빼로 만들기
 print("<정보>")
 print("길이: 13")
-print("몸통: &&&&")
+print("몸통: ***")
 print("토핑: $")
 print("막대길이: 5")
-func drawExample5Pepero(length: Int, bady: String,topping: String, barLength: Int) {
-    for _ in 1...length {
-        print(topping, bady, topping, separator: "")
-    }
-    for _ in 1...barLength{
-        print(" |  |")
-    }
-    print("\n")
-}
-drawExample5Pepero(length: 13, bady: "&&&&", topping:"$", barLength: 5)
+
+makePepero(lenght: 13, topping: "$", body: "***", barLength: 5, bar:   " | |")
