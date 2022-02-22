64ed11cda061e31eb104e14da31c3b08fd6a3ebc
TerryEHLee <capssemerll@gmail.com>
2022-02-22 22:49:11 +0900

    main.swift func실행파일입니다

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,17 +6,17 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-func drawIcecreamStar(numberOfStars: Int) {
-    for _ in 1...numberOfStars {
-        print("*************")
-    }
-}
+/* Step2 실행 함수 입니다.
+ drawIcecreamStar(numberOfStars: 15)
+ drawIcecreamBody(numberOfBodies: 5)
+*/
 
-func drawIcecreamBody(numberOfBodies: Int) {
-    for _ in 1...numberOfBodies {
-        print("    |  |")
-    }
-}
+// Step3 실행함수 입니다.
 
-drawIcecreamStar(numberOfStars: 15)
-drawIcecreamBody(numberOfBodies: 5)
+
+drawBerow(recipe: "Berow 1", body: " ***", topping: "" , bodyLength: 10, stickLength: 4)
+drawBerow(recipe: "Berow 2", body: "***", topping: "&" , bodyLength: 12, stickLength: 4)
+drawBerow(recipe: "Berow 3", body: "***", topping: "#" , bodyLength: 12, stickLength: 6)
+drawBerow(recipe: "Berow 4", body: " |0|", topping: "" , bodyLength: 6, stickLength: 4)
+drawBerow(recipe: "My Own", body: "|=|", topping: "+" , bodyLength: 7, stickLength: 4)
+
