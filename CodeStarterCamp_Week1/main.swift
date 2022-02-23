1b45c5c68239256e1c892703ff8021e6093a984e
freemandeveloper <freemandeveloper1@gmail.com>
2022-02-23 12:39:47 +0900

    icecream 코드 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-func printbar(of length: Int) {
-    for bar in 1 ... length {
-        print("    | |    ")
+printicecream(of: 8)
+
+func printicecream(of length: Int) {
+    for icecream in 1 ... length {
+        print("***********")
     }
 }
-
-printbar(of: 4)
