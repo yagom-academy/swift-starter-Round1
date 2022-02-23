e1ee0ed91c92e0ceac496bb311d52c00cdb3aac5
freemandeveloper <freemandeveloper1@gmail.com>
2022-02-24 01:12:06 +0900

    Step3 실행예시2 코드 추가

--- CodeStarterCamp_Week1/printPockybyorder.swift
+++ CodeStarterCamp_Week1/printPockybyorder.swift
@@ -8,7 +8,7 @@ func makePockybody(topping:String, pockybody:String){
 
 func printPockybody(lengthofPocky:Int){
     for _ in 1 ... lengthofPocky {
-        makePockybody(topping: " ", pockybody: "***")
+        makePockybody(topping: "&", pockybody: "***")
 }
 }
 
@@ -19,6 +19,6 @@ func printPockybar(pockyBar:String, lengthofpockybar: Int) {
 }
 
 func printPocky(){
-    printPockybody(lengthofPocky: 10)
+    printPockybody(lengthofPocky: 12)
     printPockybar(pockyBar: " | | ", lengthofpockybar: 4)
 }
