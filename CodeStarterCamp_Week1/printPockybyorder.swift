0b4e530946e16ddb86108fb27349cb402fa65d54
freemandeveloper <freemandeveloper1@gmail.com>
2022-02-23 23:29:37 +0900

    Step3 실행예시1  코드 추가

---
+++ CodeStarterCamp_Week1/printPockybyorder.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func makePockybody(topping:String, pockybody:String){
+    print(topping, terminator: "")
+    print(pockybody, separator: "", terminator: "")
+    print(topping)
+}
+
+func printPockybody(lengthofPocky:Int){
+    for _ in 1 ... lengthofPocky {
+        makePockybody(topping: " ", pockybody: "***")
+}
+}
+
+func printPockybar(pockyBar:String, lengthofpockybar: Int) {
+    for _ in 1 ... lengthofpockybar {
+            print(pockyBar)
+}
+}
+
+func printPocky(){
+    printPockybody(lengthofPocky: 10)
+    printPockybar(pockyBar: " | | ", lengthofpockybar: 4)
+}
+
---
+++ PrintPockbyOrder.swift
@@ -0,0 +1,8 @@
+//
+//  PrintPockbyOrder.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Sung Chul Yea on 2022/02/23.
+//
+
+import Foundation
