dd4123452135a7d4fe25621b97449faa0a7953ac
freemandeveloper <freemandeveloper1@gmail.com>
2022-02-24 01:08:55 +0900

    세 메서드를 호출하는 함수 코드  추가

--- CodeStarterCamp_Week1/printPockybyorder.swift
+++ CodeStarterCamp_Week1/printPockybyorder.swift
@@ -1,8 +1,23 @@
 import Foundation
 
+func makePockybody(topping:String, pockybody:String){
+    print(topping, terminator: "")
+    print(pockybody, separator: "", terminator: "")
+    print(topping)
+}
+
+func printPockybody(lengthofPocky:Int){
+    for _ in 1 ... lengthofPocky {
+        makePockybody(topping: "", pockybody: "")
+}
+}
+
 func printPockybar(pockyBar:String, lengthofpockybar: Int) {
     for _ in 1 ... lengthofpockybar {
             print(pockyBar)
 }
 }
 
+func printPocky(){
+    printPockybody(lengthofPocky: )
+    printPockybar(pockyBar: "", lengthofpockybar: )
