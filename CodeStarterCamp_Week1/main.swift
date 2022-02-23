d2daa4ca6dc860063ee4384f8926bfb386348a5d
freemandeveloper <freemandeveloper1@gmail.com>
2022-02-23 12:43:41 +0900

    icecream에 bar 코드 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,5 +1,13 @@
 import Foundation
 
+printicecream(of: 8)
+
+func printicecream(of length: Int) {
+    for icecream in 1 ... length {
+        print("***********")
+    }
+}
+
 printbar(of: 4)
 
 func printbar(of length: Int) {
