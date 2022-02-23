bcb481041d3e9854c1160c0dfc706ac1b0214610
freemandeveloper <freemandeveloper1@gmail.com>
2022-02-24 01:06:35 +0900

    길이에 따라 몸통을 그리는 코드 추가

--- CodeStarterCamp_Week1/printPockybyorder.swift
+++ CodeStarterCamp_Week1/printPockybyorder.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-func makePockybody(topping:String, pockybody:String){
-    print(topping, terminator: "")
-    print(pockybody, separator: "", terminator: "")
-    print(topping)
+func printPockybody(lengthofPocky:Int){
+    for _ in 1 ... lengthofPocky {
+        makePockybody(topping: "", pockybody: "")
+}
 }
