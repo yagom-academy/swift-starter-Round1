3dcaef4deea1c9ecf146e18dc0a730c202840376
Hangga99 <qkrrlxo418@naver.com>
2022-02-23 22:23:39 +0900

    코드 및 순서도 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -3,8 +3,8 @@ import Foundation
 drawIceTop()
 drawIceBar()
 
-func drawPeperoBarLength(BarLength: Int) {
-    for _ in 1...BarLength {
+func drawPeperoBarLength(sickLength: Int) {
+    for _ in 1...sickLength {
         print(" | | ")
     }
 }
@@ -13,73 +13,53 @@ func drawPeperoBody(body: String, topping: String) {
     print(topping + body + topping)
 }
 
-func drawPeperoBar(lenght: Int, topping: String, body: String) {
+func drawPeperoBodyAndLength(length: Int, topping: String, body: String) {
     if body == "***" {
-        for _ in 1...lenght {
+        for _ in 1...length {
             drawPeperoBody(body: body, topping: topping)
         }
     }
     else {
-        for i in 1...lenght {
-            if (i % 2) != 0{
+        for oddNumber in 1...length {
+            if (oddNumber % 2) != 0 {
                 print(" | |")
-            }
-            else {
+            } else {
                 print(body)
             }
         }
     }
 }
 
-func makePepero(lenght: Int, topping: String, body: String, barLength: Int, bar: String) {
-    drawPeperoBody(body: body, topping: topping)
-    drawPeperoBar(lenght: lenght, topping: topping, body: body)
-    drawPeperoBarLength(BarLength: barLength)
+func makePepero(lenght: Int, topping: String, body: String, sickLength: Int, bar: String) {
+    drawPeperoBodyAndLength(length: lenght, topping: topping, body: body)
+    drawPeperoBarLength(sickLength: sickLength)
+}
+
+
+func printPeperoData (lenght: Int, body: String, topping: String, sickLength: Int){
+    print("<정보>")
+    print("길이: \(lenght)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이: \(sickLength)")
 }
 
 // 실행 예시 1
-print("<정보>")
-print("길이: 10")
-print("몸통: ***")
-print("토핑:")
-print("막대길이: 4")
-
-makePepero(lenght: 10, topping: " ", body: "***", barLength: 4, bar: " | |")
-
+printPeperoData(lenght: 10, body: "***", topping:  " ", sickLength: 4)
+makePepero(lenght: 10, topping: " ", body: "***", sickLength: 4, bar: " | |")
 
 // 실행 예시 2
-print("<정보>")
-print("길이: 12")
-print("몸통: ***")
-print("토핑: &")
-print("막대길이: 4")
-
-makePepero(lenght: 12, topping: "&", body: "***", barLength: 4, bar: " | |")
+printPeperoData(lenght: 12, body: "***", topping:  "&", sickLength: 4)
+makePepero(lenght: 12, topping: "&", body: "***", sickLength: 4, bar: " | |")
 
 // 실행 예시 3
-print("<정보>")
-print("길이: 12")
-print("몸통: ***")
-print("토핑: #")
-print("막대길이: 6")
-
-makePepero(lenght: 12, topping: "#", body: "***", barLength: 6, bar:  " | |")
+printPeperoData(lenght: 12, body: "***", topping:  "#", sickLength: 6)
+makePepero(lenght: 12, topping: "#", body: "***", sickLength: 6, bar:  " | |")
 
 // 실행 예시 4
-print("<정보>")
-print("길이: 6")
-print("몸통: |0|")
-print("토핑: ")
-print("막대길이: 4")
-
-makePepero(lenght: 6, topping: "", body: " |0|", barLength: 4, bar:  " | |")
-
+printPeperoData(lenght: 6, body: "|0|", topping:  "", sickLength: 4)
+makePepero(lenght: 6, topping: "", body: " |0|", sickLength: 4, bar:  " | |")
 
 // 나만의 빼빼로 만들기
-print("<정보>")
-print("길이: 13")
-print("몸통: ***")
-print("토핑: $")
-print("막대길이: 5")
-
-makePepero(lenght: 13, topping: "$", body: "***", barLength: 5, bar:   " | |")
+printPeperoData(lenght: 13, body: "***", topping:  "$", sickLength: 5)
+makePepero(lenght: 13, topping: "$", body: "***", sickLength: 5, bar:   " | |")
