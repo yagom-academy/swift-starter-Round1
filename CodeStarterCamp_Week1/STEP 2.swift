8e5369b421e2eb29a77f9728ae92c5cd8a4297f5
ZZBAE <jebyun2@gmail.com>
2022-03-03 22:05:33 +0900

    첫번째 commit 입니다. step2

---
+++ CodeStarterCamp_Week1/STEP 2.swift
@@ -0,0 +1,20 @@
+//
+//  STEP 2.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 변재은 on 2022/03/03.
+//
+
+import Foundation
+
+func cookIcecreamBody() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func cookIcecreamStick() {
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+}
