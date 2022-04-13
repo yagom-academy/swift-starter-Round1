7bf22ec2f5085bcf2b2ae7a78b624d89ef71e0fe
wolframhwang <hzw94@naver.com>
2022-04-13 20:28:53 +0900

    [feat] Step 2: 아이스크림 그리기 - 아이스크림, 막대기를 표현할 함수 두가지를 작성하였습니다. - 각각, row, col 에 관하여 for 문을 통해 구현했습니다.
    Signed-off-by: wolframhwang <hzw94@naver.com>

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeIceCream() {
+    let height = 8
+    let width = 11
+    let rangeRow = 0..<height
+    let rangeCol = 0..<width
 
+    for _ in rangeRow {
+        for _ in rangeCol {
+            print("*", terminator: "")
+        }
+        print()
+    }
+}
+
+func makeBar() {
+    let height = 5
+    let width = 7
+    let rangeRow = 0..<height
+    let rangeCol = 0..<width
+
+    for _ in rangeRow {
+        for i in rangeCol {
+            if i == 4 || i == 6 {
+                print("|", terminator: "")
+                continue
+            }
+            print(" ", terminator: "")
+        }
+        print()
+    }
+}
+
+makeIceCream()
+makeBar()
