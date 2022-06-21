2093458d7725350a52fb6235c5e1e6e2f9ce0936
맹선아 <maengseon-a@maengseon-aui-MacBookAir.local>
2022-06-21 22:58:36 +0900

    flow chart 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -13,10 +13,14 @@ import Foundation
 //아이스크림 부분을 그리는 함수
 func printIcecreamBody (a: Int){
     print ("***********")
+
+
 }
     for count in 1...8 {
         printIcecreamBody(a : count)
     }
+
+
 
 
 //막대기(손잡이) 부분을 그리는 함수
@@ -24,5 +28,25 @@ func printStick (b: Int){
     print ("    | |    ")
 }
     for count in 1...4 {
-        printStick(b : count)
+        printStick(b: count)
+    }
+
+
+
+
+
+
+//아이스크림 부분을 그리는 함수 (2)
+
+func printIceCreamBody2 (Line: Int) {
+    func printHead (repeat : Int){
+        print ("*",separator:"",terminator:"")
+}
+    for count in 1...11 {
+        printHead (repeat : count)
+    }
+}
+for count2 in 1...8 {
+        print()
+        printIceCreamBody2(Line : count2)
     }
