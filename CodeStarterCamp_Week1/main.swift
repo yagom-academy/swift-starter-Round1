40406c0243cfc4669a46b33579b4ea1037dc523e
제리 <64826110+gwd0311@users.noreply.github.com>
2022-06-22 19:07:50 +0900

    Update main.swift

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,29 +8,4 @@
 
 import Foundation
 
-// 아이스크림 부분을 그리는 함수
-func iceCream (width: Int,height: Int){
-    for _ in 1...height{
-        for _ in 1...width {
-            print("",terminator: "*")
-        }
-        print("")
-    }
-}
-
-// 막대기(손잡이) 부분을 그리는 함수
-func bar (width: Int, height: Int){
-    for _ in 1...height{
-        for i in 1...width {
-            if i == width/2  || i == width/2 + 2 {
-                print("",terminator: "|")
-            }
-            else {
-                print("",terminator: " ")
-            }
-        }
-        print("")
-    }
-}
-iceCream(width: 11, height: 8)
-bar(width: 11, height: 4)
+print("Hello World")
