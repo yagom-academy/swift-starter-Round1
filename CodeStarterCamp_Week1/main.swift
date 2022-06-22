60b802671bcad6263bd8d5696cf430d546dd2092
맹선아 <maengseon-a@maengseon-aui-MacBookAir.local>
2022-06-22 14:58:01 +0900

    code수정(반복문을 함수안으로), flowchart재작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -11,42 +11,21 @@ import Foundation
 
 
 //아이스크림 부분을 그리는 함수
-func printIcecreamBody (a: Int){
-    print ("***********")
-
-
-}
-    for count in 1...8 {
-        printIcecreamBody(a : count)
+func printIceCream (){
+    for _ in 1...8 {
+        print ("***********")
     }
+}
 
+printIceCream()
 
 
 
 //막대기(손잡이) 부분을 그리는 함수
-func printStick (b: Int){
-    print ("    | |    ")
-}
-    for count in 1...4 {
-        printStick(b: count)
-    }
-
-
-
-
-
-
-//아이스크림 부분을 그리는 함수 (2)
-
-func printIceCreamBody2 (Line: Int) {
-    func printHead (repeat : Int){
-        print ("*",separator:"",terminator:"")
-}
-    for count in 1...11 {
-        printHead (repeat : count)
+func printStick (){
+    for _ in 1...4 {
+        print ("    | |    ")
     }
 }
-for count2 in 1...8 {
-        print()
-        printIceCreamBody2(Line : count2)
-    }
+
+printStick()
