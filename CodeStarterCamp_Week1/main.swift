26cf76a5c3b7ae68cf1c9c0f441fd5b1a863f0ee
gye0rehan <101076197+gye0rehan@users.noreply.github.com>
2022-06-22 17:30:36 +0900

    Week1-Step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,15 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+func makeIce(ice:Int){
+    for _ in 1...ice{
+            print("***********")
+        }
+} // 아이스크림 부분을 ice만큼 그리기
 
-import Foundation
+func makeBar(bar:Int){
+    for _ in 1...bar{
+            print("    | |")
+        }
+} // 막대부분을 bar만큼 그리기
 
-print("Hello, World!")
 
+makeIce(ice: 8) // 아이스크림 부분 8만큼 호출
+makeBar(bar: 4) // 막대 부분을 4만큼 호출
