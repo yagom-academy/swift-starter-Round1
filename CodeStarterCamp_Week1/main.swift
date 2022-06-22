05f03fb78ea0fb67657e4df2ad5e3c1a52e20a05
Jae Hyung Kim <smfc.mene@gmail.com>
2022-06-22 23:19:45 +0800

    feat: 아이스크림 그리는 함수 구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,16 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+func printIceCreamBody() {
+    for _ in 1...8 {
+        print(" *********")
+    }
+}
 
+func printIceCreamHandle() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+printIceCreamBody()
+printIceCreamHandle()
