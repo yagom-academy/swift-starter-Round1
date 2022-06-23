b595828971117ff5151c0b25617425fe4b8f3fe5
98-LEE <lyw2072@knou.ac.kr>
2022-06-24 01:36:15 +0900

    Remove

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,32 +8,4 @@
 
 import Foundation
 
-func drawHead(size : Int) {
-    for _ in 0...size-3{
-        for _ in 0...size{
-            print("*", terminator: "")
-        }
-        print("")
-    }
-}
-drawHead(size: 11)
-
-
-func drawBody(size : Int){
-    for _ in 0...size{
-        print("    |  |")
-    }
-}
-drawBody(size: 4)
-//func drawHead(size : Int, size2 : Int){
-//    for _ in 0...size{
-//        for _ in 0...size2{
-//            print("*", terminator: "")
-//        }
-//        print("")
-//    }
-//}
-//}
-//
-//
-//drawHead(size: 8, size2: 11)
+print("Hello World")
