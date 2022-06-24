db247abeba44aea76f177af4aa9adc51660563b7
hanjongwoo <gwd0311@naver.com>
2022-06-24 17:02:39 +0900

    Step3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,31 +5,34 @@
 
 import Foundation
 
-// 아이스크림 부분을 그리는 함수
-func makeIceCream(width: Int,height: Int){
-    for _ in 1...height{
-        for _ in 1...width {
-            print("",terminator: "*")
-        }
-        print("")
+// 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
+func drawBar (barHeight: Int,space: String) {
+    for _ in 1...barHeight {
+        print(space+"| |"+space)
     }
 }
-
-// 막대기(손잡이) 부분을 그리는 함수
-func makeBar(iceCreamWidth: Int, barHeight: Int){
-    for _ in 1...barHeight{
-        for i in 1...iceCreamWidth {
-            if i == iceCreamWidth/2  || i == iceCreamWidth/2 + 2 {
-                print("",terminator: "|")
-            }
-            else {
-                print("",terminator: " ")
-            }
-        }
-        print("")
+// 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+func drawBody (body: String, topping: String) {
+    print(topping+body+topping)
+}
+// 길이(Int)에 따라 몸통을 그리는 함수
+func drawAllBody (height: Int, body: String, topping: String) {
+    for _ in 1...height {
+        drawBody(body: body, topping: topping)
     }
 }
+// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+func drawPepero (height: Int, body: String, topping: String, barHeight: Int) {
+    print("<정보>")
+    print("길이:"+String(height))
+    print("몸통:"+body)
+    print("토핑:"+topping)
+    print("막대길이:"+String(barHeight))
+    print("")
+    
+    drawAllBody(height: height, body: body, topping: topping)
+    drawBar(barHeight: barHeight, space: String(repeating: " ", count: topping.count))
+}
 
-makeIceCream(width: 11, height: 8)
-makeBar(iceCreamWidth: 11, barHeight: 4)
+drawPepero(height: 6, body: "|0|", topping: " ", barHeight: 4)
 
