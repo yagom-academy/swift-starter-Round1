5adda70eab4f522ceda9b6ec7a196bae6ab047af
gye0rehan <101076197+gye0rehan@users.noreply.github.com>
2022-06-24 14:50:23 +0900

    STEP2 피드백 반영

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -2,21 +2,21 @@ import Foundation
 
 
 // 아이스크림 몸통 그리기 함수
-func makeBody(bodyHeight:Int){
+func makeBody(bodyHeight: Int){
     for cup in 1...bodyHeight{
         print("***********")
     }
 }
 
 //아이스크림 바 그리기 함수
-func makeBar(barHeight:Int){
+func makeBar(barHeight: Int){
     for car in 1...barHeight{
         print("    | |")
     }
 }
 
 //아이스크림 그리기 함수
-func makeIceCream(bodyHeight:Int,barHeight:Int){
+func makeIceCream(bodyHeight: Int,barHeight: Int){
     makeBody(bodyHeight: bodyHeight)
     makeBar(barHeight: barHeight)
 }
