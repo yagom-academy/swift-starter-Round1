a2218192f32354c48dddd6feb897fd19b4276c57
junhoL15 <junhoL1046@gmail.com>
2022-06-24 20:16:41 +0900

    함수 주석 제거

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-/// 아이스크림 부분을 그리는 함수
 func printIcecream(icecreamHeight height: Int, icecreamWidth width: Int = 11, icecreamShape shape: Character = "*") {
     for _ in 1...height {
         for _ in 1...width {
@@ -18,7 +17,6 @@ func printIcecream(icecreamHeight height: Int, icecreamWidth width: Int = 11, ic
     }
 }
 
-/// 막대기(손잡이) 부분을 그리는 함수
 func printStick(stickHeight height: Int, blankSpaceCount: Int = 4, Stickshape shape: String = "| |") {
     var blankSpace: String = ""
     for _ in 1...blankSpaceCount {
