f2623d5e8099d349102ac02cef3c829901179b41
jonghancha <jonghancha@gmail.com>
2022-06-24 18:25:02 +0900

    convention맞춰 일부 수정

--- CodeStarterCamp_Week1/Step3.swift
+++ CodeStarterCamp_Week1/Step3.swift
@@ -18,13 +18,11 @@ func makeStick(size: Int) {
 }
 
 func makeBodyPart(body: String, topping: String) {
-    
     if topping.isEmpty {
         peperoBodyPart = "  " + topping + body + topping
     }else{
         peperoBodyPart = " " + topping + body + topping
     }
-    
 }
 
 
@@ -37,11 +35,11 @@ func makeBodyAll(size: Int) {
 
 func customPepero(bodySize: Int, topping: String, handleSize: Int, body: String, handle: String = "    | |    ") {
     print("""
-        <정보>\n
-        길이: \(bodySize)\n
-        몸통: \(body)\n
-        토핑: \(topping)\n
-        막대길이: \(handleSize)\n
+         <정보>
+         길이: \(bodySize)
+         몸통: \(body)
+         토핑: \(topping)
+         막대길이: \(handleSize)
         """)
     makeBodyPart(body: body, topping: topping)
     makeBodyAll(size: bodySize)
