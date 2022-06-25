e9c4114f9d623e2066adf1b64b220129ebe29974
hanjongwoo <gwd0311@naver.com>
2022-06-25 18:39:49 +0900

    STEP3 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,33 +6,39 @@
 import Foundation
 
 // 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
-func drawBar(barHeight: Int,space: String) {
+func drawBar(_ barHeight: Int, _ space: String) {
     for _ in 1...barHeight {
         print(space+"| |"+space)
     }
 }
 // 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
-func drawBody(body: String, topping: String) {
+func drawBody(_ body: String, _ topping: String) {
     print(topping+body+topping)
 }
 // 길이(Int)에 따라 몸통을 그리는 함수
-func drawAllBody(bodyHeight: Int, body: String, topping: String) {
+func drawAllBody(_ bodyHeight: Int, _ body: String, _ topping: String) {
     for _ in 1...bodyHeight {
-        drawBody(body: body, topping: topping)
+        drawBody(body, topping)
     }
 }
-// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
-func drawPepero(bodyHeight: Int, body: String, topping: String, barHeight: Int) {
-    print("<정보>")
-    print("길이:"+String(bodyHeight))
-    print("몸통:"+body)
-    print("토핑:"+topping)
-    print("막대길이:"+String(barHeight))
-    print("")
-    
-    drawAllBody(bodyHeight: bodyHeight, body: body, topping: topping)
-    drawBar(barHeight: barHeight, space: String(repeating: " ", count: topping.count))
+// 빼빼로의 정보를 출력하는 함수
+func printPeperoInfo(_ bodyHeight: Int, _ body: String, _ topping: String, _ barHeight: Int) {
+    let info = """
+    <정보>
+    길이: \(String(bodyHeight))
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(String(barHeight))
+    """
+    print(info)
 }
 
-drawPepero(bodyHeight: 6, body: "|0|", topping: " ", barHeight: 4)
+// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+func drawPepero(_ bodyHeight: Int, _ body: String, _ topping: String, _ barHeight: Int) {
+    printPeperoInfo(bodyHeight, body, topping, barHeight)
+    drawAllBody(bodyHeight, body, topping)
+    drawBar(barHeight, String(repeating: " ", count: topping.count))
+}
+
+drawPepero(6, "|0|", " ", 4)
 
