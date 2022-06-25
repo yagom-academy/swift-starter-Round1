7443fd6e3ece72deba1c6df62c32c02e52b52d26
junhoL15 <junhoL1046@gmail.com>
2022-06-25 21:54:10 +0900

    Step3: 주문에 따른 빼빼로 그리기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -2,31 +2,50 @@
 //  main.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by junho lee on 2022/06/25.
 //
 
 import Foundation
 
-func printIcecream(height: Int, width: Int, shape: Character) {
-    for _ in 1...height {
-        for _ in 1...width {
-            print(shape, terminator: "")
-        }
-        print()
+func printPepero(bodyLength: Int,
+                 bodyShape: String,
+                 toppingShape: String,
+                 stickLength: Int) {
+    let peperoInformation: String = """
+    <정보>
+    길이: \(bodyLength)
+    몸통: \(bodyShape)
+    토핑: \(toppingShape)
+    막대길이: \(stickLength)
+    
+    """
+    print(peperoInformation)
+    printPeperoBody(length: bodyLength,
+                    bodyShape: bodyShape,
+                    toppingShape: toppingShape)
+    printPeperoStick(length: stickLength)
+    print()
+}
+
+func printPeperoStick(length: Int) {
+    for _ in 1...length {
+        print(" | | ")
     }
 }
 
-func printStick(height: Int, blankSpaceCount: Int, shape: String) {
-    var blankSpace: String = ""
-    for _ in 1...blankSpaceCount {
-        blankSpace += " "
-    }
-    for _ in 1...height {
-        print("\(blankSpace)\(shape)")
+func printPeperoBodyAndTopping(bodyshape: String, toppingShape: String) {
+    print("\(toppingShape)\(bodyshape)\(toppingShape)")
+}
+
+func printPeperoBody(length: Int, bodyShape: String, toppingShape: String) {
+    for _ in 1...length {
+        printPeperoBodyAndTopping(bodyshape: bodyShape,
+                                  toppingShape: toppingShape)
     }
 }
 
-printIcecream(height: 8, width: 11, shape: "*")
-
-printStick(height: 4, blankSpaceCount: 4, shape: "| |")
+printPepero(bodyLength: 10, bodyShape: "***", toppingShape: " ", stickLength: 4)
+printPepero(bodyLength: 12, bodyShape: "***", toppingShape: "&", stickLength: 4)
+printPepero(bodyLength: 12, bodyShape: "***", toppingShape: "#", stickLength: 6)
+printPepero(bodyLength: 6, bodyShape: "|0|", toppingShape: " ", stickLength: 4)
+printPepero(bodyLength: 8, bodyShape: "$$$", toppingShape: "+", stickLength: 4)
