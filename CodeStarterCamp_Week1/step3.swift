24f599c337d893f95bb9ee8f5a62ce2ea0be22f9
LimKiWoong <lkw4670@naver.com>
2022-06-27 22:29:15 +0900

    step3코드수정

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -8,22 +8,22 @@
 import Foundation
 
 // 몸통모양과 토핑 함수
-func makeBodyPretty(body: String = "| |", topping: String = "") -> String{
-    if(topping.isEmpty){
-        return " "+topping+body+topping
+func makeBodyShapeAndTopping(bodyShape: String = "| |", bodyTopping: String = "") -> String{
+    if(bodyTopping.isEmpty){
+        return " "+bodyTopping+bodyShape+bodyTopping
     } else {
-        return topping+body+topping
+        return bodyTopping+bodyShape+bodyTopping
     }
 }
 // 몸통길이 함수
-func makeBodyLength(length: Int, _ body: String){
-    for _ in 1...length {
+func makeBodyLength(bodyLength: Int,bodyShapePlusTopping body: String){
+    for _ in 1...bodyLength {
         print(" "+body)
     }
 }
 // 막대길이 함수
-func makeFootLength(length: Int){
-    for _ in 1...length {
+func makeFootLength(footLength: Int){
+    for _ in 1...footLength {
         print("  | | ")
     }
 }
@@ -40,6 +40,6 @@ func makeYammyBar(bodyLength: Int = 6,bodyShape: String = "| |",bodyTopping: Str
 
 """)
     
-    makeBodyLength(length: bodyLength, makeBodyPretty(body: bodyShape, topping: bodyTopping))
-    makeFootLength(length: footLength)
+    makeBodyLength(bodyLength: bodyLength,bodyShapePlusTopping: makeBodyShapeAndTopping(bodyShape: bodyShape, bodyTopping: bodyTopping))
+    makeFootLength(footLength: footLength)
 }
