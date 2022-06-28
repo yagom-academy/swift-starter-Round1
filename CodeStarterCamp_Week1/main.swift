5f7b55bac48d4324e77dfff391b04db6803c485b
ericwonjoonk98 <wonjoonkang94@gmail.com>
2022-06-28 17:01:07 +0900

    step3 완료

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,19 +6,35 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 import Foundation
-func makingIcecream(x: Int) {
-    for _ in 1...x {
-        for _ in 1...11{
-            print("*",terminator: "")
-            }
-        print("\n")
-}
-}
-func makingStick(y: Int) {
-    for _ in 1...y{
-        print("   ","|","|")
-    }
+func BasicPepero(BodyLength: Int,BodyShape: String, ToppingShape: String,StickLength: Int) {
+    let PeperoInfo: String = """
+<정보>
+길이: \(BodyLength)
+몸통: \(BodyShape)
+토핑: \(ToppingShape)
+막대길이: \(StickLength)
+"""
+    print(PeperoInfo)
+    makingBody(BodyLength: BodyLength, BodyShape:BodyShape,ToppingShape: ToppingShape)
+    makingStick(StickLength: StickLength)
 }
 
-makingIcecream(x:8)
-makingStick(y:4)
+func makingStick(StickLength: Int) {
+    for _ in 1...StickLength{
+        print(" | |")
+    }
+}
+func bodyTopping(BodyShape:String,ToppingShape:String) -> String {
+    let WholeBody: String = "\(ToppingShape)"+"\(BodyShape)"+"\(ToppingShape)"
+    return WholeBody
+}
+func makingBody(BodyLength: Int, BodyShape: String,ToppingShape: String) {
+    for _ in 1...BodyLength {
+        print(bodyTopping(BodyShape: BodyShape,ToppingShape: ToppingShape))
+    }
+}
+BasicPepero(BodyLength: 6,BodyShape: "**",ToppingShape: "|",StickLength: 3)
+
+
+
+
