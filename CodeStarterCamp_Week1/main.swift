ee871bb93accc7d4e530a62d845f7337a67b04ef
codesparrow12 <kanghd97@gmail.com>
2022-06-28 14:53:37 +0900

    STEP3 피드백 수정: 전달인자 레이블 추가, print 출력문을 하나로 줄임

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,32 +8,34 @@
 
 import Foundation
 
-func makePeperoToppingBody(body: String, topping: String) -> String {
+func makePeperoToppingBody(with body: String, with topping: String) -> String {
     return (topping + body + topping)
 }
 
-func drawPeperoToppingBody(Length: Int, toppingBody: String) {
+func drawPeperoToppingBody(of Length: Int, with toppingBody: String) {
     for _ in 1...Length {
         print(toppingBody)
     }
 }
 
-func drawPeperoStick(Length: Int) {
+func drawPeperoStick(of Length: Int) {
     for _ in 1...Length {
         print(" | |")
     }
 }
 
 func drawPepero(bodyLength: Int, body: String, topping: String, stickLength: Int) {
-    print("<정보>")
-    print("길이: \(bodyLength)")
-    print("몸통: \(body)")
-    print("토핑: \(topping)")
-    print("막대길이: \(stickLength)\n")
+    print("""
+        <정보>
+        길이: \(bodyLength)
+        몸통: \(body)
+        토핑: \(topping)
+        막대길이: \(stickLength)\n
+        """)
     
-    let toppingBody = makePeperoToppingBody(body: body, topping: topping)
-    drawPeperoToppingBody(Length: bodyLength, toppingBody: toppingBody)
-    drawPeperoStick(Length: stickLength)
+    let toppingBody = makePeperoToppingBody(with: body, with: topping)
+    drawPeperoToppingBody(of: bodyLength, with: toppingBody)
+    drawPeperoStick(of: stickLength)
 }
 
 drawPepero(bodyLength: 6, body: "|0|", topping: "&", stickLength: 4)
