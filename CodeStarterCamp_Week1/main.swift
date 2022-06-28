b9c40d8068c6cf33d987f6d591d003166d5aed84
yonggipo <yonggipo@icloud.com>
2022-06-28 18:00:20 +0900

    step 2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,3 +10,27 @@ import Foundation
 
 print("Hello, World!")
 
+// 👨‍🔬 Step 2: 아이스크림 그리기
+
+let iceCream = "***********"
+let stick = "    | |"
+
+// 아이스크림 부분을 그리는 함수
+func iceCreambody() {
+    for _ in 1...8 {
+        print("\(iceCream)")
+    }
+}
+
+// 막대기(손잡이) 부분을 그리는 함수
+func iceCreamstick() {
+    for _ in 1...4 {
+        print("\(stick)")
+    }
+}
+
+//함수를 호출
+iceCreambody()
+iceCreamstick()
+
+print("\n----------------------\n")
