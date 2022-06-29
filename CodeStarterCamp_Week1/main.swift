ac934e3eb1d8c8ba8aa1030dab1afc6335e85272
ericwonjoonk98 <wonjoonkang94@gmail.com>
2022-06-29 16:42:06 +0900

    week1 step 3 코멘트에 따른 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,17 +6,17 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 import Foundation
-func BasicPepero(BodyLength: Int,BodyShape: String, ToppingShape: String,StickLength: Int) {
+func basicPepero(bodyLength: Int,bodyShape: String, toppingShape: String,stickLength: Int) {
     let PeperoInfo: String = """
 <정보>
-길이: \(BodyLength)
-몸통: \(BodyShape)
-토핑: \(ToppingShape)
-막대길이: \(StickLength)
+길이: \(bodyLength)
+몸통: \(bodyShape)
+토핑: \(toppingShape)
+막대길이: \(stickLength)
 """
     print(PeperoInfo)
-    makingBody(BodyLength: BodyLength, BodyShape:BodyShape,ToppingShape: ToppingShape)
-    makingStick(StickLength: StickLength)
+    makingBody(bodyLength: bodyLength, bodyShape:bodyShape,toppingShape: toppingShape)
+    makingStick(StickLength: stickLength)
 }
 
 func makingStick(StickLength: Int) {
@@ -24,16 +24,16 @@ func makingStick(StickLength: Int) {
         print(" | |")
     }
 }
-func bodyTopping(BodyShape:String,ToppingShape:String) -> String {
-    let WholeBody: String = "\(ToppingShape)"+"\(BodyShape)"+"\(ToppingShape)"
+func bodyTopping(bodyShape:String,toppingShape:String) -> String {
+    let WholeBody: String = "\(toppingShape)"+"\(bodyShape)"+"\(toppingShape)"
     return WholeBody
 }
-func makingBody(BodyLength: Int, BodyShape: String,ToppingShape: String) {
-    for _ in 1...BodyLength {
-        print(bodyTopping(BodyShape: BodyShape,ToppingShape: ToppingShape))
+func makingBody(bodyLength: Int, bodyShape: String,toppingShape: String) {
+    for _ in 1...bodyLength {
+        print(bodyTopping(bodyShape: bodyShape,toppingShape: toppingShape))
     }
 }
-BasicPepero(BodyLength: 6,BodyShape: "**",ToppingShape: "|",StickLength: 3)
+basicPepero(bodyLength: 6,bodyShape: "**",toppingShape: "|",stickLength: 3)
 
 
 
