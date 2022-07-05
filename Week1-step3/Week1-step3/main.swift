676678d96671cf7855119cd874050b1028c13a4b
bert <bert11223@naver.com>
2022-07-05 19:24:32 +0900

    Week1-step3

--- Week1-step3/Week1-step3/main.swift
+++ Week1-step3/Week1-step3/main.swift
@@ -7,22 +7,7 @@
 
 import Foundation
 
-func makeChoco(topping : String, body : String, length : Int) {
-    for _ in 1...length {
-        print("\(topping)\(body)\(topping)")
-    }
-}
-
-func makeStick(stickLength : Int) {
-    for _ in 1...stickLength {
-        print(" | | ")
-    }
-    print("")
-}
-
-
-
-func makerIceCream(길이: Int, 몸통: String, 토핑: String, 막대길이: Int) {
+func makerChocoStick(길이: Int, 몸통: String, 토핑: String, 막대길이: Int) {
     print("""
 <정보>
 길이:\(길이)
@@ -31,15 +16,30 @@ func makerIceCream(길이: Int, 몸통: String, 토핑: String, 막대길이: In
 막대길이:\(막대길이)
 """)
     print("")
-  makeChoco(topping: 토핑, body: 몸통, length: 길이)
-   makeStick(stickLength: 막대길이)
+    makerChoco(topping: 토핑, body: 몸통, length: 길이)
+    makerStick(stickLength: 막대길이)
 }
 
-makerIceCream(길이: 10, 몸통: "***", 토핑: " ", 막대길이: 4)
-makerIceCream(길이: 12, 몸통: "***", 토핑: "&", 막대길이: 4)
-makerIceCream(길이: 12, 몸통: "***", 토핑: "#", 막대길이: 6)
-makerIceCream(길이: 6, 몸통: "|0|", 토핑: " ", 막대길이: 4)
+
+func makerChoco(topping : String, body : String, length : Int) {
+    for _ in 1...length {
+        print("\(topping)\(body)\(topping)")
+    }
+}
 
 
-    
-   
+func makerStick(stickLength : Int) {
+    for _ in 1...stickLength {
+        print(" | | ")
+    }
+    print("")
+}
+
+makerChocoStick(길이: 10, 몸통: "***", 토핑: " ", 막대길이: 4)
+makerChocoStick(길이: 12, 몸통: "***", 토핑: "&", 막대길이: 4)
+makerChocoStick(길이: 12, 몸통: "***", 토핑: "#", 막대길이: 6)
+makerChocoStick(길이: 6, 몸통: "|0|", 토핑: " ", 막대길이: 4)
+
+
+
+
