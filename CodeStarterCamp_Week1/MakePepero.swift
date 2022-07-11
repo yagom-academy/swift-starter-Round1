9470dac7004eff05ae1c7768ba0c5edb958e5755
chacha <hyhy0429_@nvaer.com>
2022-07-11 22:43:47 +0900

    Use the makePeperoTopping func

--- CodeStarterCamp_Week1/MakePepero.swift
+++ CodeStarterCamp_Week1/MakePepero.swift
@@ -31,3 +31,18 @@ func MakePepero(bodyHeight: Int, peperoBody: String, topping: String,  stickHeig
     makePeperoBody(bodyHeight: bodyHeight, topping: topping, peperoBody: peperoBody)
     makePeperoBar(stickHeight: stickHeight, bar: bar)
 }
+
+//실행예시 1
+MakePepero(bodyHeight: 10, peperoBody:" ***", topping: "", stickHeight: 4, bar: " | |")
+
+//실행예시 2
+MakePepero(bodyHeight: 12, peperoBody: "***", topping: "&", stickHeight: 4, bar: " | |")
+
+//실행예시 3
+MakePepero(bodyHeight: 12, peperoBody: "***", topping: "#", stickHeight: 6, bar: " | |")
+
+//실행예시 4
+MakePepero(bodyHeight: 6, peperoBody: " |0|", topping: "", stickHeight: 4, bar: " | |")
+
+//나만의 빼빼로
+MakePepero(bodyHeight: 4, peperoBody: "@@@", topping: "*", stickHeight: 4, bar: " |_|")
