2da8326202575f4cf5e0217754695cdb398a9b8f
baejunggil <threehandssoftbjk@gmail.com>
2022-10-19 14:49:10 +0900

    step2 들여쓰기 완료

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -33,46 +33,47 @@ var bar = "| |"                 // 막대
 // 몸통의 첫 라인 log
 func bodySectionLog() -> String {
 
-let sectionLog = topping + body + topping
+    let sectionLog = topping + body + topping
 
-return sectionLog
+    return sectionLog
 }
 
 // 몸통의 길이 log
 func bodyRowsLog() {
-for _ in 0..<nLength {
-    print(bodySectionLog())
-}
+    for _ in 0..<nLength {
+        print(bodySectionLog())
+    }
 }
 
 // 막대기의 첫 라인 log
 func barSectionLog() -> String {
-let nBarFirst = bodySectionLog().count / 2
+    
+    let nBarFirst = bodySectionLog().count / 2
 
-var blank = ""
+    var blank = ""
 
-for _ in 1..<nBarFirst {
-    blank = blank.appending(" ")
-}
+    for _ in 1..<nBarFirst {
+        blank = blank.appending(" ")
+    }
 
-let fullBar = blank + bar
+    let fullBar = blank + bar
 
-return fullBar
+    return fullBar
 }
 
 // 막대기의 길이 log
 func barRowsLog() {
 
-for _ in 0..<nBarLength {
-    
-    print(barSectionLog())
-}
+    for _ in 0..<nBarLength {
+        
+        print(barSectionLog())
+    }
 }
 
 // 아이스크림 만들기 함수
 func createIceCream() {
-bodyRowsLog()
-barRowsLog()
+    bodyRowsLog()
+    barRowsLog()
 }
 
 // MARK: Step 2: 아이스크림 그리기 실행
