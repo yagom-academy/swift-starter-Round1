32fc986a1a89af39e0cc518179a8db8cba4243ac
kimseongj <ksj8277@naver.com>
2022-10-19 20:41:59 +0900

    step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,16 +1,22 @@
 import Foundation
 
+let iceCream = "***********"
+let Stick = "    | |    "
+let iceCreamHeight = 10
+let StickHeight = 4
+
 func makeIcecream() {
-    for _ in 0..<10 {
-        print("***********")
+    for _ in 0..<iceCreamHeight {
+        print(iceCream)
     }
 }
 
 func makeStick() {
-    for _ in 0..<4 {
-        print("    | |    ")
+    for _ in 0..<StickHeight {
+        print(Stick)
     }
 }
 
 makeIcecream()
 makeStick()
+
