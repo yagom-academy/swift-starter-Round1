a9f7b648d185b460b8ab4ee088d18c0888671199
윤지윤 <jy@yunjiyun-ui-MacBookAir.local>
2022-10-21 02:36:36 +0900

    step2_r1

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,7 +9,7 @@
 import Swift
 
 
-func drawBody(cnt1: Int,num: Int){
+func drawBody(cnt1: Int,num: Int){      // 아이스크림 그리는 함수
     for _ in 1...cnt1{
         for _ in 1...num{
             print("*",terminator: "")
@@ -18,12 +18,12 @@ func drawBody(cnt1: Int,num: Int){
     }
 }
 
-func drawStick(cnt2: Int){
+func drawStick(cnt2: Int){              // 아이스크림의 막대 그리는 함수
     for _ in 1...cnt2{
         print("    | |    ")
     }
 }
 
-drawBody(cnt1: 8,num: 11)
+drawBody(cnt1: 8,num: 11)               // 미션 예시 그림의 * 갯수를 변수 값으로 할당
 drawStick(cnt2: 4)
 
