eae4347df2b309f2541829b96da3535f0b96c7a1
윤지윤 <jy@yunjiyun-ui-MacBookAir.local>
2022-10-21 02:16:53 +0900

    step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,8 +5,25 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+//MARK: - Step2: IceCream
+import Swift
 
-import Foundation
 
-print("Hello, World!")
+func drawBody(cnt1: Int,num: Int){
+    for _ in 1...cnt1{
+        for _ in 1...num{
+            print("*",terminator: "")
+        }
+        print("\n")
+    }
+}
+
+func drawStick(cnt2: Int){
+    for _ in 1...cnt2{
+        print("    | |    ")
+    }
+}
+
+drawBody(cnt1: 8,num: 11)
+drawStick(cnt2: 4)
 
