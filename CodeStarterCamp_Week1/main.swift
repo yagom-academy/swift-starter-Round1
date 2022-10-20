85ae96d8cdefdb63265d672ed692267962d93c13
gyeong <okbamtol@naver.com>
2022-10-20 18:34:02 +0900

    step 2까지

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,7 +6,23 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+/**
+for stars in 1...8{
+        print(stars, "***********" )
+}
+for bars in 1...4{
+    print(bars, "\t |  |")
+}
+*/
+func stars (count: Int){
+    print(count, "***********" )
+}
+for count in 1...8{
+    stars(count: count)
+}
+func bars (count: Int){
+    print(count, "\t |  |" )
+}
+for count in 1...4{
+    bars(count: count)
+}
