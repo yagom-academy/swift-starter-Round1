6e7a36c1a5521786036f3db872e70fcff476c124
500beckwon <kjsx60@gmail.com>
2022-10-20 18:53:37 +0900

    STEP 2 코드 보존 및 분리를 위한 파일 생성

---
+++ CodeStarterCamp_Week1/CreateIceCream.swift
@@ -0,0 +1,27 @@
+//
+//  CreateIceCream.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by ByungHoon Ann on 2022/10/20.
+//
+
+import Foundation
+
+
+/// 아이스크림 부분 출력
+func iceCreamPartDraw(count: Int) {
+    for _ in 0..<count {
+        print("***********")
+    }
+}
+
+/// 막대 부분 출력
+func stickPartDraw(count: Int) {
+    for _ in 0..<count {
+        print("    | |")
+    }
+}
+/*
+ iceCreamPartDraw(count: 8)
+ stickPartDraw(count: 4)
+*/
