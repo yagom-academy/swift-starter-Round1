3115937f12b0708d1d69b563f7b62cce473273a3
gyeong <okbamtol@naver.com>
2022-10-21 17:25:24 +0900

    mian

---
+++ CodeStarterCamp_Week1/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+//
+/**
+for stars in 1...8{
+        print(stars, "***********" )
+}
+for bars in 1...4{
+    print(bars, "\t |  |")
+}
+*/
+func stars (count: Int){
+    print(count, "***********" )
+}
+for count in 1...8{
+    stars(count: count)
+}
+func bars (count: Int){
+    print(count, "\t |  |" )
+}
+for count in 1...4{
+    bars(count: count)
+}
+ 
+//
