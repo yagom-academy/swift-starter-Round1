8caf89289f2265a5e81c1574ec5ab797c01ba9e0
siwol <siwolssi@gmail.com>
2022-10-21 12:25:31 +0900

    STEP2

---
+++ CodeStarterCamp_Week1/STEP2.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+func iceMake(iceHeight: Int) {
+    for _ in 1...iceHeight {
+        print("***********")
+                }
+            }
+
+iceMake(iceHeight: 8)
+
+func stickMake(stickHeight: Int) {
+    for _ in 1...stickHeight {
+        print("    | |")
+    }
+}
+
+stickMake(stickHeight: 4)
