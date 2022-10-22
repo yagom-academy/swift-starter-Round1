c85d8535b3d7a0251bcdee1d29bb226fa4b84b9b
망망 <mang@mangmang-ui-MacBookPro.local>
2022-10-22 23:38:14 +0900

    step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+let ice : String = "***********" // 상수 ice 할당 값
+let bar : String = "    | |    " // 상수 bar 할당 값
 
+// 아이스크림 부분 함수
+func icecream() {
+    for ice_high in 1...8 { // 상수 ice 8번 반복
+        print(ice)
+    }
+}
+
+// 스틱 부분 함수
+func stickBar() {
+    for bar_high in 1...4 { // 상수 bar 4번 반복
+        print(bar)
+    }
+}
+
+// 함수 호출
+icecream()
+stickBar()
