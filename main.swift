89f4c193206e1b440f2a377dbaa9195e4d780274
Rhode <98301931+hodoovely@users.noreply.github.com>
2022-10-22 20:25:48 +0900

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  week1Step2
+//
+//  Created by Jinah Park on 2022/10/22.
+//
+
+import Foundation
+
+func icecream1() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func icecream2() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+
+icecream1()
+icecream2()
