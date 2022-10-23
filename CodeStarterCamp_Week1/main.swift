b13839117dfc0cbf1e20cf6715f01a5c3db7b95f
gonasik <110089773+snacknam@users.noreply.github.com>
2022-10-23 22:05:11 +0900

    step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-print("Hello, World!")
+var icecreamWidth = 11
 
+func icecream(ingredient: String, height: Int) -> Void {
+    for _ in 1...height {
+        for _ in 1...icecreamWidth {
+            print("\(ingredient)", terminator: "")
+        }
+        print("")
+    }
+}
+
+func stick(height: Int) -> Void {
+    for _ in 1...height {
+        for _ in 1...icecreamWidth/2-1 {
+            print(" ", terminator: "")
+        }
+        print("| |")
+    }
+}
+
+icecream(ingredient: "*", height: 8)
+stick(height: 4)
