0512355243b7507c037b1cc4ce2e13df33fca60c
karmanx <wjdchl@ewhain.net>
2022-10-23 15:15:16 +0900

    Week 1 [STEP 2] 아샷추

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,7 +6,23 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-print("Hello, World!")
+// 아이스크림 부분을 그리는 함수
+func drawIceCream() {
+    let widthIceCream = String(repeating: "*", count: 11)
+    for _ in 1...8 {
+        print(widthIceCream)
+    }
+}
 
+// 막대기(손잡이) 부분을 그리는 함수
+func drawStick() {
+    let widthStick = "| |"
+    for _ in 1...4 {
+        print("    \(widthStick)    ")
+    }
+}
+
+// 함수 실행
+drawIceCream()
+drawStick()
