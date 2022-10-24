287c28666c699f74e6d3a55b099a02a07c921b54
karmanx <wjdchl@ewhain.net>
2022-10-24 20:25:03 +0900

    Week1 [STEP 2] 아샷추 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,22 +7,25 @@
 //
 
 
-// 아이스크림 부분을 그리는 함수
 func drawIceCream() {
-    let widthIceCream = String(repeating: "*", count: 11)
+    let iceCream = "*"
     for _ in 1...8 {
-        print(widthIceCream)
+        for _ in 1...11 {
+            print(iceCream, terminator: "")
+        }
+        print()
     }
 }
 
-// 막대기(손잡이) 부분을 그리는 함수
+
 func drawStick() {
-    let widthStick = "| |"
+    let stick = "| |"
+    let blank = "    "
     for _ in 1...4 {
-        print("    \(widthStick)    ")
+        print(blank + stick + blank)
     }
 }
 
-// 함수 실행
+
 drawIceCream()
 drawStick()
