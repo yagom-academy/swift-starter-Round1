2808c6dc07f3245bdd6bb898ebaf8cf466294aaa
윤지윤 <jy@yunjiyun-ui-MacBookAir.local>
2022-10-24 19:53:28 +0900

    step2_r2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,22 +8,22 @@
 //MARK: - Step2: IceCream
 import Swift
 
-
-func drawBody(cnt1: Int,num: Int){      // 아이스크림 그리는 함수
-    for _ in 1...cnt1{
-        for _ in 1...num{
-            print("*",terminator: "")
+func drawBody(){
+    for _ in 1...8{
+        for _ in 1...11{
+            print("*", terminator: "")
         }
-        print("\n")
+        print("\n", terminator: "")
     }
 }
 
-func drawStick(cnt2: Int){              // 아이스크림의 막대 그리는 함수
-    for _ in 1...cnt2{
-        print("    | |    ")
+func drawStick(){
+    for _ in 1...4{
+        print("    | |")
     }
 }
 
-drawBody(cnt1: 8,num: 11)               // 미션 예시 그림의 * 갯수를 변수 값으로 할당
-drawStick(cnt2: 4)
+drawBody()
+drawStick()
+
 
