92b7b4f060a016e8fbc19639b805d852389f8426
hemil0102 <hemil0102@gmail.com>
2022-10-24 17:49:45 +0900

    the naming of function and flowchart has changed as guide.

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,21 +8,22 @@
 
 import Foundation
 
-// Mark: - 아이스크림 몸통 함수
-func icecreamBody(height: Int) {
+// Mark: - 아이스크림 몸통을 그리는 함수
+func drawIcecreamBody(height: Int) {
     for n in 0..<height {
         print ("***********")
     }
 }
 
-// Mark: - 아이스크림 스틱 함수
-func icecreamStick(height: Int ) {
+// Mark: - 아이스크림 스틱을 그리는 함수
+func drawIcecreamStick(height: Int ) {
     for n in 0..<height {
         print ("    | |")
     }
 }
 
-icecreamBody(height: 8) // 몸통 8줄을 출력
-icecreamStick(height: 4) // 스틱 4줄을 출력
+drawIcecreamBody(height: 8)
+drawIcecreamStick(height: 4)
 
 
+          
