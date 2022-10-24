31f89028cf1b329a072259274b13e2468ade29f3
김석우 <noix@gimseog-uui-MacBookPro.local>
2022-10-24 15:54:04 +0900

    step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,27 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+let ice: String = "***********" // 상수 ice에 값 할당
+let bar: String = "    | |    " // 상수 bar에 값 할당
 
-import Foundation
+/*
+아이스크림을 두 부분으로
+아이스크림은 윗부분
+막대기는 아랫부분
+*/
 
-print("Hello, World!")
+// 윗부분
+func icecream() -> Void {
+    for length in 1...8{
+        print(ice)
+    }
+}
+
+// 아랫부분
+func stick() -> Void {
+    for length in  1...4{
+        print(bar)
+    }
+}
+
+// 함수 실행
+icecream()
+stick()
 
