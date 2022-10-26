1c458f2e826cb3f11f3f89c06ded5700f3952d5d
윤지윤 <jy@yunjiyun-ui-MacBookAir.local>
2022-10-26 22:01:20 +0900

    step3_r1

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,29 +1,38 @@
 //
-//  main.swift
+//  File.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 윤지윤 on 2022/10/26.
 //
-//MARK: - Step2: IceCream
+// 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
+// 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+// 길이(Int)에 따라 몸통을 그리는 함수
+// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+
+
 import Swift
 
-func drawBody(){
-    for _ in 1...8{
-        for _ in 1...11{
-            print("*", terminator: "")
-        }
-        print("\n", terminator: "")
+func drawStick(stick:Int){
+    for _ in 1...stick{
+        print(" | |")
     }
 }
-
-func drawStick(){
-    for _ in 1...4{
-        print("    | |")
+func drawBody(topping:String, body:String){
+        print ("\(topping)\(body)\(topping)")
+}
+func drawVeticalBody(topping:String, body:String, length:Int){
+    for _ in 1...(length-1){
+//        var drawBodyShape: (String, String) -> String = drawBody(topping:body:)
+        print("\(topping)\(body)\(topping)")
     }
 }
+func drawingComplete(topping: String, body: String, length:Int, stick:Int){
+    drawBody(topping: topping, body: body)
+    drawVeticalBody(topping: topping, body: body, length: length)
+    drawStick(stick: stick)
+}
 
-drawBody()
-drawStick()
-
-
+drawingComplete(topping: " ", body: "***", length: 10, stick: 4)
+drawingComplete(topping: "&", body: "***", length: 12, stick: 4)
+drawingComplete(topping: "#", body: "***", length: 12, stick: 6)
+drawingComplete(topping: " ", body: "|0|", length: 6, stick: 4)
---
+++ CodeStarterCamp_Week1/step2.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+//MARK: - Step2: IceCream
+//import Swift
+//
+//func drawBody(){
+//    for _ in 1...8{
+//        for _ in 1...11{
+//            print("*", terminator: "")
+//        }
+//        print("\n", terminator: "")
+//    }
+//}
+//
+//func drawStick(){
+//    for _ in 1...4{
+//        print("    | |")
+//    }
+//}
+//
+//drawBody()
+//drawStick()
+
+
