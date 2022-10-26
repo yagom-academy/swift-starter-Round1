0955f3ad92d6b988dccb188424eddcec428d7ebb
Channoori <chsa2584613@naver.com>
2022-10-26 20:13:11 +0900

    Step3 고쳐야할 부분 수정

--- CodeStarterCamp_Week1/Pepero.swift
+++ CodeStarterCamp_Week1/Pepero.swift
@@ -8,25 +8,25 @@
 
 import Foundation
 
-func drawPeperoStick(_ height: Int) {
-    for _ in 1...height { // 순서도에서 _ 대신 stickHeight로 표현
-        print(" | | ")
-    }
-}
-
-func drawPePeroBodyPrint(body: String, toping: String) -> String {
+func drawPeperoBody(body: String, toping: String) -> String {
     return toping+body+toping
 }
 
-func drawPePeroBodyHeight(height: Int, printString: String) {
-    for _ in 1...height { // 순서도에서 _ 대신 bodyHeight로 표현
+func repeatPeperoBody(peperoHeight: Int, printString: String) {
+    for _ in 1...peperoHeight { // 순서도에서 _ 대신 bodyHeight로 표현
         print(printString)
     }
 }
 
-func makePepero(_ height: Int, stickHeight: Int, body: String = "***", toping: String = " ") {
-    print("<정보>\n길이: \(height)\n몸통: \(body)\n토핑: \(toping)\n막대길이: \(stickHeight)\n")
-    let peperoBodyString = drawPePeroBodyPrint(body: body, toping: toping)
-    drawPePeroBodyHeight(height: height, printString: peperoBodyString)
+func drawPeperoStick(_ height: Int) {
+    for _ in 1...height { // 순서도에서 _ 대신 stickHeight로 표현
+        print(" | |")
+    }
+}
+
+func drawPepero(_ peperoHeight: Int, stickHeight: Int, body: String = "***", toping: String = " ") {
+    print("<정보>\n길이: \(peperoHeight)\n몸통: \(body)\n토핑: \(toping)\n막대길이: \(stickHeight)\n")
+    let peperoBodyString = drawPeperoBody(body: body, toping: toping)
+    repeatPeperoBody(peperoHeight: peperoHeight, printString: peperoBodyString)
     drawPeperoStick(stickHeight)
 }
