5c0d8d8e949276b1831f895e1a049e86e19dbc84
Duckduck1596 <108049138+Duckduck1596@users.noreply.github.com>
2022-10-27 10:47:48 +0900

    코드 수정

--- CodeStarterCamp_Week1/Step 3.swift
+++ CodeStarterCamp_Week1/Step 3.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 
-func drawPeperoStick(length: Int) {
+func drawPeperoStick(length: Int, body: String, toppings: String) {
     for _ in 1...length {
+        for _ in 1...(body.count + toppings.count * 2) / 2 - 1 {
+            print(" ", terminator: "")
+        }
         print("| |")
     }
 }
 
 func decidePeperoBody(body: String, toppings: String) {
-    print("toppings + body + toppings")
+    print(toppings + body + toppings)
 }
 
 func drawPeperobody(length: Int, body: String, toppings: String) {
@@ -25,14 +28,22 @@ func drawPeperobody(length: Int, body: String, toppings: String) {
 }
 
 func drawPepero(bodyLength: Int, bodyShape: String, toppingsShape: String, stickLength: Int) {
-    print("<정보>\n길이 : \(bodyLength)\n몸통 : \(bodyShape)\n토핑 : \(toppingsShape)\n막대길이 : \(stickLength)")
+    print("""
+              <정보>
+              길이 : \(bodyLength)
+              몸통 : \(bodyShape)
+              토핑 : \(toppingsShape)
+              막대길이 : \(stickLength)
+              """)
     drawPeperobody(length: bodyLength, body: bodyShape, toppings: toppingsShape)
-    drawPeperoStick(length: stickLength)
+    drawPeperoStick(length: stickLength, body: bodyShape, toppings: toppingsShape)
 }
 
 
 drawPepero(bodyLength: 6, bodyShape: " |0|", toppingsShape: "", stickLength: 4)
 
 drawPepero(bodyLength: 12, bodyShape: "***", toppingsShape: "#", stickLength: 6)
+
 drawPepero(bodyLength: 12, bodyShape: "***", toppingsShape: "&", stickLength: 4)
+
 drawPepero(bodyLength: 10, bodyShape: "***", toppingsShape: "", stickLength: 4)
