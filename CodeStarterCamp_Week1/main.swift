bc33d1bf1cfde7dced9e2c2674c56d71bf2273c9
윤지윤 <jy@yunjiyun-ui-MacBookAir.local>
2022-10-31 20:18:20 +0900

    step3_r3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -4,29 +4,23 @@
 //
 //  Created by 윤지윤 on 2022/10/26.
 //
-// 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
-// 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
-// 길이(Int)에 따라 몸통을 그리는 함수
-// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
-
 
 import Swift
 
-func drawStick(stick:Int){
-    for _ in 1...stick{
+func drawStick(stick: Int) {
+    for _ in 1...stick {
         print(" | |")
     }
 }
-func drawBody(topping:String, body:String){
+func drawBody(topping: String, body: String) {
         print ("\(topping)\(body)\(topping)")
 }
-func drawVeticalBody(topping:String, body:String, length:Int){
-    for _ in 1...(length-1){
-        //        var drawBodyShape: (String, String) -> String = drawBody(topping:body:)
-        print("\(topping)\(body)\(topping)")
+func drawVeticalBody(topping: String, body: String, length: Int) {
+    for _ in 1...(length-1) {
+        drawBody(topping: topping, body: body)
     }
 }
-func makePepero(topping: String, body: String, length:Int, stick:Int){
+func makePepero(topping: String, body: String, length: Int, stick: Int) {
     drawBody(topping: topping, body: body)
     drawVeticalBody(topping: topping, body: body, length: length)
     drawStick(stick: stick)
