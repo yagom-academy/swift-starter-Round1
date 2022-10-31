9f7f88bcc2b5526ebb38715cf83bfebae0c297ce
gyeong <okbamtol@naver.com>
2022-10-31 16:49:57 +0900

    스텝3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,40 +1,47 @@
+//
 //  main.swift
 //  SomeProject
 //
 //  Created by 정경우 on 2022/10/22.
 //
-func makeStars (count: Int) {
-    print("***********" )
-}
+var peperobody: String = ""
+var bodyLength: Int = 1
+var stickLength: Int = 1
 
-for count in 1...8 {
-    makeStars(count: count)
-}
- 
- func makeBars (count: Int) {
- print("   |   |" )
- }
- 
- for count in 1...4 {
- makeBars(count: count)
- }
- 
-/*
-func makeBars (count: Int) {
-    let stick : String = "   |   |"
+func chooseBodyTopping(bodyType: String = "a", topType: String = "b") -> String{
+    print("몸통종류 ", terminator: ":")
+    let bodyType = readLine()!
     
-    for a in 1...4 {
-        print(stick)
-    }
+    print("토핑종류 ", terminator: ":")
+    let topType = readLine()!
+    
+    peperobody = (topType + bodyType + topType)
+    return peperobody
 }
-*/
 
+func makeBody() {
+    print("몸통길이 ", terminator: ":")
+    bodyLength = Int(readLine()!)!
+}
 
+func makeStick() {
+    print("막대길이 ", terminator: ":")
+    stickLength = Int(readLine()!)!
+}
 
+func drawPepero() {
+    chooseBodyTopping()
+    makeBody()
+    makeStick()
+    
+    for _ in 1...bodyLength {
+        print(peperobody)
+    }
+    
+    for _ in 1...stickLength {
+        print(" | |")
+    }
+    
+}
 
-
-
-
-
-
-
+drawPepero()
