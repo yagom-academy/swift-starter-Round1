db928f3fa3e177ae8a819ef8f1d932aaaa32a11c
kokkilE <kkuldak1@naver.com>
2022-11-01 12:31:22 +0900

    Week1 [STEP 3] kokkile

---
+++ CodeStarterCamp_Week1/Week1_STEP3.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+// 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
+func drawStick(lenthOfStick: Int, shapeOfStick: String, lenthOfBlankOfStick: Int) -> Void {
+    for _ in 1...lenthOfStick {
+        for _ in 1...lenthOfBlankOfStick {
+            print(" ", terminator: "")
+        }
+        print(shapeOfStick)
+    }
+}
+
+// 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+func drawBody(shapeOfIceCream: String = "*", shapeOfTopping: String = "") -> Void {
+    print("\(shapeOfTopping)\(shapeOfIceCream)\(shapeOfTopping)")
+}
+
+// 길이(Int)에 따라 몸통을 그리는 함수
+func drawFullBody(lenthOfBody: Int, shapeOfIceCream: String, shapeOfTopping: String ) -> Void {
+    for _ in 1...lenthOfBody {
+        drawBody(shapeOfIceCream: shapeOfIceCream, shapeOfTopping: shapeOfTopping)
+    }
+}
+
+// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+func drawIceCream(lenthOfBody: Int, shapeOfIceCream: String, shapeOfTopping: String, lenthOfStick: Int, shapeOfStick: String ) -> Void {
+    let lenthOfBlankOfStick: Int = ((shapeOfTopping + shapeOfIceCream + shapeOfTopping).count - shapeOfStick.count) / 2
+
+    drawFullBody(lenthOfBody: lenthOfBody, shapeOfIceCream: shapeOfIceCream, shapeOfTopping: shapeOfTopping)
+    drawStick(lenthOfStick: lenthOfStick, shapeOfStick: shapeOfStick, lenthOfBlankOfStick: lenthOfBlankOfStick)
+    
+}
