5ab10a5e512e296f3c03acd2ec01f5aefac275ca
yegang <yeggang0602@gmail.com>
2022-11-01 18:24:27 +0900

    STEP3 코드

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,24 +1,39 @@
 //
-//  main.swift
+//  main_2.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 예강이다 on 2022/10/28.
 //
 
 import Foundation
 
-func makeIceCream() {
-    for _ in 1...8 {
-        print("***********")
+func makePepero(length: Int, peperoBody: String, topping: Character, stick: Int) {
+    print("""
+    <정보>
+    길이: \(length)
+    몸통: \(peperoBody)
+    토핑: \(topping)
+    막대길이: \(stick)
+    
+    """)
+    makePeperoBody(Length: length, peperoBody: peperoBody, topping: topping)
+    makeStick(stick: stick)
+    
+}
+
+func makePeperoBody(Length: Int, peperoBody: String, topping: Character) {
+    for _ in 1...Length {
+        print("\(topping)\(peperoBody)\(topping)")
     }
 }
 
-func makeStick() {
-    for _ in 1...4 {
-        print("    | |")
+func makeStick(stick: Int) {
+    for _ in 1...stick {
+        print(" | |")
     }
 }
 
-makeIceCream()
-makeStick()
+makePepero(length: 10, peperoBody: "***", topping: " ", stick: 4)
+makePepero(length: 12, peperoBody: "***", topping: "&", stick: 4)
+makePepero(length: 12, peperoBody: "***", topping: "#", stick: 6)
+makePepero(length: 6, peperoBody: "|0|", topping: " ", stick: 4)
