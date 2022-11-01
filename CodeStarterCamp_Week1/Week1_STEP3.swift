9a29e7b2f331a425c574e1e2242a56d743c0d595
kokkilE <kkuldak1@naver.com>
2022-11-01 20:12:20 +0900

    Week 1 [STEP3] 4th commit

--- CodeStarterCamp_Week1/Week1_STEP3.swift
+++ CodeStarterCamp_Week1/Week1_STEP3.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 // 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
-func drawStick(lenthOfStick: Int, shapeOfStick: String, leftBlank: Int) -> Void {
+func drawStick(lenthOfStick: Int, shapeOfStick: String, leftBlank: Int) {
     for _ in 1...lenthOfStick {
         for _ in 1...leftBlank {
             print(" ", terminator: "")
@@ -19,19 +19,19 @@ func drawStick(lenthOfStick: Int, shapeOfStick: String, leftBlank: Int) -> Void 
 }
 
 // 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
-func drawBody(shapeOfIceCream: String = "*", shapeOfTopping: String = "") -> Void {
+func drawBody(shapeOfIceCream: String = "*", shapeOfTopping: String = "") {
     print("\(shapeOfTopping)\(shapeOfIceCream)\(shapeOfTopping)")
 }
 
 // 길이(Int)에 따라 몸통을 그리는 함수
-func drawFullBody(lenthOfBody: Int, shapeOfIceCream: String, shapeOfTopping: String ) -> Void {
+func drawFullBody(lenthOfBody: Int, shapeOfIceCream: String, shapeOfTopping: String ) {
     for _ in 1...lenthOfBody {
         drawBody(shapeOfIceCream: shapeOfIceCream, shapeOfTopping: shapeOfTopping)
     }
 }
 
 // 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
-func drawIceCream(lenthOfBody: Int, shapeOfIceCream: String, shapeOfTopping: String, lenthOfStick: Int, shapeOfStick: String ) -> Void {
+func drawIceCream(lenthOfBody: Int, shapeOfIceCream: String, shapeOfTopping: String, lenthOfStick: Int, shapeOfStick: String ) {
     let leftBlank: Int = ((shapeOfTopping + shapeOfIceCream + shapeOfTopping).count - shapeOfStick.count) / 2
 
     drawFullBody(lenthOfBody: lenthOfBody, shapeOfIceCream: shapeOfIceCream, shapeOfTopping: shapeOfTopping)
