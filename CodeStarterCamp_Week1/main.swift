75149a4474f9bcbe35d80c6d3122866324c035df
whk1140 <whk1140@gmail.com>
2022-11-08 23:39:20 +0900

    Step 3 revise

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,50 +1,51 @@
 import Foundation
 
-var peperoArea = ""
-print("빼빼로의 길이를 입력해주세요",terminator:": ")
+var peperoValue = ""
+print("빼빼로의 길이를 입력해주세요", terminator:": ")
 let length = Int(readLine()!)!
-print("빼빼로의 몸통 모양을 입력해주세요",terminator: ": ")
+print("빼빼로의 몸통 모양을 입력해주세요", terminator: ": ")
 let body = readLine()!
-print("빼빼로의 토핑을 모양을 입력해주세요",terminator:": ")
+print("빼빼로의 토핑을 모양을 입력해주세요", terminator: ": ")
 let topping = readLine()!
-print("빼빼로의 막대길이를 입력해주세요",terminator:": ")
+print("빼빼로의 막대길이를 입력해주세요", terminator: ": ")
 let barLength = Int(readLine()!)!
 
-func peperoBodyTopping(toppingShape: String, bodyShape: String) {
+func choicePeperoBody(toppingShape: String, bodyShape: String) {
     if bodyShape == "|0|" || toppingShape == "" {
         print( " \(toppingShape)" + "\(bodyShape)" + "\(toppingShape)" )
-        peperoArea = " \(toppingShape)" + "\(bodyShape)" + "\(toppingShape)"
-    }else {
+        peperoValue = " \(toppingShape)" + "\(bodyShape)" + "\(toppingShape)"
+    } else {
         print( "\(toppingShape)" + "\(bodyShape)" + "\(toppingShape)" )
-        peperoArea = "\(toppingShape)" + "\(bodyShape)" + "\(toppingShape)"
+        peperoValue = "\(toppingShape)" + "\(bodyShape)" + "\(toppingShape)"
     }
 }
 
-func peperoLength() { for _ in 1...length { peperoBodyTopping(toppingShape: topping, bodyShape: body) }  }
-func peperoBarLength() {
-    for _ in 1...barLength { print( String(repeating: " ", count: peperoArea.count / 2 - 1) + "| |" ) }
+func peperoLength() {
+    for _ in 1...length {
+        choicePeperoBody(toppingShape: topping, bodyShape: body)
+    }
 }
 
-func drawPepero(){
+func peperoBarLength() {
+    for _ in 1...barLength {
+        print( String(repeating: " ", count: peperoValue.count / 2 - 1) + "| |" )
+    }
+}
+
+func peperoInfo() {
+    print("<정보>")
+    print("길이: \(length)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이: \(barLength)")
+    print("")
+}
+
+func drawPepero() {
+    peperoInfo()
+    
     peperoLength()
     peperoBarLength()
 }
 
 drawPepero()
-
-
-//import Foundation
-//
-//let iceCream: String = "***********"
-//let stick: String = "    | |"
-//
-//func drawIceCream() {
-//    for _ in 1...8 { print(iceCream) }
-//}
-//
-//func drawStick() {
-//    for _ in 1...4 { print(stick) }
-//}
-//
-//drawIceCream()
-//drawStick()
