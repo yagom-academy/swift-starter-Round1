731da1cb6cf24623ea3fb5c1892e2dc9b916266c
Dyaoss <eogus12003@naver.com>
2022-11-12 19:40:53 +0900

    step 2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+//step 2: 아이스크림 그리기
 
+func drawIceCream(body: Int){
+    print("***********")
+}
+func drawStick(stick: Int){
+    print("    | |    ")
+}
+
+for body in 0...7{
+    drawIceCream(body: body)
+}
+for stick in 0...3{
+    drawStick(stick: stick)
+}
