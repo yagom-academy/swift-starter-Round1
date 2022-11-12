80dc37ef508d41fd9aafcaf1dac1b927d596ec16
Dyaoss <eogus12003@naver.com>
2022-11-12 20:47:08 +0900

    step 3

---
+++ CodeStarterCamp_Week1/step3.swift
@@ -0,0 +1,16 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Noru on 2022/11/12.
+//
+
+import Foundation
+
+func drawBody(length: Int, body: String, topping: String){
+    for _ in 1...length{
+        print(topping,body,topping)
+    }
+}
+
+drawBody(length: <#T##Int#>, body: <#T##String#>, topping: <#T##String#>)
