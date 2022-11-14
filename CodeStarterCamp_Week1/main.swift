59b460bf23379898c47e1289584d3324ce0503c7
Dyaoss <eogus12003@naver.com>
2022-11-14 21:07:05 +0900

    step2 순서도추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,42 +10,17 @@ import Foundation
 
 //step 2: 아이스크림 그리기
 
-//func drawIceCream(body: Int){
-//    print("***********")
-//}
-//func drawStick(stick: Int){
-//    print("    | |    ")
-//}
-//
-//for body in 0...7{
-//    drawIceCream(body: body)
-//}
-//for stick in 0...3{
-//    drawStick(stick: stick)
-//}
-
-//step3: 주문에 따른 뺴빼로 그리기
-
-func drawPPR(bodyLength: Int, body: String, topping: String, stickLength: Int){
-print("""
-<정보>
-길이: \(bodyLength)
-몸통: \(body)
-토핑: \(topping)
-막대길이: \(stickLength)
-
-""")
-    
-    for _ in 1...bodyLength{
-        print(topping,body,topping, separator: "")
-    }
-    
-    for _ in 1...stickLength{
-        print(" | | ")
-    }
+func drawIceCream(body: Int){
+    print("***********")
+}
+func drawStick(stick: Int){
+    print("    | |    ")
 }
 
-
-drawPPR(bodyLength: 10, body: "***", topping: "&", stickLength: 4)
-drawPPR(bodyLength: 7, body: "***", topping: "#", stickLength: 5)
+for body in 0...7{
+    drawIceCream(body: body)
+}
+for stick in 0...3{
+    drawStick(stick: stick)
+}
 
