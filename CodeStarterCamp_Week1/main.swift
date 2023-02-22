34e1687130eba43245bcf7a21e5ce542145c6f89
MaryJo-github <jotjdal0215@gmail.com>
2023-02-23 00:29:38 +0900

    step2 icecream

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+func print_icecream (width: Int, height: Int) {
+    for _ in 1...height {           // flowchart - h
+        for _ in 1...width {        // flowchart - w
+            print("* ", terminator: "")
+        }
+        print("\n")
+    }
+}
 
+func print_bar (width: Int) {
+    for _ in 1...4 {                // flowchart - stick_h
+        for w in 1...width {
+            if (w == (width/2) || w == (width/2+2)) {
+                print("| ", terminator: "")
+            }
+            else {
+                print("  ", terminator: "")
+            }
+        }
+        print("\n")
+    }
+}
+    
+var width = 11
+var height = 8
+print_icecream(width: width, height: height)
+print_bar(width: width)
