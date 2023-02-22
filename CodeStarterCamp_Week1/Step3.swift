dc982d7dd2b89b84e888a453c66965f097a892e5
hemg2 <hemg2@naver.com>
2023-02-22 15:09:54 +0900

    네이밍 변경, 순서도 추가

--- CodeStarterCamp_Week1/Step3.swift
+++ CodeStarterCamp_Week1/Step3.swift
@@ -2,8 +2,45 @@
 //  Week1Step3.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by 1 on 2023/02/22.
+//  Created by Tom on 2023/02/22.
 //
 
 import Foundation
 
+func length(x: Int) {
+    
+}
+
+func body(y: String) {
+    
+}
+func topping(z: String) {
+    
+}
+
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 = 매개변수 기본값 ...) -> 반환타입 {
+//    /* 함수 구현부 */
+//    return 반환값
+//}
+//
+//func greeting(friend: String, me: String = "yagom") {
+//    print("Hello \(friend)! I'm \(me)")
+//}
+//
+//// 매개변수 기본값을 가지는 매개변수는 호출시 생략할 수 있습니다
+//greeting(friend: "hana") // Hello hana! I'm yagom
+//greeting(friend: "john", me: "eric") // Hello john! I'm eric
+
+
+//func 함수이름(전달인자 레이블 매개변수1이름: 매개변수1타입, 전달인자 레이블 매개변수2이름: 매개변수2타입 ...) -> 반환타입 {
+//    /* 함수 구현부 */
+//    return
+//}
+//
+//// 함수 내부에서 전달인자를 사용할 때에는 매개변수 이름을 사용합니다
+//func greeting(to friend: String, from me: String) {
+//    print("Hello \(friend)! I'm \(me)")
+//}
+//
+//// 함수를 호출할 때에는 전달인자 레이블을 사용해야 합니다
+//greeting(to: "hana", from: "yagom") // Hello hana! I'm yagom
