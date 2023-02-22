7894c81c611db1fcd0fc677d237edae4b9eae73b
Appledayz <songdongdi@naver.com>
2023-02-22 18:19:26 +0900

    Week1 [STEP2] 아이스크립 그리기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+func printIceCreamPart(width: Int = 7, height: Int = 9) {
+    for _ in 1...height {
+        for _ in 1...width {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
 
+func printIceCreamStickPart(iceCreamWidth: Int = 7, length: Int = 3) {
+    let iceCreamWidthHalf = iceCreamWidth/2
+    for _ in 1...length {
+        for _ in 1...iceCreamWidthHalf-1 {
+            print(" ", terminator: "")
+        }
+        print("|", terminator: "")
+        if iceCreamWidth%2 == 1 {
+            print(" ", terminator: "")
+        }
+        print("|")
+    }
+}
+
+var width = 5
+var height = 5
+var stickLength = 3
+
+printIceCreamPart(width: width, height: height)
+printIceCreamStickPart(iceCreamWidth: width, length: stickLength)
+
+printIceCreamPart()
+printIceCreamStickPart()
