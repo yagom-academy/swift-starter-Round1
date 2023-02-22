7d9f7825b8d655c1f727c84e3a7071009317a190
mint3382 <mskim0926@naver.com>
2023-02-22 16:50:38 +0900

    줄바꿈 고민

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+//아이스크림 부분을 그리는 함수
 func drawIceCreamPart(){
     for _ in 1...8{
         print("***********")
@@ -15,3 +16,16 @@ func drawIceCreamPart(){
 }
 
 drawIceCreamPart()
+
+//이렇게 하면 줄바꿈을 두번씩 함. -> 한번씩만 할 수 있는 방법은 없을까?
+/*
+func drawIceCreamPart(){
+    for _ in 1...8{
+        for _ in 1...11{
+            print ("*", terminator: "")
+        }
+        print ("\n")
+    }
+} */
+
+
