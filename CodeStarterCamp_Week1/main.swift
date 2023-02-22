7e016df6f74335d6820c8f0a69dffd4965971327
김형철 <hyungchul@gimhyeongcheol-ui-MacBookPro.local>
2023-02-22 16:24:34 +0900

    Step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+let bodyComponent: String = """
+*
+"""
+let barComponent: String = "    | |"
+
+
+func icecreamBody(component: String) -> String {
+    var body: String = ""
+    for _ in 1 ... 8 {
+        for _ in 1 ... 11 {
+            body += bodyComponent
+        }
+        body += "\n"
+    }
+    body.remove(at: body.index(before: body.endIndex))
+    return body
+}
+
+
+func icecreamBar(component: String)  {
+    for _ in 1 ... 4 {
+        print(component)
+    }
+}
+
+print(icecreamBody(component: bodyComponent))
+icecreamBar(component: barComponent)
+
+
+
+
+
+
+
 
