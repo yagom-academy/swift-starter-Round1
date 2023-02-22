8b4eb74fff736bce64b1ec98e1b628d53ee23ee4
Min Hyun <minnimida@gmail.com>
2023-02-22 10:59:48 +0900

    Week1 STEP 2 함수 및 다이어그램 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 
+
+
+// 한 번의 for문을 활용한 함수
+func iceHeadOne() {
+    for _ in 1...8 {
+        print(String(repeating: "*", count: 11))
+    }
+}
+
+func iceBodyOne() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+// 파라미터 전달받아서 하나의 함수로 합치기
+func iceMerge(isHead: Bool = true) -> Void {
+    let repeatCount:UInt = isHead == true ? 8 : 4
+    let repeatChar:String = isHead == true ? String(repeating: "*", count: 11) : "    | |"
+    for _ in 1...repeatCount {
+        print(repeatChar)
+    }
+}
+
+
+//iceMerge(isHead:true)
+//iceMerge(isHead:false)
