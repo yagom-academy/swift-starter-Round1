fffc54280dc45af13b569f00ff170eed54f9a829
Min Hyun <minnimida@gmail.com>
2023-02-23 11:06:22 +0900

    Week1 Step 2 수정사항 적용

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,32 +8,29 @@
 
 import Foundation
 
-//print("Hello, World!")
 
-
-
-// 한 번의 for문을 활용한 함수
-func iceHeadOne() {
+func printIceHead() {
     for _ in 1...8 {
         print(String(repeating: "*", count: 11))
     }
 }
 
-func iceBodyOne() {
+func printIceBody() {
     for _ in 1...4 {
         print("    | |")
     }
 }
 
-// 파라미터 전달받아서 하나의 함수로 합치기
-func iceMerge(isHead: Bool = true) -> Void {
-    let repeatCount:UInt = isHead == true ? 8 : 4
-    let repeatChar:String = isHead == true ? String(repeating: "*", count: 11) : "    | |"
+func printIceMerge(isHead: Bool) {
+    let repeatCount:UInt = isHead ? 8 : 4
+    let repeatChar:String = isHead ? String(repeating: "*", count: 11) : "    | |"
     for _ in 1...repeatCount {
         print(repeatChar)
     }
 }
 
 
-//iceMerge(isHead:true)
-//iceMerge(isHead:false)
+printIceHead()
+printIceBody()
+//printIceMerge(isHead:true)
+//printIceMerge(isHead:false)
