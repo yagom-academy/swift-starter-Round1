e3333d89f1193f2fc0dad0d3e790f37dcacd5b0a
Toby Kim <yechan123456@gmail.com>
2023-02-23 17:18:25 +0900

    step2 second commit

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,19 +9,18 @@
 import Foundation
 
 // 아이스크림 부분 만드는 함수
-func icecream() {
+func icecreamLength() {
     for _ in 1...8 {
         print("***********")
     }
 }
 
 // 아이스크림막대기 부분 만드는 함수
-func icecreamStick() {
+func icecreamBarLength() {
     for _ in 1...4 {
         print("    | |")
     }
 }
 
-
-icecream()
-icecreamStick()
+icecreamLength()
+icecreamBarLength()
