24c2fdc69930dc3ef5e73b1fd0b5aeec079de234
hemg2 <hemg2@naver.com>
2023-02-23 15:33:26 +0900

    빼빼로 피드백 수정

--- CodeStarterCamp_Week1/Step3.swift
+++ CodeStarterCamp_Week1/Step3.swift
@@ -15,50 +15,20 @@ func bodys(startTopping:String, body: String, lastTopping: String) {
 
 ///몸통의길이
 func snackBody(length: Int, body: () -> Void) {
-    
     for _ in 1...length {
         body()
     }
 }
-/// 막대 길이
-func stickLength(stickLength: Int, stickShape: String) {
+///막대 길이
+func stickLength(stickLength: Int) {
     for _ in 1...stickLength {
-        print(stickShape)
+        print(" | |")
     }
 }
 
 
-func result() {
-    print("--빼빼로 시작--")
-    print("길이: 10")
-    print("몸통: ***")
-    print("토핑:    ")
-    print("막대길이: 4")
-    snackBody(length: 10, body: {bodys(startTopping: "", body: " ***", lastTopping: "")})
-    stickLength(stickLength: 4, stickShape: " | |")
-    print("  ")
-    print("-- & 빼빼로 시작--")
-    print("길이: 12")
-    print("몸통: ***")
-    print("토핑:  &")
-    print("막대길이: 4")
-    snackBody(length: 12, body: {bodys(startTopping: "&", body: "***", lastTopping: "&")})
-    stickLength(stickLength: 4, stickShape: " | |")
-    print("  ")
-    print("-- # 빼빼로 시작--")
-    print("길이: 12")
-    print("몸통: ***")
-    print("토핑:  #")
-    print("막대길이: 6")
-    snackBody(length: 12, body: {bodys(startTopping: "#", body: "***", lastTopping: "#")})
-    stickLength(stickLength: 6, stickShape: " | |")
-    print("  ")
-    print("--누드 빼빼로 시작--")
-    print("길이: 6")
-    print("몸통: |0|")
-    print("토핑:  ")
-    print("막대길이: 4")
-    snackBody(length: 6, body: {bodys(startTopping: "", body: " |0|", lastTopping: "")})
-    stickLength(stickLength: 4, stickShape: " | |")
+func result(바길이: Int, 몸통: String, 토핑: String, 몸통길이: Int) {
+    print("<정보> \n몸통길이: \(몸통길이) \n몸통: \(몸통) \n토핑:\(토핑) \n바길이: \(바길이)")
+    snackBody(length: 몸통길이, body: {bodys(startTopping: 토핑, body: 몸통, lastTopping: 토핑)})
+    stickLength(stickLength: 바길이)
 }
-
