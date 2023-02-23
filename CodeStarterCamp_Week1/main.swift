91cbeedb3ab8c19e99e3c0008943530bef40a504
Tag <sea15510@gmail.com>
2023-02-23 13:32:49 +0900

    CodeStarterCamp_Week1 파일 Step2 작성 완료

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,8 +5,47 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+//  CodeStarterCamp_Week1_tag
 
 import Foundation
 
 print("Hello, World!")
 
+func icecream() {
+    print("***********")
+}
+
+func stick() {
+    print("    | |")
+}
+
+for _ in 1...8 {
+    icecream()
+}
+
+for _ in 1...4 {
+    stick()
+}
+
+//var icecreamwidth = 11
+//var icecreamlength = 8
+////var stickwidth = 1 *막대기 사이의 공간도 조절할 수 있을까?
+//var sticklength = 4
+//
+//func icecream() {
+//    for _ in 1...icecreamlength {
+//        for _ in 1...icecreamwidth {
+//            print("*", terminator: "")
+//        }
+//        print("")
+//    }
+//}
+//
+//func stick() {
+//    for _ in 1...sticklength {
+//        print("    | |")
+//        }
+//    }
+//
+//icecream()
+//stick()
