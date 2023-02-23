66c264357d7d132ced7965d0bc9e16aca88f4f17
mint3382 <mskim0926@naver.com>
2023-02-23 11:09:09 +0900

    코드 가이드라인에 맞게 수정,불필요한 주석 삭제

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,37 +8,22 @@
 
 import Foundation
 
-//아이스크림 부분을 그리는 함수
-/*
-func drawIceCreamPart(){
-    for _ in 1...8{
-        print("***********")
-    }
-}
- */
 
-drawIceCreamPart()
-
-//이렇게 하면 줄바꿈을 두번씩 함. -> 한번씩만 할 수 있는 방법은 없을까?
-
-func drawIceCreamPart(){
+func drawIceCreamPart() {
     for _ in 1...8{
         for _ in 1...11{
-            print ("*", terminator: "")
+            print("*", terminator: "")
         }
         print ()
     }
 }
 
-//막대기 부분을 그리는 함수
+drawIceCreamPart()
 
-func drawBarPart(){
+func drawBarPart() {
     for _ in 1...4{
-        print ("    | |    ")
+        print("    | |    ")
     }
 }
-
-//막대가 언제나 가운데에 올 수 있도록 해보자!
-//변수 선언을 하나 하고 (변수 / 2) - 1 과 (변수 / 2) + 1에 막대를 그려 넣으면 되지 않을까.
 
 drawBarPart()
