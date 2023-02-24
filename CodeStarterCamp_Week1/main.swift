2f596449e7f09067333a8827567199e8f63ce378
yagom <jin@seohyejin-ui-MacBookAir.local>
2023-02-25 00:23:39 +0900

    아이스크림 만들기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeSweet(x: String) {
+    var count = 1
+    while count <= 10 {
+        print(x, terminator: "")
+        count += 1
+    }
+    print(x)
+}
+
+func makeBar(x: String) {
+    print("    \(x) \(x)    ")
+}
+
+var line = 1
+while line <= 8 {
+    makeSweet(x: "*")
+    line += 1
+}
+
+line = 1
+while line <= 4 {
+    makeBar(x: "|")
+    line += 1
+}
 
