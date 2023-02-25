4cb8c5b19af38fea3368d9913cd6a4048c682a4b
Tag <sea15510@gmail.com>
2023-02-25 20:34:35 +0900

    wekk1_step3 v1.1 작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,27 +7,49 @@
 
 import Foundation
 
-func inputLenght() {
-    print("길이: ", terminator: "")
-    let lenght = Int(readLine()!)!
+var bodyAndTopping: String = ""
+
+func makeBody(body: String, topping: String) {
+    return bodyAndTopping = "\(topping)\(body)\(topping)"
 }
 
-func inputBody() {
-    print("몸통: ", terminator: "")
-    let body = readLine()!
+func makeAllBody(bodyLenght: Int, bodyAndTopping: String) {
+    for _ in 1...lenght {
+        if bodyAndTopping.count > 3 {
+            print(bodyAndTopping)
+        } else {
+            print(" " + bodyAndTopping)
+        }
+    }
 }
 
-func inputTopping() {
-    print("토핑: ", terminator: "")
-    let topping = readLine()!
+func makeStick(stickLenght: Int) {
+    for _ in 1...stickLenght {
+        print(" | |")
+    }
 }
 
-func inputStickLenght() {
-    print("막대길이: ", terminator: "")
-    let stickLenght = Int(readLine()!)!
+func makePepero(lenght: Int, body: String, topping: String, stickLenght: Int) {
+    makeBody(body: body, topping: topping)
+    makeAllBody(bodyLenght: lenght, bodyAndTopping: bodyAndTopping)
+    makeStick(stickLenght: stickLenght)
 }
 
 print("<정보>")
+print("길이: ", terminator: "")
+let lenght = Int(readLine()!)!
+print("몸통: ", terminator: "")
+let body = readLine()!
+print("토핑: ", terminator: "")
+let topping = readLine()!
+print("막대길이: ", terminator: "")
+let stickLenght = Int(readLine()!)!
+print("")
+makePepero(lenght: lenght, body: body, topping: topping, stickLenght: stickLenght)
+
+//makeBody(body: body, topping: topping)
+//makeAllBody(bodyLenght: lenght, bodyAndTopping: bodyAndTopping)
+//makeStick(stickLenght: stickLenght)
 
 //print(type(of:lenght))
 //print(type(of:body))
