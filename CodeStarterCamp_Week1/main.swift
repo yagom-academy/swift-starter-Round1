042afb762cf39b3a648f6380bac2789e3a371175
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-02-26 14:19:50 +0900

    iceCream

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+let cream: String = "*"
+let stick: String = "    | |    "
+var countCream: Int = 1
+var countBar: Int = 1
+var count: Int = 1
 
+func iceCream(cream: String){
+    for countCream in 1...11{
+        print("\(cream)", terminator: "")
+    }
+    print("")
+}
+
+func bar(stick: String){
+    for countBar in 1...4{
+        print(stick)
+    }
+}
+
+for count in 1...8{
+    iceCream(cream: cream)
+}
+
+bar(stick: stick)
