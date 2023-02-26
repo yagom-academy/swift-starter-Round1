9957586ca6c9aeac6fcf106bbf91eb4dd76da45d
ny <nayeon6868@gmail.com>
2023-02-26 19:14:22 +0900

    swift-starter-week1

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeIceCream (iceWidth: Int, iceHeight: Int, bar: Int) {
+    for _ in 1...iceHeight {
+        for _ in 1...iceWidth {
+            print("*", terminator: "")
+        }
+        print()
+    }
+    
+    let numb : Int = (iceWidth-(iceWidth%2))/2-1
+    
+    for _ in 1...bar {
+        for _ in 1...numb {
+            print(" ", terminator: "")
+            }
+            print("| |")
+        }
+}
+
+makeIceCream(iceWidth: 11, iceHeight: 9, bar: 4)
 
