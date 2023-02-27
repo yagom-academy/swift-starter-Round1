ddfdc2ab173130c34c4a40d0b4acc23a29316835
a-jb97 <a_jb97@naver.com>
2023-02-27 23:08:45 +0900

    Step3 1차 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,37 +7,36 @@
 
 import Foundation
 
-func writeStick(stickLength: Int) {
+func drawStick(stickLength: Int) {
     for _ in 1...stickLength {
         print(" | |")
     }
 }
 
-func writeBodyTopping(bodyShape: String, toppingShape: String) {
-    print(toppingShape, terminator: "")
-    print(bodyShape, terminator: "")
-    print(toppingShape)
+func drawBodyTopping(bodyShape: String, toppingShape: String) {
+    print("\(toppingShape)\(bodyShape)\(toppingShape)")
 }
 
-func lengthBodyTopping(bodyLength: Int, bodyShape: String, toppingShape: String) {
+func drawLengthBodyTopping(bodyLength: Int, bodyShape: String, toppingShape: String) {
     for _ in 1...bodyLength {
-        writeBodyTopping(bodyShape: bodyShape, toppingShape: toppingShape)
+        drawBodyTopping(bodyShape: bodyShape, toppingShape: toppingShape)
     }
 }
 
-func writePPeppero(bodyLength: Int, bodyShape: String, toppingShape: String, stickLength: Int) {
-    print("<정보>")
-    print("길이: \(bodyLength)")
-    print("몸통: \(bodyShape)")
-    print("토핑: \(toppingShape)")
-    print("막대길이: \(stickLength)\n")
-    lengthBodyTopping(bodyLength: bodyLength, bodyShape: bodyShape, toppingShape: toppingShape)
-    writeStick(stickLength: stickLength)
-    print("\n")
+func writePpeppero(bodyLength: Int, bodyShape: String, toppingShape: String, stickLength: Int) {
+    print("""
+          <정보>
+          길이: \(bodyLength)
+          몸통: \(bodyShape)
+          토핑: \(toppingShape)
+          막대길이: \(stickLength)\n
+          """)
+    drawLengthBodyTopping(bodyLength: bodyLength, bodyShape: bodyShape, toppingShape: toppingShape)
+    drawStick(stickLength: stickLength)
 }
 
-writePPeppero(bodyLength: 10, bodyShape: "***", toppingShape: " ", stickLength: 4)
-writePPeppero(bodyLength: 12, bodyShape: "***", toppingShape: "&", stickLength: 4)
-writePPeppero(bodyLength: 12, bodyShape: "***", toppingShape: "#", stickLength: 6)
-writePPeppero(bodyLength: 6, bodyShape: "|0|", toppingShape: " ", stickLength: 4)
-writePPeppero(bodyLength: 10, bodyShape: "$$$", toppingShape: "#", stickLength: 3)
+writePpeppero(bodyLength: 10, bodyShape: "***", toppingShape: " ", stickLength: 4)
+writePpeppero(bodyLength: 12, bodyShape: "***", toppingShape: "&", stickLength: 4)
+writePpeppero(bodyLength: 12, bodyShape: "***", toppingShape: "#", stickLength: 6)
+writePpeppero(bodyLength: 6, bodyShape: "|0|", toppingShape: " ", stickLength: 4)
+writePpeppero(bodyLength: 10, bodyShape: "$$$", toppingShape: "#", stickLength: 3)
