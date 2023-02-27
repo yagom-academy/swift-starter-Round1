af3078c805bb46dfde0678ee3a6f5b979be92b01
강도훈 <donishoon@gangdohun-ui-MacBookAir.local>
2023-02-27 23:27:07 +0900

    STEP2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,3 +10,24 @@ import Foundation
 
 print("Hello, World!")
 
+//for icecream in 1...8{
+//    print( "***********")
+//}
+//for stick in 1...4{
+//    print("    | |")
+//}
+
+func makeIcecream(size: Int) {
+    for icecream in 1...10{
+        print( "***********")
+    }
+}
+makeIcecream(size: 1)
+// 사이즈를 입력하면 원하는 만큼 반복해주는 함수를 만들려면 어떻게 해야할까요? ㅠ
+
+func makeStick(size: Int) {
+    for stick in 1...4{
+        print("    | |")
+    }
+}
+makeStick(size: 1)
