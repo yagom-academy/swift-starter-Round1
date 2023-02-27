334cf8f8ad28b10aacdf6d537283858f88adaff3
허준하 <heojunha@172.20.nate.com>
2023-02-28 00:19:59 +0900

    아이스크림,막대 넓이 길이가 가각 정해져있을때 코드

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+func ice (length : Int , wide : Int){
+    for lengthTime in 1...length{
+        for wideTime in 1...wide{
+            print("*",terminator: "")
+        }
+    print("")       // 한 줄로 이어지는걸 방지
+    }
+}
+func iceBar (barLength : Int) {
+    for barLengthTime in 1...barLength {
+        print("    | |    ")
+    }
+}
 
+ice(length:8,wide:11)
+iceBar(barLength:4)
