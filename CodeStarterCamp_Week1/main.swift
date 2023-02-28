8cfbbad2ecc368b0bd63f57ff016c92d53214dd7
dduddupark <develop.dduddu@gmail.com>
2023-02-28 13:27:21 +0900

    step3 빼빼로 만들기 13:27

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -11,6 +11,10 @@ import Foundation
 /**
  Step 2: 아이스크림 그리기
  */
-createIceCream(width: 12, bodyHeight: 8, barHeight: 4)
+//createIceCream(width: 12, bodyHeight: 8, barHeight: 4)
 
 
+/**
+ Step 3: 주문에 따른 빼빼로 그리기
+ */
+createPepero(bodyHeight: 6, body: "|0|", toping: "", barHeight: 4)
--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -6,3 +6,64 @@
 //
 
 import Foundation
+
+/**
+ 빼빼로 만들기
+ 
+ bodyHeight : 빼빼로 몸통 길이
+ body : 빼빼로 몸통
+ toping : 빼빼로 토핑
+ barHeight : 빼빼로 막대 길이
+ */
+func createPepero(bodyHeight: Int, body: String, toping: String?, barHeight: Int) {
+    createPeperoBody(bodyHeight: bodyHeight, body: body, toping: toping)
+    createPeperoBar(barHeight: barHeight, bodyWidth: body.count, topingWidth: toping?.count ?? 0)
+}
+        
+/**
+ 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
+ */
+private func createPeperoBar(barHeight: Int, bodyWidth: Int, topingWidth: Int) {
+    for _ in 1...barHeight {
+        if (topingWidth > 0) {  //빼빼로 토핑이 있을때만 실행
+            for _ in 1...topingWidth {
+                print(" ", terminator: "")
+            }
+        }
+        
+        let half = bodyWidth/2  //너비를 반으로 갈라서 중심을 찾습니다.
+        
+        for index in 1...bodyWidth {
+            if index == half || index == half+2 {   //중심에 선 하나 중심 옆옆에 선 하나를 그어 빼빼로 바 모양을 만듭니다.
+                print("|", terminator: "")
+            } else {
+                if half%2==0 && index == half+1{
+                   print("", terminator: "")
+                } else {
+                    print(" ", terminator: "")
+                }
+            }
+        }
+    
+        print()
+    }
+}
+
+/**
+ 길이(Int)에 따라 몸통을 그리는 함수
+ */
+private func createPeperoBody(bodyHeight: Int, body: String, toping: String?) {
+    for _ in 0...bodyHeight {
+        createPeperoBodyItem(body: body, toping: toping)
+        print()
+    }
+}
+
+/**
+ 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+ */
+private func createPeperoBodyItem(body: String, toping: String?) {
+    print(toping ?? "", terminator: "")
+    print(body, terminator: "")
+    print(toping ?? "", terminator: "")
+}
