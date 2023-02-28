04e07ed37de50cc605fbe683f98c316c7745ded7
Appledayz <songdongdi@naver.com>
2023-02-28 11:47:24 +0900

    step3 수정 3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-/*
- STEP 2. 아이스크림 그리기
- */
 //var width = 5
 //var height = 5
 //var stickLength = 3
@@ -18,9 +15,6 @@ import Foundation
 //printIceCreamPart(width: width, height: height)
 //printIceCreamStickPart(iceCreamWidth: width, length: stickLength)
 
-/*
- STEP 3. 주문에 따른 빼빼로 그리기
- */
 printPepero(bodyLength: 10, body: "***", topping: "", stickLength: 4)
 printPepero(bodyLength: 12, body: "***", topping: "&", stickLength: 4)
 printPepero(bodyLength: 10, body: "***", topping: "#", stickLength: 6)
