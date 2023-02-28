efc9c375357e0f87410a4845183ddc45fc8c8531
Hoon94 <dleognsdl1@naver.com>
2023-02-28 16:48:55 +0900

    PR 리뷰 수정

--- CodeStarterCamp_Week1/Step3.swift
+++ CodeStarterCamp_Week1/Step3.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-// 입력받은 몸통과 토핑을 합쳐서 만든 몸통을 저장하는 변수
-var body: String = String()
-// 합친 정보를 담는 변수
-var inputData: String = String()
-
 func drawStick(length: Int) {
     // length 만큼 반복하여 막대를 그림
     for _ in 1...length {
@@ -20,33 +15,31 @@ func drawStick(length: Int) {
 }
 
 // 몸통, 토핑을 매개변수로 받아 토핑과 몸통을 그리는 함수
-func drawBody(inner: String, topping: String) {
-    body = topping + inner + topping
+func drawBody(inner: String, topping: String) -> String {
+    return topping + inner + topping
 }
 
-func drawPepero(length: Int) {
+func drawPepero(length: Int, body: String) {
     for _ in 1...length {
         print(body)
     }
 }
 
 // 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
-func makeFinishedPepero(peperoLength: Int, inner: String, stickLength: Int, topping: String = " ") {
-    // 정보(길이, 몸통, 토핑, 막대길이)를 저장
-    inputData = """
-                <정보>
-                길이: \(peperoLength)
-                몸통: \(inner)
-                토핑: \(topping)
-                막대길이: \(stickLength)
+func makePepero(peperoLength: Int, inner: String, stickLength: Int, topping: String = " ") {
+    // 정보(길이, 몸통, 토핑, 막대길이)를 출력
+    print("""
+        <정보>
+        길이: \(peperoLength)
+        몸통: \(inner)
+        토핑: \(topping)
+        막대길이: \(stickLength)
 
-                """
-    
-    // 정보 출력
-    print(inputData)
+        """
+    )
     
     // 파트별로 구분한 함수 실행
-    drawBody(inner: inner, topping: topping)
-    drawPepero(length: peperoLength)
+    let body = drawBody(inner: inner, topping: topping)
+    drawPepero(length: peperoLength, body: body)
     drawStick(length: stickLength)
 }
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -14,10 +14,10 @@ import Foundation
 
 // Step3: 주문에 따른 빼빼로 그리기
 // 예제 1
-makeFinishedPepero(peperoLength: 10, inner: "***", stickLength: 4)
+makePepero(peperoLength: 10, inner: "***", stickLength: 4)
 // 예제 2
-makeFinishedPepero(peperoLength: 12, inner: "***", stickLength: 4, topping: "&")
+makePepero(peperoLength: 12, inner: "***", stickLength: 4, topping: "&")
 // 예제 3
-makeFinishedPepero(peperoLength: 12, inner: "***", stickLength: 6, topping: "#")
+makePepero(peperoLength: 12, inner: "***", stickLength: 6, topping: "#")
 // 예제 4
-makeFinishedPepero(peperoLength: 6, inner: "|0|", stickLength: 4, topping: " ")
+makePepero(peperoLength: 6, inner: "|0|", stickLength: 4, topping: " ")
