17afdfe8d2b373de3d4d37d3a95344233df08d14
ysj <yyssjj9999@gmail.com>
2023-02-28 16:52:29 +0900

    step3수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,14 +8,19 @@
 
 import Foundation
 
-
 //step3
 
-func printPepero(lenght: Int, body: String, topping: String, stickLenght: Int) {
-    
+func makeBodyTopping(body: String, topping: String) {
+    print(" " + topping + body + topping)
+}
+
+func printBodyPart(body: String, topping: String, lenght: Int) {
     for _ in 1...lenght {
-        print(" " + topping + body + topping)
+        makeBodyTopping(body: body, topping: topping)
     }
+}
+
+func printStickPart(stickLenght: Int) {
     for _ in 1...stickLenght {
         print("  " + "| |")
     }
@@ -33,15 +38,14 @@ func printPeperoInformation(lenght: Int, body: String, topping: String, stickLen
     """)
 }
 
-//실행 예시1
-printPeperoInformation(lenght: 10, body: "***", topping: " ", stickLenght: 4)
+func printPepero(lenght: Int, body: String, topping: String, stickLenght: Int) {
+    printPeperoInformation(lenght: lenght, body: body, topping: topping, stickLenght: stickLenght)
+    printBodyPart(body: body, topping: topping, lenght: lenght)
+    printStickPart(stickLenght: stickLenght)
+}
+
 printPepero(lenght: 10, body: "***", topping: " ", stickLenght: 4)
-//실행 예시2
-printPeperoInformation(lenght: 12, body: "***", topping: "&", stickLenght: 4)
 printPepero(lenght: 12, body: "***", topping: "&", stickLenght: 4)
-//실행 예시3
-printPeperoInformation(lenght: 12, body: "***", topping: "#", stickLenght: 6)
 printPepero(lenght: 12, body: "***", topping: "#", stickLenght: 6)
-//실행 예시4
-printPeperoInformation(lenght: 6, body: "|0|", topping: " ", stickLenght: 4)
 printPepero(lenght: 6, body: "|0|", topping: " ", stickLenght: 4)
+
