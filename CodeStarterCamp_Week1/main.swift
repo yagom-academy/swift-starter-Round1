4f1906c6d5c7264a0f5d8b1f75e76132dab4cd84
yagom <jin@seohyejin-ui-MacBookAir.local>
2023-02-28 22:28:18 +0900

    Week1 [STEP2] Whales

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,59 +7,6 @@
 //
 
 import Foundation
-/*
-func makeSweet(x: String) {
-    var count = 1
-    while count <= 10 {
-        print(x, terminator: "")
-        count += 1
-    }
-    print(x)
-}
-
-func makeBar(x: String) {
-    print("    \(x) \(x)    ")
-}
-
-var line = 1
-while line <= 8 {
-    makeSweet(x: "*")
-    line += 1
-}
-
-line = 1
-while line <= 4 {
-    makeBar(x: "|")
-    line += 1
-}
-
-
-// 처음엔 아이스크림 합치는 것도 while문으로 했는데 for문으로 할 수 있겠구나 싶어서 수정했습니다!!
-
-for _ in 1...8 {
-    makeSweet(x: "*")
-}
-for _ in 1...4 {
-    makeBar(x: "|")
-}
-
-// for문으로 훨씬 깔끔하게 수정되는 것을 보고 전체를 하나의 함수로 수정해보고싶어졌습니다.
-func makeIcecream(a: String, b: String) {
-    for _ in 1...8 {
-        for _ in 1...10 {
-            print(a, terminator: "")
-        }
-        print(a)
-    }
-    for _ in 1...4 {
-        print("    \(b) \(b)    ")
-    }
-    print("Good, Yummy")
-}
-
-makeIcecream(a: "*", b: "|")
-// 훨씬 깔끔해져서 아주아주 마음에 들었습니다!!
-*/
 
 // PR 받고 수정하기
 func makeIcecreamBody(kind: String) {
@@ -77,10 +24,3 @@ func makeBar(bar: String) {
 }
 makeIcecreamBody(kind: "*")
 makeBar(bar: "|")
-
-print("STEP3")
-makePaparo(lengthBody: 10, lengthBar: 4) //실행 예시 1
-makePaparo(lengthBody: 12, lengthBar: 4, toping: "&") //실행 예시 2
-makePaparo(lengthBody: 12, lengthBar: 6, toping: "#") //실행 예시 3
-makePaparo(lengthBody: 6, lengthBar: 4, body: "|0|") //실행 예시 4
-makePaparo(lengthBody: 12, lengthBar: 6, toping: "&#", body: "*") //나만의 빼빼로
