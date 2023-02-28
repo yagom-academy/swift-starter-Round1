f23f1d069380c88b35398989ab803dfab012550b
Hoon94 <dleognsdl1@naver.com>
2023-02-28 20:09:05 +0900

    PR 리뷰: 주석 정리

--- CodeStarterCamp_Week1/Step3.swift
+++ CodeStarterCamp_Week1/Step3.swift
@@ -8,13 +8,11 @@
 import Foundation
 
 func drawStick(length: Int) {
-    // length 만큼 반복하여 막대를 그림
     for _ in 1...length {
         print(" | |")
     }
 }
 
-// 몸통, 토핑을 매개변수로 받아 토핑과 몸통을 그리는 함수
 func drawBody(inner: String, topping: String) -> String {
     return topping + inner + topping
 }
@@ -25,20 +23,18 @@ func drawPepero(length: Int, body: String) {
     }
 }
 
-// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
 func makePepero(peperoLength: Int, inner: String, stickLength: Int, topping: String = " ") {
-    // 정보(길이, 몸통, 토핑, 막대길이)를 출력
-    print("""
-        <정보>
-        길이: \(peperoLength)
-        몸통: \(inner)
-        토핑: \(topping)
-        막대길이: \(stickLength)
+    let inputData: String = """
+                            <정보>
+                            길이: \(peperoLength)
+                            몸통: \(inner)
+                            토핑: \(topping)
+                            막대길이: \(stickLength)
 
-        """
-    )
+                            """
     
-    // 파트별로 구분한 함수 실행
+    print(inputData)
+    
     let body = drawBody(inner: inner, topping: topping)
     drawPepero(length: peperoLength, body: body)
     drawStick(length: stickLength)
