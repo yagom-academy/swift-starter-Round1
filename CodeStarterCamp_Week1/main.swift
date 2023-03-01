91bb3f431c53b09fb35cd25e9f1e89549467090a
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-03-01 22:52:41 +0900

    ppaeppaero 1st

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,33 +8,33 @@
 
 import Foundation
 
-let cream: String = "*"
-let stick: String = "    | |    "
+//let cream: String = "*"
+//let stick: String = "    | |    "
+//
+//func drawIcecream(cream: String){
+//    for _ in 1...11{
+//        print("\(cream)", terminator: "")
+//    }
+//    print("")
+//}
+//
+//func drawStick(stick: String){
+//    for _ in 1...4{
+//        print(stick)
+//    }
+//}
+//
+//func drawBodyLength(){
+//    for _ in 1...8{
+//        drawIcecream(cream: cream)
+//    }
+//}
 
-func drawIcecream(cream: String){
-    for _ in 1...11{
-        print("\(cream)", terminator: "")
-    }
-    print("")
-}
-
-func drawStick(stick: String){
-    for _ in 1...4{
-        print(stick)
-    }
-}
-
-func drawBodyLength(){
-    for _ in 1...8{
-        drawIcecream(cream: cream)
-    }
-}
-
-drawBodyLength()
-drawStick(stick: stick)
+//drawBodyLength()
+//drawStick(stick: stick)
 
 //step3 함수호출 영역
-//drawPpaeppaero(body: "***", topping: " ", length: 10, stickLength: 4)
-//drawPpaeppaero(body: "***", topping: "&", length: 12, stickLength: 4)
-//drawPpaeppaero(body: "***", topping: "#", length: 12, stickLength: 6)
-//drawPpaeppaero(body: "|0|", topping: " ", length: 6, stickLength: 4)
+drawPpaeppaero(body: "***", topping: " ", length: 10, stickLength: 4)
+drawPpaeppaero(body: "***", topping: "&", length: 12, stickLength: 4)
+drawPpaeppaero(body: "***", topping: "#", length: 12, stickLength: 6)
+drawPpaeppaero(body: "|0|", topping: " ", length: 6, stickLength: 4)
