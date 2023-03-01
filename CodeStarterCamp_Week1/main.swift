1af1861c1ee8b2686401dba19278ce774f5c8c2a
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-03-01 16:20:09 +0900

    icream modify

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,25 +10,31 @@ import Foundation
 
 let cream: String = "*"
 let stick: String = "    | |    "
-var countCream: Int = 1
-var countBar: Int = 1
-var count: Int = 1
 
-func iceCream(cream: String){
-    for countCream in 1...11{
+func drawIcecream(cream: String){
+    for _ in 1...11{
         print("\(cream)", terminator: "")
     }
     print("")
 }
 
-func bar(stick: String){
-    for countBar in 1...4{
+func drawStick(stick: String){
+    for _ in 1...4{
         print(stick)
     }
 }
 
-for count in 1...8{
-    iceCream(cream: cream)
+func drawBodyLength(){
+    for _ in 1...8{
+        drawIcecream(cream: cream)
+    }
 }
 
-bar(stick: stick)
+drawBodyLength()
+drawStick(stick: stick)
+
+//step3 함수호출 영역
+//drawPpaeppaero(body: "***", topping: " ", length: 10, stickLength: 4)
+//drawPpaeppaero(body: "***", topping: "&", length: 12, stickLength: 4)
+//drawPpaeppaero(body: "***", topping: "#", length: 12, stickLength: 6)
+//drawPpaeppaero(body: "|0|", topping: " ", length: 6, stickLength: 4)
