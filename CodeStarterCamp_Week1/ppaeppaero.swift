0b6e247ef1ddeeb53231211c2c7a7c857aae21c1
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-03-02 10:25:32 +0900

    week1 step3 recommit

--- CodeStarterCamp_Week1/ppaeppaero.swift
+++ CodeStarterCamp_Week1/ppaeppaero.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+
+func stickDraw(sLength: Int){
+    for _ in 1...sLength{
+        print(" | | ")
+    }
+}
+
+func bodyForm(body: String, topping: String) -> String{
+    return topping+body+topping
+}
+
+func bodyDraw(bLength: Int, body: String, stick: Int){
+    for _ in 1...bLength{
+        print(body)
+    }
+    stickDraw(sLength: stick)
+}
+
+func drawPpaeppaero(body: String, topping: String, length: Int, stickLength: Int) {
+    
+    print("""
+        <정보>
+        길이: \(length)
+        몸통: \(body)
+        토핑: \(topping)
+        막대길이: \(stickLength)
+        """)
+    let bodyD:String = bodyForm(body: body, topping: topping)
+    bodyDraw(bLength: length, body: bodyD, stick: stickLength)
+}
+
