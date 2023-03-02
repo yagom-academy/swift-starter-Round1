0c696893ea083b2edd5995fb9ec03a64fff4e29c
dayoung <celeste7297@naver.com>
2023-03-02 19:56:57 +0900

    main.swift

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,33 +8,44 @@
 
 import Foundation
 
-// Step 2 (modified)
+// Step 3 (1)
+func makeBar(barlength: Int) {
+    for _ in 1...barlength {
+        print(" | |")}}
 
-func makeIcecreamBody(bodywidth: Int, bodyheight: Int) {
-    for _ in 1...bodyheight {
-        for _ in 1...bodywidth {
-            print("*", terminator: "")
-        }
-        print()
-    }
+func makeBodyRow(bodyshape: String, topping: String) -> String {
+    return topping + bodyshape + topping
 }
 
-func makeIcecreamBar(bodywidth: Int, barheight: Int) {
-    for _ in 1...barheight {
-        if bodywidth % 2 != 0 {
-            for _ in 1...((bodywidth + 1) / 2) - 2 {
-                print(" ", terminator: "")
-            }
-            print("| |")
-        }
-        if bodywidth % 2 == 0 {
-            for _ in 1...(bodywidth / 2) - 1 {
-                print(" ", terminator: "")
-            }
-            print("||")
-        }
-    }
+var bodyRow: (String, String) -> String = makeBodyRow(bodyshape:topping:)
+
+func makeBodyCol(bodylength: Int, bodyrow: Any) {
+    for _ in  1...bodylength {
+            print(bodyrow)
+}}
+
+func makePePero(bodylength: Int, bodyshape: String, topping: String, barlength: Int) {
+    print("<정보>\n길이: \(bodylength)\n몸통: \(bodyshape)\n토핑: \(topping)\n막대길이: \(barlength)\n")
+        makeBodyCol(bodylength: bodylength, bodyrow: bodyRow(bodyshape, topping))
+        makeBar(barlength: barlength)
 }
 
-makeIcecreamBody(bodywidth: 11, bodyheight: 8)
-makeIcecreamBar(bodywidth: 11, barheight: 4)
+
+
+//Step 3 (2)
+//func makePePero2(bodyshape: String, bodylength: Int, topping: String, barlength: Int) {
+//    print("<정보>\n길이: \(bodylength)\n몸통: \(bodyshape)\n토핑: \(topping)\n막대길이: \(barlength)\n")
+//    for _ in  1...bodylength {
+//        print(topping + bodyshape + topping)}
+//    for _ in 1...barlength {
+//        print(" | |")
+//    }
+//}
+
+
+makePePero(bodylength: 10, bodyshape: " ***", topping: "", barlength: 4)
+makePePero(bodylength: 12, bodyshape: "***", topping: "&", barlength: 4)
+makePePero(bodylength: 12, bodyshape: "***", topping: "#", barlength: 6)
+makePePero(bodylength: 6, bodyshape: " |0|", topping: "", barlength: 6)
+
+
