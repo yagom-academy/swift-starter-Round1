5dcfd39137cebac8660a03953177360502aff457
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-03-02 10:20:16 +0900

    week1 step2 delete

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,31 +8,6 @@
 
 import Foundation
 
-//let cream: String = "*"
-//let stick: String = "    | |    "
-//
-//func drawIcecream(cream: String){
-//    for _ in 1...11{
-//        print("\(cream)", terminator: "")
-//    }
-//    print("")
-//}
-//
-//func drawStick(stick: String){
-//    for _ in 1...4{
-//        print(stick)
-//    }
-//}
-//
-//func drawBodyLength(){
-//    for _ in 1...8{
-//        drawIcecream(cream: cream)
-//    }
-//}
-
-//drawBodyLength()
-//drawStick(stick: stick)
-
 //step3 함수호출 영역
 drawPpaeppaero(body: "***", topping: " ", length: 10, stickLength: 4)
 drawPpaeppaero(body: "***", topping: "&", length: 12, stickLength: 4)
