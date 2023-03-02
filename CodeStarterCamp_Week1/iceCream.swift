d63b4b52d366e378dd7e373d57fd279c58f4a01f
Im-loki <al4126@naver.com>
2023-03-02 14:24:09 +0900

    step2 수정

--- CodeStarterCamp_Week1/iceCream.swift
+++ CodeStarterCamp_Week1/iceCream.swift
@@ -1,12 +1,12 @@
-for _ in 1...8{
-    for _ in 1...11{
-        print("*", terminator: "")
-    }
-    print("")
+func makeIceCream () {
+    print("***********")
 }
-for _ in 1...4{
-    for _ in 1...4{
-        print(" ", terminator: "")
-    }
-    print("| |")
+for _ in 1...8 {
+    makeIceCream()
+}
+func makeBar () {
+    print("    | |")
+}
+for bar in 1...4 {
+    makeBar()
 }
