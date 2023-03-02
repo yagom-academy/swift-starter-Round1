4d8ab5d04eae6b8f43b86afc3c3caf6096d3aa5e
j1nt0 <sk2018univ@naver.com>
2023-03-03 00:05:51 +0900

    step 3.1

---
+++ Step3/Step3/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Step3
+//
+//  Created by 이진 on 2023/03/02.
+//
+
+import Foundation
+
+func drawBbabbaroPartBody(content: String, topping: String = " ") {
+    print("\(topping)\(content)\(topping)")
+}
+
+func drawBbabbaroFullBody(length: Int, content: String, topping: String = " ") {
+    for _ in 1...length {
+        drawBbabbaroPartBody(content: content, topping: topping)
+    }
+}
+
+func drawBbabbaroBar(length: Int) {
+    for _ in 1...length {
+        print(" | |")
+    }
+}
+
+func makeBbabbaro(lengthBody: Int, lengthBar: Int, content: String, topping: String=" ") {
+    drawBbabbaroFullBody(length: lengthBody, content: content, topping: topping)
+    drawBbabbaroBar(length: lengthBar)
+}
+
+makeBbabbaro(lengthBody: 10, lengthBar: 4, content: "***", topping: "$")
