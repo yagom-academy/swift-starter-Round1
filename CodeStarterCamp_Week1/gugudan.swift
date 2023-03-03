cc965475d66b4bb5b425f15a5a6fd54e46fcaa21
Im-loki <al4126@naver.com>
2023-03-03 17:08:23 +0900

    icecream

--- CodeStarterCamp_Week1/gugudan.swift
+++
@@ -1,9 +0,0 @@
-func gugu(dan: Int) {
-    for count in 1...9 {
-        print("\(dan) * \(count) = \(dan * count)")
-    }
-}
-
-gugu(dan: 6)
-
-
---
+++ CodeStarterCamp_Week1/함수고급.swift
@@ -0,0 +1,36 @@
+func greeting(friend: String, me: String = "yagom") {
+    print("Hello \(friend)! I'm \(me)")
+}
+
+// 매개변수 기본값을 가지는 매개변수는 호출시 생략할 수 있습니다
+greeting(friend: "hana") // Hello hana! I'm yagom
+greeting(friend: "john", me: "eric") // Hello john! I'm eric
+
+
+// 함수 내부에서 전달인자를 사용할 때에는 매개변수 이름을 사용합니다
+func greeting(to friend: String, from me: String) {
+    print("Hello \(friend)! I'm \(me)")
+}
+
+
+// 함수를 호출할 때에는 전달인자 레이블을 사용해야 합니다
+greeting(to: "hana", from: "yagom") // Hello hana! I'm yagom
+
+var someFunction: (String, String) -> Void = greeting(to:from:)
+someFunction("eric", "yagom") // Hello eric! I'm yagom
+
+someFunction = greeting(friend:me:)
+someFunction("eric", "yagom") // Hello eric! I'm yagom
+
+
+func runAnother(function: (String, String) -> Void) {
+    function("jenny", "mike")
+}
+
+// Hello jenny! I'm mike
+runAnother(function: greeting(friend:me:))
+
+// Hello jenny! I'm mike
+runAnother(function: someFunction)
+
+// 참고 : 스위프트의 전반적인 문법에서 띄어쓰기는 신경써야할 때가 많습니다
