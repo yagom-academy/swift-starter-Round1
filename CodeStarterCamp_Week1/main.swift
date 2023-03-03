5ce4c565474c915238a99ae8d216f23a6c79fe15
ny <nayeon6868@gmail.com>
2023-03-03 17:09:27 +0900

    swift-starter-week1

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  makePpr.swift
 //  CodeStarterCamp_Week1
 //
 //  Created by 나연  강 on 2023/03/02.
@@ -7,6 +7,33 @@
 
 import Foundation
 
-//makeWholeIceCream(iceWidth: 10, iceHeight: 9, barHeight: 5)
+func makePeperoBar(barHeight: Int) {
+    for _ in 1...barHeight {
+        print(" | |")
+    }
+}
 
-makeWholePpr(pprBody: "***", pprTopping: "@", pprHeight: 12, barHeight: 4)
+func makePeperoBody(bodyIs body: String, heightIs height: Int, with topping: String) {
+    for _ in 1...height {
+        if topping == "" {
+            print(" \(body)")
+        } else {
+            print("\(topping)\(body)\(topping)")
+        }
+    }
+}
+
+func makeWholePepero(bodyIs body: String, heightIs height: Int, with topping: String, barHeight: Int) {
+    print("""
+    <정보>
+    길이: \(height)
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(barHeight)
+    
+    """)
+    makePeperoBody(bodyIs: body, heightIs: height, with: topping)
+    makePeperoBar(barHeight: barHeight)
+}
+
+makeWholePepero(bodyIs: "***", heightIs: 14, with: "$", barHeight: 4)
