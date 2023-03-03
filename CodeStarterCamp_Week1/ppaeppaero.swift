72b0015c8dcc0fe8034b51dccb50c65d9cd0b887
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-03-03 11:17:15 +0900

    API Guideline 준수,순서도 버전업

--- CodeStarterCamp_Week1/ppaeppaero.swift
+++ CodeStarterCamp_Week1/ppaeppaero.swift
@@ -8,25 +8,24 @@
 import Foundation
 
 
-func drawStick(length: Int){
-    for _ in 1...length{
+func drawStick(length: Int) {
+    for _ in 1...length {
         print(" | | ")
     }
 }
 
-func makeBody(body: String, topping: String) -> String{
-    return topping+body+topping
+func formBody(bodyMaterial: String, topping: String) -> String {
+    return topping + bodyMaterial + topping
 }
 
-func drawBody(length: Int, body: String, stick: Int){
-    for _ in 1...length{
+func drawBody(length: Int, body: String, stick: Int) {
+    for _ in 1...length {
         print(body)
     }
     drawStick(length: stick)
 }
 
 func drawPpaeppaero(body: String, topping: String, bodyLength: Int, stickLength: Int) {
-    
     print("""
         <정보>
         길이: \(bodyLength)
@@ -34,6 +33,6 @@ func drawPpaeppaero(body: String, topping: String, bodyLength: Int, stickLength:
         토핑: \(topping)
         막대길이: \(stickLength)
         """)
-    let bodyline:String = makeBody(body: body, topping: topping)
-    drawBody(length: bodyLength, body: bodyline, stick: stickLength)
+    let bodyComponent: String = formBody(bodyMaterial: body, topping: topping)
+    drawBody(length: bodyLength, body: bodyComponent, stick: stickLength)
 }
