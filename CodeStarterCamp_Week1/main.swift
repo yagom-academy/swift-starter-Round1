3031db054eab26652a50712991ecb174c20db4bf
허준하 <heojunha@heojunhaui-MacBookPro.local>
2023-03-04 01:57:42 +0900

    step3 코드,순서도

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,26 +1,53 @@
 //
-//  main.swift
+//  STEP 3.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 허준하 on 2023/03/03.
 //
 
 import Foundation
 
-func createIce(length: Int , width: Int) {
-    for _ in 1...length{
-        for _ in 1...width{
-            print("*",terminator: "")
-        }
-        print("")       // 한 줄로 이어지는걸 방지
-    }
+
+var assignCenterIce: String = "*********"
+var assignSideIce: String = "&"
+var assignBarLength: Int = 4
+var assignIceLength: Int = 12
+
+print("<정보>")
+print("길이: \(assignIceLength)")
+print("몸통: \(assignCenterIce)")
+print("토핑: \(assignSideIce)")
+print("막대길이: \(assignBarLength)\n")
+
+func createPartofIce(centerIce: String,sideIce: String) -> String{
+    
+    let iceLine: String = sideIce + centerIce + sideIce
+    return iceLine
 }
-func createIcebar(barLength: Int) {
-    for _ in 1...barLength {
-        print("    | |    ")
+
+func createAllofIce(iceLength: Int) {
+    for _ in 1...iceLength {
+        print(createPartofIce(centerIce: assignCenterIce, sideIce: assignSideIce))
     }
 }
 
-createIce(length:8,width:11) //아이스크림 만드는 함수호출
-createIcebar(barLength:4)   //막대기 만드는 함수호출
+func createBar(barLength: Int) {
+    let spaceCount: Int = ((createPartofIce(centerIce: assignCenterIce, sideIce: assignSideIce).count) + 1) / 2 - 2
+    let centerBar: String = "| |"
+    var bar: String = String()
+    
+    for _ in 1...spaceCount {
+        bar = bar + " "
+    }
+    
+    bar = bar + centerBar
+    
+    for _ in 1...barLength {
+        print(bar)
+    }
+    
+}
+
+createAllofIce(iceLength: assignIceLength)
+createBar(barLength: assignBarLength)
+
