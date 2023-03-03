d9b23fa8c9b16f33835686dbce3f66676006259d
Im-loki <al4126@naver.com>
2023-03-03 20:25:50 +0900

    iceCream

---
+++ CodeStarterCamp_Week1/iceCream.swift
@@ -0,0 +1,16 @@
+func doIceCream () {
+    for _ in 1...11 {
+        print("*", terminator: "")
+    }
+}
+for _ in 1...9 {
+    doIceCream()
+}
+func dsd () {
+    for _ in 1...4 {
+        print(" ", terminator: "")
+    }
+}
+for _ in 1...4 {
+    print(\(dsd)"| |")
+}
