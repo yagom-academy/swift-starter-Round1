adb2846d953ec1f2ad75ff6d9d79b5104c2a1cbe
Im-loki <al4126@naver.com>
2023-03-05 14:25:18 +0900

    step2 아이스크림과 막대 제작

--- CodeStarterCamp_Week1/iceCream.swift
+++ CodeStarterCamp_Week1/iceCream.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+func doIceCream() {
+    for _ in 1...11 {
+        for _ in 1...9 {
+            print("*", separator: "", terminator: "")
+        }
+        print()
+    }
+}
+
+func addLength() {
+    for _ in 1...4 {
+        print("   | |")
+    }
+}
