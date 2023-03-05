01ab90d77cac0865a13538372404f26f2e4d17b4
dayoung <celeste7297@naver.com>
2023-03-06 00:13:42 +0900

    main.swift

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,43 +9,34 @@
 import Foundation
 
 // Step 3 (1)
-func makeBar(barlength: Int) {
-    for _ in 1...barlength {
-        print(" | |")}}
-
-func makeBodyRow(bodyshape: String, topping: String) -> String {
-    return topping + bodyshape + topping
+func makeBar(barHeigth: Int) {
+    for _ in 1...barHeigth {
+        print(" | |")
+    }
 }
 
-var bodyRow: (String, String) -> String = makeBodyRow(bodyshape:topping:)
+func makeBodyWidth(bodyShape: String, topping: String) -> String {
+    return topping + bodyShape + topping
+}
 
-func makeBodyCol(bodylength: Int, bodyrow: Any) {
-    for _ in  1...bodylength {
-            print(bodyrow)
-}}
+var bodyWidth: (String, String) -> String = makeBodyWidth(bodyShape:topping:)
 
-func makePePero(bodylength: Int, bodyshape: String, topping: String, barlength: Int) {
-    print("<정보>\n길이: \(bodylength)\n몸통: \(bodyshape)\n토핑: \(topping)\n막대길이: \(barlength)\n")
-        makeBodyCol(bodylength: bodylength, bodyrow: bodyRow(bodyshape, topping))
-        makeBar(barlength: barlength)
+func makeBodyHeight(bodyHeight: Int, bodyWidth: Any) {
+    for _ in  1...bodyHeight {
+        print(bodyWidth)
+    }
+}
+
+func makePePero(bodyHeight: Int, bodyShape: String, topping: String, barHeigth: Int) {
+    print("<정보>\n길이: \(bodyHeight)\n몸통: \(bodyShape)\n토핑: \(topping)\n막대길이: \(barHeigth)\n")
+        makeBodyHeight(bodyHeight: bodyHeight, bodyWidth: bodyWidth(bodyShape, topping))
+        makeBar(barHeigth: barHeigth)
 }
 
 
-
-//Step 3 (2)
-//func makePePero2(bodyshape: String, bodylength: Int, topping: String, barlength: Int) {
-//    print("<정보>\n길이: \(bodylength)\n몸통: \(bodyshape)\n토핑: \(topping)\n막대길이: \(barlength)\n")
-//    for _ in  1...bodylength {
-//        print(topping + bodyshape + topping)}
-//    for _ in 1...barlength {
-//        print(" | |")
-//    }
-//}
-
-
-makePePero(bodylength: 10, bodyshape: " ***", topping: "", barlength: 4)
-makePePero(bodylength: 12, bodyshape: "***", topping: "&", barlength: 4)
-makePePero(bodylength: 12, bodyshape: "***", topping: "#", barlength: 6)
-makePePero(bodylength: 6, bodyshape: " |0|", topping: "", barlength: 6)
+makePePero(bodyHeight: 10, bodyShape: " ***", topping: "", barHeigth: 4)
+makePePero(bodyHeight: 12, bodyShape: "***", topping: "&", barHeigth: 4)
+makePePero(bodyHeight: 12, bodyShape: "***", topping: "#", barHeigth: 6)
+makePePero(bodyHeight: 6, bodyShape: " |0|", topping: "", barHeigth: 6)
 
 
