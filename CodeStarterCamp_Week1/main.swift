60e0691a2f3fc9d6ebf5800851bcecd5c5f4760a
Hot-ttu <ghkdwndud7@naver.com>
2023-03-08 20:06:39 +0900

    Week1 Step3 PR

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,17 +8,83 @@
 
 import Foundation
 
-func makeIceCream() {
-    for _ in 1...7 {
-        print("***********")
+func printPeperoBar(lengthOfBar: Int) {
+    for _ in 1...lengthOfBar {
+        print(" | |")
     }
 }
 
-func makeBar() {
-    for _ in 1...4 {
-        print("    | |")
+func sumPeperoToppingAndBody(body: String, topping: String) -> String {
+    return topping + body + topping
+}
+
+func printPeperoBody(lengthOfBody: Int, body: String, topping: String) {
+    for _ in 1...lengthOfBody {
+        print(sumPeperoToppingAndBody(body: body, topping: topping))
     }
 }
 
-makeIceCream()
-makeBar()
+func printPepero(lengthOfBar: Int, body: String, topping: String = " ", lengthOfBody: Int) {
+    printPeperoBody(lengthOfBody: lengthOfBody, body: body, topping: topping)
+    printPeperoBar(lengthOfBar: lengthOfBar)
+}
+
+print("""
+
+실행 예시 1
+<정보>
+길이: 10
+몸통: ***
+토핑:
+막대길이: 4
+
+""")
+printPepero(lengthOfBar: 4, body: "***", lengthOfBody: 10)
+
+print("""
+
+실행 예시 2
+<정보>
+길이: 12
+몸통: ***
+토핑: &
+막대길이: 4
+
+""")
+printPepero(lengthOfBar: 4, body: "***", topping: "&", lengthOfBody: 12)
+
+print("""
+
+실행 예시 3
+<정보>
+길이: 12
+몸통: ***
+토핑: #
+막대길이: 6
+
+""")
+printPepero(lengthOfBar: 6, body: "***", topping: "#", lengthOfBody: 12)
+
+print("""
+
+실행 예시 4
+<정보>
+길이: 6
+몸통: |0|
+토핑:
+막대길이: 4
+
+""")
+printPepero(lengthOfBar: 4, body: "|0|", lengthOfBody: 6)
+
+print("""
+
+실행 예시 5 (나만의 빼빼로)
+<정보>
+길이: 8
+몸통: |0|
+토핑: #
+막대길이: 4
+
+""")
+printPepero(lengthOfBar: 4, body: "|0|", topping: "#", lengthOfBody: 8)
