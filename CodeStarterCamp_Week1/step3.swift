a2f04d0cae9c3ed5cc31f213b0e3cf229321d2d0
강도훈 <donishoon@gangdohun-ui-MacBookAir.local>
2023-03-09 01:50:55 +0900

    STEP3 수정 23.03.09

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -8,23 +8,33 @@
 import Foundation
 
 
-func makeStick (stickShape: String, stickSize: Int) {
-    for _ in 1...stickSize{
+
+
+func makePepepro (body: String ,topping: String, peperoSize: Int, stickShape: String, stickSize: Int) {
+    // 몸통과 토핑을 매게변수로 받아 토핑을 그리는 함수
+    print("<정보>\n길이: \(peperoSize)\n몸통: \(body)\n토핑: \(topping)\n막대길이: \(stickSize)\n ")
+    
+    // 길이에 따라 몸통을 그리는 함수
+    for _ in 1...peperoSize {
+        print("\(topping)\(body)\(topping)")
+    }
+    // 길이에 따른 막대를 그리는 함수
+    for _ in 1...stickSize {
         print("\(stickShape)")
     }
 }
 
-func makePepero(body: String ,topping: String, peperoSize: Int ) {
-    for _ in 1...peperoSize{
-        print("\(topping)\(body)\(topping) ")
+
+func myOwnPepero(body: String ,firstTopping: String , lastTopping: String, peperoSize: Int, stickShape: String, stickSize: Int) {
+print("<정보>\n길이: \(peperoSize)\n몸통: \(body)\n토핑: \(firstTopping)\(lastTopping)\n막대길이: \(stickSize)\n ")
+
+ for _ in 1...peperoSize {
+    print("\(firstTopping)\(body)\(lastTopping) ")
+}
+    for _ in 1...stickSize {
+        print("\(stickShape)")
     }
-}
-
-func makePepero2(body: String ,topping1: String , topping2: String, peperoSize2: Int )
-{ for _ in 1...peperoSize2 {
-    print("\(topping1)\(body)\(topping2) ") }
-}
-
+        }
 
 
 
