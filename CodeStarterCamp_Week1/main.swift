9c39bc3c94b55e57da3677169dfa9153050ae7e3
Hot-ttu <ghkdwndud7@naver.com>
2023-03-09 17:02:40 +0900

    Week1 Step3 피드백

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -14,77 +14,36 @@ func printPeperoBar(lengthOfBar: Int) {
     }
 }
 
-func sumPeperoToppingAndBody(body: String, topping: String) -> String {
+func makePeperoToppingAndBody(body: String, topping: String) -> String {
     return topping + body + topping
 }
 
 func printPeperoBody(lengthOfBody: Int, body: String, topping: String) {
     for _ in 1...lengthOfBody {
-        print(sumPeperoToppingAndBody(body: body, topping: topping))
+        print(makePeperoToppingAndBody(body: body, topping: topping))
     }
 }
 
+var runCount: Int = 1
+
 func printPepero(lengthOfBar: Int, body: String, topping: String = " ", lengthOfBody: Int) {
+    print("""
+
+    실행 예시 \(runCount)
+    <정보>
+    길이: \(lengthOfBody)
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(lengthOfBar)
+
+    """)
     printPeperoBody(lengthOfBody: lengthOfBody, body: body, topping: topping)
     printPeperoBar(lengthOfBar: lengthOfBar)
+    runCount += 1
 }
 
-print("""
-
-실행 예시 1
-<정보>
-길이: 10
-몸통: ***
-토핑:
-막대길이: 4
-
-""")
 printPepero(lengthOfBar: 4, body: "***", lengthOfBody: 10)
-
-print("""
-
-실행 예시 2
-<정보>
-길이: 12
-몸통: ***
-토핑: &
-막대길이: 4
-
-""")
 printPepero(lengthOfBar: 4, body: "***", topping: "&", lengthOfBody: 12)
-
-print("""
-
-실행 예시 3
-<정보>
-길이: 12
-몸통: ***
-토핑: #
-막대길이: 6
-
-""")
 printPepero(lengthOfBar: 6, body: "***", topping: "#", lengthOfBody: 12)
-
-print("""
-
-실행 예시 4
-<정보>
-길이: 6
-몸통: |0|
-토핑:
-막대길이: 4
-
-""")
 printPepero(lengthOfBar: 4, body: "|0|", lengthOfBody: 6)
-
-print("""
-
-실행 예시 5 (나만의 빼빼로)
-<정보>
-길이: 8
-몸통: |0|
-토핑: #
-막대길이: 4
-
-""")
 printPepero(lengthOfBar: 4, body: "|0|", topping: "#", lengthOfBody: 8)
