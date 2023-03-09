749398cf12a2fa9c6f52439b7747855e33f89dca
강도훈 <donishoon@gangdohun-ui-MacBookAir.local>
2023-03-10 01:06:25 +0900

    23.03.10수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -27,29 +27,29 @@ myFirstMakeStick(size: 4)
 print()
 print("STEP3")
 
+
 // 다양한 빼빼로 출력
 print()
 print("실행 예시1")
 
-makePepepro(body: "***", topping: "", peperoSize: 10, stickShape: "ㅣㅣ", stickSize: 4)
+makePepepro(body: "***", peperoSize: 10, stickSize: 4)
 
 print()
 print("실행 예시2")
 
-makePepepro(body: "***", topping: "&", peperoSize: 12, stickShape: " ㅣㅣ", stickSize: 4)
+makePepepro(body: "***", topping: "&", peperoSize: 12, stickSize: 4)
 
 print()
 print("실행 예시3")
 
-makePepepro(body: "***", topping: "#", peperoSize: 12, stickShape: " ㅣㅣ", stickSize: 6)
+makePepepro(body: "***", topping: "#", peperoSize: 12, stickSize: 6)
 
 print()
 print("실행 예시4")
 
-makePepepro(body: " ㅣ0ㅣ", topping: "", peperoSize: 4, stickShape: " ㅣ ㅣ", stickSize: 4)
+makePepepro(body: "ㅣ0ㅣ", peperoSize: 4, stickSize: 4)
 
 print()
 print("내가 만든 빼뺴로")
 
-myOwnPepero(body: "ㅁㅁ", firstTopping: "<", lastTopping: ">", peperoSize: 5, stickShape: " ㅣㅣ", stickSize: 4)
- 
+makePepepro(body: "ㅁ", topping: "^^", peperoSize: 10, stickSize: 3)
--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -8,33 +8,29 @@
 import Foundation
 
 
+// 길이에 따른 막대를 그리는 함수
+func makeStick(stickSize: Int) {
+    for _ in 1...stickSize {
+        print(" ㅣㅣ")
+    }
+}
+
+// 몸통과 토핑을 매게변수로 받아 토핑과 몸통을 그리는 함수
+func makePeperoTopping(body: String, topping: String = "") {
+    print("\(topping)\(body)\(topping)")
+}
 
 
-func makePepepro (body: String ,topping: String, peperoSize: Int, stickShape: String, stickSize: Int) {
-    // 몸통과 토핑을 매게변수로 받아 토핑을 그리는 함수
-    print("<정보>\n길이: \(peperoSize)\n몸통: \(body)\n토핑: \(topping)\n막대길이: \(stickSize)\n ")
-    
-    // 길이에 따라 몸통을 그리는 함수
+// 길이에 따라 몸통을 그리는 함수
+func makePeperoBody(body: String, topping: String, peperoSize: Int) {
     for _ in 1...peperoSize {
         print("\(topping)\(body)\(topping)")
     }
-    // 길이에 따른 막대를 그리는 함수
-    for _ in 1...stickSize {
-        print("\(stickShape)")
-    }
 }
 
-
-func myOwnPepero(body: String ,firstTopping: String , lastTopping: String, peperoSize: Int, stickShape: String, stickSize: Int) {
-print("<정보>\n길이: \(peperoSize)\n몸통: \(body)\n토핑: \(firstTopping)\(lastTopping)\n막대길이: \(stickSize)\n ")
-
- for _ in 1...peperoSize {
-    print("\(firstTopping)\(body)\(lastTopping) ")
+// 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+func makePepepro(body: String ,topping: String = " ", peperoSize: Int, stickSize: Int) {
+    print("<정보>\n길이: \(peperoSize)\n몸통: \(body)\n토핑: \(topping)\n막대길이: \(stickSize)\n ")
+    makePeperoBody(body: body, topping: topping, peperoSize: peperoSize)
+    makeStick(stickSize: stickSize)
 }
-    for _ in 1...stickSize {
-        print("\(stickShape)")
-    }
-        }
-
-
-
