ded5bc9a256d536d038eb0eea45f7a0192df0d04
Im-loki <al4126@naver.com>
2023-03-10 16:41:03 +0900

    빼빼로 코드 및 순서도 네이밍 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,14 +8,10 @@
 
 import Foundation
 
-print(" <정보>\n 길이: 10\n 몸통: ***\n 토핑: ","\n 막대 길이: 4\n")
-cookPepero(peperoChoco: "***", peperoTopping: ",", peperoLength: 10, peperoBarLength: 4)
+createPepero(choco: "***", body: 10, bar: 4, topping: "")
 
-print(" <정보>\n 길이: 12\n 몸통: ***\n 토핑: &\n 막대 길이: 4\n")
-cookAmpPepero(ampPeperoChoco: "***", ampPeperoTopping: "&", ampPeperoLength: 12, ampPeperoBarLength: 4)
+createPepero(choco: "***", body: 12, bar: 4, topping: "&")
 
-print(" <정보>\n 길이: 12\n 몸통: ***\n 토핑: #\n 막대 길이: 6\n")
-cookSharpPepero(sharpPeperoChoco: "***", sharpPeperoTopping: "#", sharpPeperoLength: 12, sharpPeperoBarLength: 6)
+createPepero(choco: "***", body: 12, bar: 6, topping: "#")
 
-print(" <정보>\n 길이: 6\n 몸통: |0|\n 토핑: ","\n 막대 길이: 4\n")
-cookNudePepero(nudePeperoChoco: " |0|", nudePeperoTopping: ",", nudePeperoLength: 6, nudePeperoBarLength: 4)
+createPepero(choco: " |0|", body: 6, bar: 4, topping: "")
--- CodeStarterCamp_Week1/pepero.swift
+++ CodeStarterCamp_Week1/pepero.swift
@@ -7,23 +7,24 @@
 
 import Foundation
 
-func cookPeperoCream(peperoChoco: String, peperoTopping: String) -> String {
-    return " ***"
+func cookPeperoBody(choco: String, topping: String) -> String {
+    return topping + choco + topping
 }
 
-func cookPeperoCreamLength(peperoChoco: String, peperoTopping: String, peperoLength: Int) {
-    for _ in 1...peperoLength {
-        print(cookPeperoCream(peperoChoco: "***", peperoTopping: ","))
+func controlPeperoBodyLength(body: Int, choco: String, topping: String) {
+    for _ in 1...body {
+        print(cookPeperoBody(choco: choco, topping: topping))
     }
 }
 
-func createPeperoBar(peperoBarLength: Int) {
-    for _ in 1...peperoBarLength {
+func controlPeperoBarLength(bar: Int) {
+    for _ in 1...bar {
         print(" | |")
     }
 }
 
-func cookPepero(peperoChoco: String, peperoTopping: String, peperoLength: Int, peperoBarLength: Int) {
-    cookPeperoCreamLength(peperoChoco: "***", peperoTopping: ",", peperoLength: 10)
-    createPeperoBar(peperoBarLength: 4)
+func createPepero(choco: String, body: Int, bar: Int, topping: String) {
+    print(" <정보>\n 길이: \(body)\n 몸통: \(choco)\n 토핑:  \(topping)\n 막대 길이: \(bar)\n")
+    controlPeperoBodyLength(body: body, choco: choco, topping: topping)
+    controlPeperoBarLength(bar: bar)
 }
