fa1810d47814c42f55f20c803bf47ca005a4daea
Hayeon Kim <qoocrab@gmail.com>
2023-03-17 16:11:14 +0900

    Feat: 1주차 미션 - Step 3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,5 +5,27 @@
 // Copyright © yagom academy. All rights reserved.
 import Foundation
 
-printIcecreamSoft(length: 8)
-printIcecreamBar(length: 4)
+while true {
+    print("초콜릿 과자의 길이를 입력하세요 (종료하려면 0): ", terminator: "")
+    let chocoSnackLength = Int(readLine()!)!
+    if (chocoSnackLength == 0) {
+        print("프로그램을 종료합니다.")
+        break
+    }
+    
+    print("초콜릿 모양을 입력하세요: ", terminator: "")
+    let chocoShape = readLine()!
+    
+    print("토핑 모양을 입력하세요: ", terminator: "")
+    let topingShape = readLine()!
+    
+    print("바의 길이를 입력하세요 (종료하려면 0): ", terminator: "")
+    let barLength = Int(readLine()!)!
+    if (barLength == 0) {
+        print("프로그램을 종료합니다.")
+        break
+    }
+    
+    printPepero(chocoShape: chocoShape, topingShape: topingShape, chocoSnackLength: chocoSnackLength, barLength: barLength)
+    print()
+}
