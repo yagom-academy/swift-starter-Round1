5dbd91eedf2d959c181a90df6f18cadba0313232
JIWON1923 <zest1923@gmail.com>
2023-05-17 22:28:57 +0900

    [Feat] 아이스크림 함수 제작

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawIceCream(size: Int = 8) {
+    for _ in 0 ..< size {
+        print(String (repeating: "*", count: 11))
+    }
+    
+    for _ in 0 ..< 4 {
+        drawStick()
+    }
+}
 
+func drawStick() {
+    print("    | |")
+}
+
+drawIceCream()
