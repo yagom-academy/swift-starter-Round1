fd317d36ce5f86d67bf2267a5e3189f350e407d5
madcow95 <lobasketve@naver.com>
2023-05-17 22:48:03 +0900

    [STEP2] 아이스크림 만들기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,4 +8,17 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawCreamPart(creamLength: Int, creamComponent: String) {
+    for _ in 1...creamLength {
+        print(creamComponent)
+    }
+}
+
+func drawHandlePart(handleLength: Int, handleComponent: String) {
+    for _ in 1...handleLength {
+        print(handleComponent)
+    }
+}
+
+drawCreamPart(creamLength: 8, creamComponent: "***********")
+drawHandlePart(handleLength: 4, handleComponent: "    | |")
