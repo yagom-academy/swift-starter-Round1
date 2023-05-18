ce340b6ef86f4b7c7406dd02e78f3e57a577ca28
JminSim <jeong0983@naver.com>
2023-05-19 00:01:57 +0900

    code

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawIceCream() {
+    for x in 1...8 {
+        for y in 1...11 {
+          print("*",terminator: "")
+        }
+        print("")
+    }
+}
 
+
+func drawBar() {
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+
+}
+
+
+drawIceCream()
+drawBar()
