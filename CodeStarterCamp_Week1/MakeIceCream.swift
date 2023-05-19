1f45a014a9071fcc2477ecb2294e48a7cec3f9a2
Kim, Yerim <yerim343@naver.com>
2023-05-19 14:43:40 +0900

    :adhesive_bandage:  makeIceCreamBody 함수 수정

--- CodeStarterCamp_Week1/MakeIceCream.swift
+++ CodeStarterCamp_Week1/MakeIceCream.swift
@@ -15,13 +15,12 @@ func makeIceCreamBody(width: Int, height: Int) {
         }
         print("")
     }
-    print("")
 }
 
 func makeIceCreamBar(width: Int, height: Int) {
     for _ in 0..<height {
         for currentWidthPosition in 0..<width {
-            // 위치가 4 혹은 6일 때 print 구문을 출력하고 for문의 다음 loop로 넘어감 
+            // 위치가 4 혹은 6일 때 print 구문을 출력하고 for문의 다음 loop로 넘어감
             if currentWidthPosition == 4 || currentWidthPosition == 6 {
                 print("|", terminator: "")
                 continue
