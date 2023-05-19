8f54399087ead30fee0948e98ac3a489a068a836
JminSim <jeong0983@naver.com>
2023-05-19 10:46:07 +0900

    코드 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 func drawIceCream() {
-    for x in 1...8 {
-        for y in 1...11 {
+    for _ in 1...8 {
+        for _ in 1...11 {
           print("*",terminator: "")
         }
         print("")
@@ -22,7 +22,6 @@ func drawBar() {
     for _ in 1...4 {
         print("    | |    ")
     }
-
 }
 
 
