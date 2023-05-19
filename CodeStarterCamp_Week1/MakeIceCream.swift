d3cc68ee15a25b75e4cb452577571cf4de0bc94e
Kim, Yerim <yerim343@naver.com>
2023-05-19 14:32:47 +0900

    :adhesive_bandage: 변수명, 파일 이름, for 문 안의 코드 등 간단한 수정

---
+++ CodeStarterCamp_Week1/MakeIceCream.swift
@@ -0,0 +1,33 @@
+//
+//  MakeIceCream.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 김예림 on 2023/05/17.
+//
+
+import Foundation
+
+
+func makeIceCreamBody(width: Int, height: Int) {
+    for _ in 0..<height {
+        for _ in 0..<width {
+            print("*",terminator: "")
+        }
+        print("")
+    }
+    print("")
+}
+
+func makeIceCreamBar(width: Int, height: Int) {
+    for _ in 0..<height {
+        for currentWidthPosition in 0..<width {
+            // 위치가 4 혹은 6일 때 print 구문을 출력하고 for문의 다음 loop로 넘어감 
+            if currentWidthPosition == 4 || currentWidthPosition == 6 {
+                print("|", terminator: "")
+                continue
+            }
+            print(" ", terminator: "")
+        }
+        print("")
+    }
+}
