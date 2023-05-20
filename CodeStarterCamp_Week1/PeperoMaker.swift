ea094e62373d5cb658bcb1a9862778b6f33c7c11
JIWON1923 <zest1923@gmail.com>
2023-05-20 20:55:21 +0900

    chore - 파일 구조 변경

---
+++ CodeStarterCamp_Week1/PeperoMaker.swift
@@ -0,0 +1,8 @@
+//
+//  PeperoMaker.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 이지원 on 2023/05/20.
+//
+
+import Foundation
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,20 +8,6 @@
 
 import Foundation
 
-func orderIceCream(width: Int, height: Int, stickSize: Int = 4) {
-    makeIceCream(width: width, height: height)
-    makeStick(width: width, stickSize: stickSize)
-}
-
-func makeIceCream(width: Int, height: Int) {
-    for _ in 0 ..< height {
-        print(String(repeating: "*", count: width))
-    }
-}
-
-func makeStick(width: Int, stickSize: Int) {
-    print(String(repeating: " ", count: (width-3)/2) + "| |")
-}
-
+// MARK: - [STEP 2] 아이스크림 만들기
 orderIceCream(width: 10, height: 8)
 
