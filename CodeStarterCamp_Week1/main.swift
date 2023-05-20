8a1cce224c9509addd68a90d245c66c4d16bca2f
JIWON1923 <zest1923@gmail.com>
2023-05-20 20:47:05 +0900

    step2 - 요구사항에 맞춰 함수 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,17 +8,20 @@
 
 import Foundation
 
-func drawIceCream(width: Int, height: Int, stickSize: Int) {
-    
+func orderIceCream(width: Int, height: Int, stickSize: Int = 4) {
+    makeIceCream(width: width, height: height)
+    makeStick(width: width, stickSize: stickSize)
+}
+
+func makeIceCream(width: Int, height: Int) {
     for _ in 0 ..< height {
         print(String(repeating: "*", count: width))
     }
-    
-    for _ in 0 ..< stickSize {
-        print(String(repeating: " ", count: (width-3)/2), terminator: "")
-        print("| |")
-    }
 }
 
+func makeStick(width: Int, stickSize: Int) {
+    print(String(repeating: " ", count: (width-3)/2) + "| |")
+}
 
-drawIceCream(width: 10, height: 8, stickSize: 4)
+orderIceCream(width: 10, height: 8)
+
