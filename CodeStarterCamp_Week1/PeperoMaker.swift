aa9138f5e9c2e55fd2b496922455450ed6b95162
JIWON1923 <zest1923@gmail.com>
2023-05-20 21:25:41 +0900

    [Step 3] 배빼로 만들기

--- CodeStarterCamp_Week1/PeperoMaker.swift
+++ CodeStarterCamp_Week1/PeperoMaker.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+func makePepero(size: Int, body: String, topping: String = " ", stickSize: Int) {
+    
+    let peperoSingleBody = drawPeperoSingleBody(body: body, topping: topping)
+    drawPeperoBody(body: peperoSingleBody, size: size)
+    drawStick(toppingThickness: topping.count, size: stickSize)
+}
+
+func drawStick(toppingThickness: Int, size: Int) {
+    for _ in 0 ..< size {
+        print(String(repeating: " ", count: toppingThickness), terminator: "")
+        print("| |")
+    }
+}
+
+func drawPeperoSingleBody(body: String, topping: String) -> String {
+    topping + body + topping
+}
+
+func drawPeperoBody(body: String, size: Int){
+    for _ in 0 ..< size {
+        print(body)
+    }
+}
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -11,3 +11,5 @@ import Foundation
 // MARK: - [STEP 2] 아이스크림 만들기
 orderIceCream(width: 10, height: 8)
 
+// MARK: - [STEP 3] 빼빼로 만들기
+makePepero(size: 10, body: "|0|", stickSize: 4)
