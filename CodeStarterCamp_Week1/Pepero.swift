3fae7fb80095cd9b7083c38e02736c00dc7dbbe8
미르 <125941932+mireu79@users.noreply.github.com>
2023-05-24 12:12:53 +0900

    Pepero.swift  재수정

--- CodeStarterCamp_Week1/Pepero.swift
+++ CodeStarterCamp_Week1/Pepero.swift
@@ -1,26 +1,27 @@
 import Foundation
 
-func makePepero(size: Int, body: String, topping: String = "", sticksize: Int) {
-     let OriginPepero = makeOriginPepro (body: body, topping: topping)
-        makePeproBody(body: OriginPepero, bodysize: bodysize)
-        makeStick(sticksize: sticksize)
+func makePepero(bodySize: Int, body: String, topping: String = "", stickSize: Int) {
+    makePeperoBody(body: makeOriginPepero (body: body, topping: topping), bodySize: bodySize)
+    makeStick(sticksize: stickSize)
 }
 
-// 스틱은 고정시키는 함수를 만들어보자
 func makeStick(sticksize:Int) {
     for _ in 0 ..< sticksize{
         print(" | |")
     }
 }
 
-func makePeproBody(body: String, bodysize: Int) {
-    for _ in 0 ..< size {
+func makePeperoBody(body: String, bodySize: Int) {
+    for _ in 0 ..< bodySize {
         print(body)
     }
 }
 
-func makeOriginPepro(body: String, topping: String) -> String{
+func makeOriginPepero(body: String, topping: String) -> String{
     topping + body + topping
 }
 
-makePepero(size: 6, body: "***", topping: "&", sticksize: 4)
+makePepero(bodySize: 6, body: "***", topping: "&", stickSize: 4)
+
+
+
