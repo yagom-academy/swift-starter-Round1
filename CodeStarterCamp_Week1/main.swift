e6af95809d582fec2b3ea917801e68d95c630d98
ltw <witlim1225@gmail.com>
2023-05-26 21:34:18 +0900

    WEEK1 [STEP2] Tony

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+func wholeIceCream(w creamWidth: Int, h creamHeight: Int, bar : Int) {
+    makeIceCream(w: creamWidth, h: creamHeight)
+    make(bar)
+}
 
+func makeIceCream(w width: Int = 11, h height: Int) {
+    var line = ""
+    for _ in 0..<width {
+        line += "*"
+    }
+    
+    for _ in 0..<height {
+        print(line)
+    }
+    
+}
+
+func make(_ bar: Int) {
+    for _ in 0..<bar {
+        print("    | |    ")
+    }
+}
+
+wholeIceCream(w: 11, h: 8, bar: 4)
+
+/*
+ 순서도
+ 
+ 시작
+ wholeIceCream을 호출한다.(넓이: 11, 높이: 8, 막대: 4)
+ |
+ 
+ 
+ 
+ 
+ */
+ 
+ 
