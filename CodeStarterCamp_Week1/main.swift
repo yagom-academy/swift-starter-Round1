5461ca86d5da52e4c6ac760a636fec8afac9485a
asvsstar <asvsstar@gamil.com>
2023-05-29 19:00:20 +0900

    아이스크림 그리기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,8 +5,28 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
 import Foundation
+func icecreamHead(){
+    for _ in 0...7{
+        for _ in 0...10{
+            print("*", terminator: "")
+        }
+        print()
+    }
+}
 
-print("Hello, World!")
+func icecreamStick(){
+    for _ in 0...3{
+        for i in 0...10{
+            if(i==4 || i==6){
+                print("|",terminator: "")
+            }else{
+                print(" ",terminator: "")
+            }
+        }
+        print()
+    }
+}
 
+icecreamHead()
+icecreamStick()
