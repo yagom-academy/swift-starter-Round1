3b5db1f92c65d4762c4bfc12af9dcea5b81137d0
asvsstar <asvsstar@gamil.com>
2023-06-05 20:43:00 +0900

    순서도 작성&주석추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,28 +8,30 @@
 
 import Foundation
 
+//뺴뺴로 머리 함수
 func makePeperoHead(peperoSize: Int, body: String, toping: String = " ") {
     for _ in 0...peperoSize {
         print("\(toping)\(body)\(toping)")
     }
 }
 
+//빼뺴로 막대 함수
 func makePeperoStick(stickSize: Int) {
     for _ in 0...stickSize {
         print(" | | ")
     }
+    print()
 }
+
+//함수 묶음
 func makePepero( peperoSize: Int, body: String, toping: String = " ", stickSize: Int) {
     makePeperoHead(peperoSize: peperoSize, body: body, toping: toping)
     makePeperoStick(stickSize: stickSize)
 }
 
 makePepero(peperoSize: 10, body: "***", stickSize: 4)
-print()
+
 makePepero(peperoSize: 12, body: "***", toping: "&", stickSize: 4)
-print()
 makePepero(peperoSize: 12, body: "***", toping: "#", stickSize: 6)
-print()
 makePepero(peperoSize: 6, body: "|0|", stickSize: 4)
-print()
 makePepero(peperoSize: 10, body: "***", toping: "\\", stickSize: 4)
