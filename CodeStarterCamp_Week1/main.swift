7da50baab9cd3f2fa7b44537a1b3b5984dd91fc3
asvsstar <asvsstar@gamil.com>
2023-06-07 06:24:53 +0900

    정보 출력 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -24,13 +24,13 @@ func makePeperoStick(stickSize: Int) {
 }
 
 //함수 묶음
-func makePepero( peperoSize: Int, body: String, toping: String = " ", stickSize: Int) {
+func makePepero(peperoSize: Int, body: String, toping: String = " ", stickSize: Int) {
+    print("<정보>\n길이: \(peperoSize)\n몸통: \(body)\n토핑: \(toping)\n막대길이: \(stickSize)\n")
     makePeperoHead(peperoSize: peperoSize, body: body, toping: toping)
     makePeperoStick(stickSize: stickSize)
 }
 
 makePepero(peperoSize: 10, body: "***", stickSize: 4)
-
 makePepero(peperoSize: 12, body: "***", toping: "&", stickSize: 4)
 makePepero(peperoSize: 12, body: "***", toping: "#", stickSize: 6)
 makePepero(peperoSize: 6, body: "|0|", stickSize: 4)
