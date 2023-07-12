e3f5f0cdaddbc67d11e4f3506b6f5948a6f6d54d
Hisop <hisop@Hisopui-MacBookPro.local>
2023-07-13 04:26:18 +0900

    STEP2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -2,11 +2,34 @@
 //  main.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by Hisop on 2023/07/12.
 //
 
 import Foundation
 
-print("Hello, World!")
+func drawIcecream(_ width:Int, _ height:Int) {
+    var str:String = ""
+    for _ in 1...width {
+        str.append("*")
+    }
+    for _ in 1...height {
+        print(str)
+    }
+}
 
+func drawBar(_ width:Int, _ height:Int) {
+    var str:String = ""
+    
+    for index in 1...width {
+        if index == width / 2 || index == width / 2 + 2 {
+            str.append ("|")
+        }
+        else { str.append(" ") }
+    }
+    for _ in 1...height {
+        print(str)
+    }
+}
+
+drawIcecream(10, 8)
+drawBar(10, 4)
