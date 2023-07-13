df6074ca733fbe9c0fc196da08d247ac6ad4ec3e
munsangu <ds-3632@hanmail.net>
2023-07-13 15:39:19 +0900

    230713

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,17 +8,37 @@
 
 import Foundation
 
-func makeTheEatingPartOfIceCream() {
+// Ice cream 만들기
+func productTheEatingPartOfIceCream() {
     for _ in 1...8 {
         print("***********")
     }
 }
 
-func makeIceCreamBar() {
+func productIceCreamBar() {
     for _ in 1...4 {
         print("    | |    ")
     }
 }
 
-makeTheEatingPartOfIceCream()
-makeIceCreamBar()
+productTheEatingPartOfIceCream()
+productIceCreamBar()
+
+// 빼빼로 만들기
+
+func productPepero(length leng: Int, body cookie: String, barLength barLeng: Int, decoration toping: String = " ") {
+    for _ in 1...leng {
+        print("\(toping)\(cookie)\(toping)")
+    }
+    for _ in 1...barLeng {
+        print(" | | ")
+    }
+}
+
+productPepero(length: 12, body: "***", barLength: 4)
+print()
+productPepero(length: 12, body: "***", barLength: 4, decoration: "&")
+print()
+productPepero(length: 12, body: "***", barLength: 6, decoration: "#")
+print()
+productPepero(length: 6, body: "|0|", barLength: 4)
