7e4635188b0ec5ade19cc76adcc5f9562230943a
ParkSY0919 <qhr498@gmail.com>
2023-07-13 13:32:19 +0900

    week1_Step2 아이스크림 그리기 & 순서도 제출

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -2,11 +2,28 @@
 //  main.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 박신영 on 2023/07/12.
 //
 
+//Step2: 아이스크림 그리기
 import Foundation
+func icecream_Draw(length: Int) {
+    for _ in 0...7 {
+        for _ in 0...10 {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
 
-print("Hello, World!")
+func stick_Draw2(length: Int) {
+    let space = "    "
+    let stick = "| |"
+    for _ in 0...3 {
+        print(space, terminator: "")
+        print(stick)
+    }
+}
 
+icecream_Draw(length: 8)
+stick_Draw2(length: 4)
