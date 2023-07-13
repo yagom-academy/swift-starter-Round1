07008bd63678568badb90a9cb0bbdb1424da4615
ParkSY0919 <qhr498@gmail.com>
2023-07-13 14:04:29 +0900

    Step2 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,23 +7,23 @@
 
 //Step2: 아이스크림 그리기
 import Foundation
-func icecream_Draw(length: Int) {
-    for _ in 0...7 {
-        for _ in 0...10 {
+func icecream_Draw(column: Int, row: Int) {
+    for _ in 0...column {
+        for _ in 0...row {
             print("*", terminator: "")
         }
         print("")
     }
 }
 
-func stick_Draw2(length: Int) {
+func stick_Draw2(column: Int) {
     let space = "    "
     let stick = "| |"
-    for _ in 0...3 {
+    for _ in 0...column {
         print(space, terminator: "")
         print(stick)
     }
 }
 
-icecream_Draw(length: 8)
-stick_Draw2(length: 4)
+icecream_Draw(column: 8, row: 6)
+stick_Draw2(column: 4)
