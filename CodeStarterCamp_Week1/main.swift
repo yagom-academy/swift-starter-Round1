e98ea2f071fda35aa92e90a8568098d7fd6f3267
Booyouhada <booyouhada@gmail.com>
2023-07-16 16:38:08 +0900

    docs: remove codes from main

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,23 +5,3 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
-func printIceCream(body:Int, stickk:Int) {
-    let star = "*"
-    let blank = "   "
-    let stick = "| |"
-
-    
-    for _ in 0...body {
-        for _ in 0...10 {
-            print (star, terminator: "")
-        }
-        print("")
-    }
-    for _ in 0...stickk {
-            print (blank, stick)
-        }
-    }
-
-printIceCream(body: 7 , stickk: 3 )
-
