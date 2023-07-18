b58437dfe40787c1ee544e2dea2085a1e2232dd4
Booyouhada <booyouhada@gmail.com>
2023-07-18 20:56:14 +0900

    docs: move step 2 code to main.swift

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,4 +5,29 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 
+func printIceCreamBody(body: Int) {
+    let star = "*"
+    for _ in 0...body {
+        for _ in 0...10 {
+            print(star,terminator:"")
+        }
+        print("")
+    }
+}
+
+func printIceCreamStick(stick: Int) {
+    let blank = "   "
+    let stickImage = "| |"
+    for _ in 0...stick {
+        print(blank, stickImage)
+    }
+}
+
+func printIceCream(body: Int, stick: Int){
+    printIceCreamBody(body: body)
+    printIceCreamStick(stick: stick)
+}
+
+printIceCream(body: 7, stick: 3)
+
 
