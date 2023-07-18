12735ed0acda7ce5817b2ad6c87d1b1b0f342a44
Ann <hyeinsound@gmail.com>
2023-07-18 20:56:16 +0900

    빼빼로 만들기 함수 추가(xcode 파일)

---
+++ CodeStarterCamp_Week1/icecream.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+//func drawIcecream() {
+//    for _ in 1...8 {
+//        print("***********")
+//    }
+//}
+//
+//func drawStick() {
+//    for _ in 1...4 {
+//        print("    ⎮ ⎮    ")
+//    }
+//}
+//
+//drawIcecream()
+//drawStick()
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -2,23 +2,46 @@
 //  main.swift
 //  CodeStarterCamp_Week1
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 조혜인 on 2023/07/18.
 //
 
 import Foundation
 
-func drawIcecream() {
-    for _ in 1...8 {
-        print("***********")
+// 길이에 따른 막대를 그리는 함수
+func drawPeperoStick(stickheight: Int, stick: String) {
+    for _ in 1...stickheight {
+        print(stick)
     }
 }
 
-func drawStick() {
-    for _ in 1...4 {
-        print("    ⎮ ⎮    ")
+// 몸통, 토핑을 매개변수로 받아 토핑과 몸통을 그리는 함수
+func drawPeperoBody(body: String, topping: String) -> String {
+    return topping + body + topping
+}
+
+// 길이에 따라 몸통을 그리는 함수
+func drawPeperoBodyHeight(bodyheight: Int, bodyshape: String) {
+    for _ in 1...bodyheight {
+        print(bodyshape)
     }
 }
 
-drawIcecream()
-drawStick()
+//각종 매개변수를 받아 위 세개의 메서드를 호출하는 함수
+func makePepero(bodyheight: Int, body: String, topping: String, stickheight: Int, stick: String) {
+    print("<정보>")
+    print("길이: \(bodyheight)\n몸통: \(body)\n토핑: \(topping)\n막대길이: \(stickheight)")
+    print("")
+    let bodyshape : String = drawPeperoBody(body: body, topping: topping)
+    drawPeperoBodyHeight(bodyheight: bodyheight, bodyshape: bodyshape)
+    drawPeperoStick(stickheight: stickheight, stick: stick)
+}
+
+makePepero(bodyheight: 10, body: "***", topping: " ", stickheight: 4, stick: " | |") //예제 1
+print("")
+makePepero(bodyheight: 12, body: "***", topping: "&", stickheight: 4, stick: " | |") //예제 2
+print("")
+makePepero(bodyheight: 12, body: "***", topping: "#", stickheight: 6, stick: " | |") //예제 3
+print("")
+makePepero(bodyheight: 6, body: " |0|", topping: " ", stickheight: 4, stick: "  | |") //예제 4
+print("")
+makePepero(bodyheight: 7, body: "$$$", topping: "*", stickheight: 3, stick: " | |") // 나만의 빼빼로
