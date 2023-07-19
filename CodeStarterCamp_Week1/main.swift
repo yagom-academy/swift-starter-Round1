7aefdd102614fbd715ca8d3ff11787de9931474b
uemu <kim-youngmin@uemu-ui-MacBookAir.local>
2023-07-20 01:06:28 +0900

    step3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,15 +1,41 @@
 import Foundation
 
-func drawIceCreamBody(line: Int){
-    for draw in 1...line{
-        print(draw, "  ***********")
+func snacksBody(length: Int, body: String, topping: String) {
+    for _ in 1...length {
+        print("\(topping)\(body)\(topping)")
     }
 }
-drawIceCreamBody(line: 8)
 
-func drawIceCreamStick(line: Int){
-    for draw in 1...line{
-    print(draw, "      | |")
+func snacksStick(length: Int, stick: String) {
+    for _ in 1...length {
+        print("\(stick)")
     }
 }
-drawIceCreamStick(line: 4)
+
+print("길이: 10")
+print("몸통: ***")
+print("토핑: ")
+print("막대길이: 4"); print()
+snacksBody(length: 10, body: "***", topping:" ")
+snacksStick(length: 4, stick: " | |"); print()
+
+print("길이: 12")
+print("몸통: ***")
+print("토핑: &")
+print("막대길이: 4"); print()
+snacksBody(length: 12, body: "***", topping:"&")
+snacksStick(length: 4, stick: " | |"); print()
+
+print("길이: 12")
+print("몸통: ***")
+print("토핑: #")
+print("막대길이: 6"); print()
+snacksBody(length: 12, body: "***", topping:"#")
+snacksStick(length: 6, stick: " | |"); print()
+
+print("길이: 6")
+print("몸통: |0|")
+print("토핑: ")
+print("막대길이: 4"); print()
+snacksBody(length: 6, body: " |0|", topping:"")
+snacksStick(length: 4, stick: " | |"); print()
