44f06312b6eb34b9406f334f9b1ce63412adc63a
uemu <kim-youngmin@uemu-ui-MacBookAir.local>
2023-07-20 04:19:27 +0900

    Feat: 피드백 반영

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,29 +1,6 @@
 import Foundation
 
-print("길이: 10")
-print("몸통: ***")
-print("토핑: ")
-print("막대길이: 4"); print()
-snacksBody(length: 10, body: "***", topping:" ")
-snacksStick(length: 4, stick: " | |"); print()
-
-print("길이: 12")
-print("몸통: ***")
-print("토핑: &")
-print("막대길이: 4"); print()
-snacksBody(length: 12, body: "***", topping:"&")
-snacksStick(length: 4, stick: " | |"); print()
-
-print("길이: 12")
-print("몸통: ***")
-print("토핑: #")
-print("막대길이: 6"); print()
-snacksBody(length: 12, body: "***", topping:"#")
-snacksStick(length: 6, stick: " | |"); print()
-
-print("길이: 6")
-print("몸통: |0|")
-print("토핑: ")
-print("막대길이: 4"); print()
-snacksBody(length: 6, body: " |0|", topping:"")
-snacksStick(length: 4, stick: " | |"); print()
+printSnacks(bodyLength: 10, body: " ***", topping: "", stickLength: 4, stick: " | |")
+printSnacks(bodyLength: 12, body: "***", topping: "&", stickLength: 4, stick: " | |")
+printSnacks(bodyLength: 12, body: "***", topping: "#", stickLength: 6, stick: " | |")
+printSnacks(bodyLength: 6, body: " |0|", topping: "", stickLength: 4, stick: " | |")
--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -7,14 +7,24 @@
 
 import Foundation
 
-func snacksBody(length: Int, body: String, topping: String) {
-    for _ in 1...length {
+func makeSnacksBody(bodyLength: Int, body: String, topping: String) {
+    for _ in 1...bodyLength {
         print("\(topping)\(body)\(topping)")
     }
 }
 
-func snacksStick(length: Int, stick: String) {
-    for _ in 1...length {
+func makeSnacksStick(stickLength: Int, stick: String) {
+    for _ in 1...stickLength {
         print("\(stick)")
     }
+}
+
+
+func printSnacks(bodyLength: Int, body: String, topping: String, stickLength: Int, stick: String) {
+    print("길이: \(bodyLength)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이:\(stickLength)")
+    makeSnacksBody(bodyLength: bodyLength, body: body, topping: topping)
+    makeSnacksStick(stickLength: stickLength, stick: stick )
 }
