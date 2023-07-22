5512f13ca7dc004c4ca41ad4e3934623f3d71030
greatness3991 <toon_3@naver.com>
2023-07-22 21:17:14 +0900

    Rename: STEP2-> main프로젝트 파일

--- CodeStarterCamp_Week1/STEP2.swift
+++ CodeStarterCamp_Week1/STEP2.swift
@@ -1,8 +1,17 @@
 //
-//  STEP1.swift
+//  step2.swift
 //  CodeStarterCamp_Week1
-//
 //  Created by Greatness on 2023/07/19.
 //
 
-import Foundation
+func printIceCream(height : Int) {
+    for _ in 1...height {
+        print("***********")
+    }
+}
+
+func printIceStick(height : Int) {
+    for _ in 1...height {
+        print("    | |    ")
+    }
+}
