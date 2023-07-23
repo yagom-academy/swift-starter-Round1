e7957eead75a79109912c6037e9dfc5f83e0c7ea
greatness3991 <toon_3@naver.com>
2023-07-23 15:10:06 +0900

    Refactor: 네이밍 수정 및 컨벤션 수정

--- CodeStarterCamp_Week1/STEP3.swift
+++ CodeStarterCamp_Week1/STEP3.swift
@@ -7,37 +7,38 @@
 //
 
 // 빼빼로 막대 길이 및 모양 출력함수
-func printPepeHandleStick(Height: Int, Shape: String) {
-    for _ in 1...Height {
-        print(Shape)
+func printPepeHandleStick(height: Int, shape: String) {
+    for _ in 1...height {
+        print(shape)
     }
 }
 
 // 빼빼로 몸통 재료 및 토핑 반환 함수
-func printPepeBody(bodyMaterial: String, bodyTopping: String) -> String {
-    return bodyTopping + bodyMaterial + bodyTopping
+func printPepeBody(bodyMaterial: String, topping: String) -> String {
+    return topping + bodyMaterial + topping
 }
 
 // 뺴빼로 몸통의 길이 및 재료를 출력 함수
-func printPepeBodyHeight(bodyHeight: Int, bodyMaterial: String, bodyTopping: String) {
+func printPepeBodyHeight(bodyHeight: Int, bodyMaterial: String, topping: String) {
     for _ in 1...bodyHeight {
-        print(printPepeBody(bodyMaterial: bodyMaterial, bodyTopping: bodyTopping))
+        print(printPepeBody(bodyMaterial: bodyMaterial, topping: topping))
         
     }
 }
 
 // 작성한 함수들의 매개변수를 활용하여 뺴빼로 전체 출력함수
-func printWholePepe(stickHeight: Int, stickShape: String, bodyMaterial: String, bodyTopping: String=" ", bodyHeight: Int) {
+func printWholePepe(stickHeight: Int, stickShape: String, bodyMaterial: String, topping: String=" ", bodyHeight: Int) {
     print("""
   <정보>
   길이: \(bodyHeight)
   몸통: \(bodyMaterial)
-  토핑: \(bodyTopping)
+  토핑: \(topping)
   막대길이: \(stickHeight)
   """)
+    
     printPepeBodyHeight(bodyHeight: bodyHeight,
                    bodyMaterial: bodyMaterial,
-                   bodyTopping: bodyTopping)
-    printPepeHandleStick(Height: stickHeight,
-                    Shape: stickShape)
+                   topping: topping)
+    printPepeHandleStick(height: stickHeight,
+                    shape: stickShape)
 }
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -11,10 +11,10 @@
 //예제1 빼빼로 토핑X
 printWholePepe(stickHeight: 4, stickShape: " | |", bodyMaterial: "***",  bodyHeight: 10)
 //예제2 빼빼로
-printWholePepe(stickHeight: 4, stickShape: " | |", bodyMaterial: "***", bodyTopping: "&", bodyHeight: 12)
+printWholePepe(stickHeight: 4, stickShape: " | |", bodyMaterial: "***", topping: "&", bodyHeight: 12)
 //예제3 뺴뱨로 토핑X
 printWholePepe(stickHeight: 6, stickShape: " | |", bodyMaterial: "***",  bodyHeight: 12)
 //예제4 뺴빼로 토핑X
 printWholePepe(stickHeight: 4, stickShape: " | |", bodyMaterial: "|0|",  bodyHeight: 6)
 //나만의 뺴빼로
-printWholePepe(stickHeight: 3, stickShape: " [ ]", bodyMaterial: "###", bodyTopping: "|", bodyHeight: 6)
+printWholePepe(stickHeight: 3, stickShape: " [ ]", bodyMaterial: "###", topping: "|", bodyHeight: 6)
