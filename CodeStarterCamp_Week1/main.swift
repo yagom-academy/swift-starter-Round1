0948ea9aa9652f35dc3759698894e61ebe3cb170
현수 <mm083434@naver.com>
2023-07-30 12:14:55 +0900

    중괄호 앞 spacing 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,20 +1,20 @@
 //Week1 Step3
 import Foundation
 var bodyWithTopping : String = ""
-func drawStick(length: Int){
-    for _ in 1...length{
+func drawStick(length: Int) {
+    for _ in 1...length {
         print(" | | ")
     }
 }
 func drawBodyAndTopping(body: String, topping: String) {
     bodyWithTopping = topping + body + topping
 }
-func drawBody(length: Int){
-    for _ in 1...length{
+func drawBody(length: Int) {
+    for _ in 1...length {
         print(bodyWithTopping)
     }
 }
-func makePepero(bodyLength: Int, body: String, topping: String, stickLength: Int){
+func makePepero(bodyLength: Int, body: String, topping: String, stickLength: Int) {
     
     print("<정보>\n길이: \(bodyLength) \n몸통: \(body)\n토핑: \(topping)\n막대길이: \(stickLength)\n")
     drawBodyAndTopping(body: body, topping: topping)
