34612333043d40e90e86810845e7b392a4f322a2
y0unghyun <ikim4127@gmail.com>
2023-08-16 10:40:52 +0900

    Step2 아이스크림 그리기와 순서도 업데이트

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,7 +6,16 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+func drawIceCream(creamLength: Int) {
+    for _ in 1...creamLength{
+        print("***********")
+    }
+} // 아이스크림을 * 11개로 8줄 출력
+func drawIceCreamBar(barLength: Int) {
+    for _ in 1...barLength{
+        print("    | |    ")
+    }
+} // 막대기를 4줄 출력
 
-print("Hello, World!")
-
+drawIceCream(creamLength: 10)
+drawIceCreamBar(barLength: 4)
