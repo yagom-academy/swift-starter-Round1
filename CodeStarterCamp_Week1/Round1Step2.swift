16be1b371771b4c603e71ac9e1cbfbb3a1bea7c0
Martin Jeong <slowkuma@gmail.com>
2023-08-16 21:27:51 +0900

    Refactoring Round 1 Step 2. * Create function repeatRowString * Reuse repeatRowString * Change variable name

--- CodeStarterCamp_Week1/Round1Step2.swift
+++ CodeStarterCamp_Week1/Round1Step2.swift
@@ -9,33 +9,34 @@ import Foundation
 
 // 아이스크림 부분을 그리는 함수
 func printIceCream() {
-    let repeatCharacter = "*"
-    let characterCount = 11
+    let rowString = String(repeating: "*", count: 11)
     let rowCount = 8
+    print(repeatRowString(rowString, count: rowCount))
 
-    var result = ""
-    for i in 1...rowCount {
-        result += String(repeating: repeatCharacter, count: characterCount)
-        if i != rowCount {
-            result += "\n"
-        }
-    }
-
-    print(result)
 }
 
 // 막대기(손잡이) 부분을 그리는 함수
 func printIceCreamBar() {
     let rowString = "    | |"
     let rowCount = 4
+    print(repeatRowString(rowString, count: rowCount))
+}
+
+// MARK: - File Private
+
+// 정해진 문자열을 줄바꿈 포함하여 반복하여 반환하며 마지막은 줄바꿈이 없음
+fileprivate func repeatRowString(_ repeating: String, count: Int) -> String {
+    if count < 1 {
+        return ""
+    }
 
     var result = ""
-    for i in 1...rowCount {
-        result += rowString
-        if i != rowCount {
+    for rowNumber in 1...count {
+        result += repeating
+        if rowNumber != count {
             result += "\n"
         }
     }
 
-    print(result)
+    return result
 }
