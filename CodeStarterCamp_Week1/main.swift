a237093a618b010d6b7d4be5a7900b1ce4cd928d
y0unghyun <ikim4127@gmail.com>
2023-08-17 07:44:34 +0900

    Step3. 주문에 따른 빼빼로 그리기, 순서도 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,17 +5,25 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
+/*
+ 
 func drawIceCream(length: Int) {
     for _ in 1...length {
         print("***********")
     }
-} // 아이스크림을 * 11개로 length 만큼 반복 출력
+}
 func drawBar(length: Int) {
     for _ in 1...length {
         print("    | |    ")
     }
-} // 막대기를 length 만큼 반복 출력
+}
 
 drawIceCream(length: 15)
 drawBar(length: 4)
+
+ */
+
+
+// 주문에 따른 빼빼로 만들기 시작
+
+drawPpeppero(length: 14, body: "&&&", barLength: 3, topping: "*")
