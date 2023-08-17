eee4bf33247afac629ffb70551d908000fa9173a
SAY-Hong <bobseahee@gmail.com>
2023-08-17 20:57:45 +0900

    step2 ver1

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,6 +7,21 @@
 //
 
 import Foundation
+import Swift
 
-print("Hello, World!")
+//단 수를 입력하면 아이스크림 부분을 그리는 함수
+func drawIcecream(dan: Int){
+    for count in 1...dan{
+        print("***********")
+    }
+}
+//막대 len 입력하면 막대 부분 그리는 함수
+func drawBar(len: Int){
+    for count in 1...len{
+        print("    | |    ")
+    }
+}
 
+//아이스크림 그리기
+drawIcecream(dan: 8)
+drawBar(len: 4)
