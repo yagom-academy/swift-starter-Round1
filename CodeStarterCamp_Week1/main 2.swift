3e5244cb002d07de652b8e9968b124ede502630b
jiwan-swfit <yoojiwan@icloud.com>
2023-08-17 17:15:35 +0900

    Week1 [STEP2] 지완

---
+++ CodeStarterCamp_Week1/main 2.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  CodeStarProject
+//
+//  Created by 유지완 on 2023/08/17.
+//
+
+import Foundation
+
+// *** 함수
+func Star(cont: Int){
+    print("***********")
+}
+
+// 막대기 함수
+func Stin(xont: Int){
+    print("    | |")
+}
+
+for cont in 1...7{
+    Star(cont: cont)
+}
+for xont in 1...4{
+    Stin(xont: xont)
+}
