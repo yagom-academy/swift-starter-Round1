77e357c591eefc4078d8cd1428fafee96b649414
jjwon2149 <45623603+jjwon2149@users.noreply.github.com>
2023-08-19 16:27:21 +0900

    Week1 [STEP 1] smith

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,21 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+func IceCream (){
+    for _ in 0..<8{
+        for _ in 0..<11 {
+            print("*",terminator: "") //출력후 마지막에 덧붙힐 ""하여 *끼리 붙여서 출력
+        }
+        print() //*한줄 완성시 줄바꿈
+    }
+}
+
+func IceCreamBar () {
+    for _ in 0..<4{
+        print("    ||    ")
+    }
+}
+
+IceCream()
+IceCreamBar()
+
 
