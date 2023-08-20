21b2232da8e80b36083bfbd7354823caeb9c8bc5
Camilla S. Park <camilla@Camillas-Air.skbroadband>
2023-08-21 04:53:02 +0900

    AddedPepero

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,19 +9,38 @@
 import Foundation
 
 
-func icecream() {
-    print("***********")
-}
-for ic in 1...8{
-    icecream()
+func  printPeperoToppingwithBody(bodyShape: String, topping: String = "") {
+    print("\(topping)\(bodyShape)\(topping)")
 }
 
+func printPeperoBody(bodyLength: Int, bodyShape: String, topping: String) {
+    for _ in 0...bodyLength {
+        printPeperoToppingwithBody(bodyShape: bodyShape, topping: topping)
+    }
+}
 
-func stick() {
-    print("    | |    ")
+func printPeperoStick(stickLength: Int, topping: String = "") {
+    let stickWithOutTopping = "| |"
+    let stickWithTopping = " | | "
+    for _ in 0...stickLength {
+        if topping == "" {
+            print(stickWithOutTopping)
+        } else {
+            print(stickWithTopping)
+        }
+    }
 }
-for sc in 1...4 {
-    stick()
+
+func printPepero(stickLength: Int, bodyLength: Int, shape: String, topping: String = "") {
+    print("\n<정보>\n길이: \(bodyLength)\n몸통: \(shape)\n토핑: \(topping)\n막대길이: \(stickLength)")
+    printPeperoBody(bodyLength: bodyLength, bodyShape: shape, topping: topping)
+    printPeperoStick(stickLength: stickLength, topping: topping)
 }
+
+printPepero(stickLength: 3, bodyLength: 10, shape: "***")
+printPepero(stickLength: 3, bodyLength: 12, shape: "***", topping: "&")
+printPepero(stickLength: 5, bodyLength: 12, shape: "***", topping: "#")
+printPepero(stickLength: 3, bodyLength: 6, shape: "|0|")
+printPepero(stickLength: 4, bodyLength: 10, shape: "|0|", topping: "+")
 
 
