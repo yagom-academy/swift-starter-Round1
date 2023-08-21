223793758b62642f13d041f5f7945719343c9583
jiwowow <wldn6809@gmail.com>
2023-08-22 00:49:45 +0900

    Step 2 - 아이스크림 그리기와 순서도 작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawIceCream() {
+    for column in 1...8 {
+        for row in 1...11 {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
 
+func drawIceCreamStick() {
+    for column in 1...4 {
+        print("    ", terminator: "")
+        for row in 1...2 {
+            print("| ",terminator: "")
+        }
+        print("")
+    }
+}
+
+drawIceCream()
+drawIceCreamStick()
