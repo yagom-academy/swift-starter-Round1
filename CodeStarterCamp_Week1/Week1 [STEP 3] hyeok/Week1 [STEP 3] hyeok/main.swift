946786340f0e749191f74aa239ca3871b31c5fc5
SimJaeHyeok <jaehyeoking@gmail.com>
2023-08-22 01:17:19 +0900

    Week1 [STEP3] Update

---
+++ CodeStarterCamp_Week1/Week1 [STEP 3] hyeok/Week1 [STEP 3] hyeok/main.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+
+func makepeperoHandle(length: Int, body: String) {
+    let handleExtent = 3
+    var handleBlank = " "
+    if body.count >= 4 {
+        for _ in 0..<body.count - handleExtent{
+            handleBlank += " "
+        }
+        for _ in 0..<length {
+            print(" |\(handleBlank)|")
+        }
+    }else if body.count == 2{
+        for _ in 0..<length {
+            print(" ||")
+        }
+    }else if body.count == 1{
+        for _ in 0..<length {
+            print("| |")
+        }
+    }
+}
+
+func peperoBodyToping(body: String, toping: String = " ") {
+    print("\(toping)\(body)\(toping)")
+    
+}
+
+func bodyLength(length: Int, body: String, toping: String) {
+    for _ in 0..<length {
+        peperoBodyToping(body: body, toping: toping)
+
+    }
+}
+
+func makepepero(length: Int, body: String, toping: String, handle: Int) {
+    print("""
+          <정보>
+          길이: \(length)
+          몸통: \(toping)\(body)\(toping)
+          토핑: \(toping)
+          막대길이: \(handle)
+          
+          """)
+    bodyLength(length: length , body: body, toping: toping)
+    makepeperoHandle(length: handle,body: body)
+    
+}
+
+makepepero(length: 8, body: "*", toping: "&", handle: 4)
