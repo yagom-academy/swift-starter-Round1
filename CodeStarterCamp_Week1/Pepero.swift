ae6acca6464f50aa958d79de3d288a26316ae2fa
chaloopy <sorrowsi93@naver.com>
2023-08-21 13:08:03 +0900

    update pepero functions

--- CodeStarterCamp_Week1/Pepero.swift
+++ CodeStarterCamp_Week1/Pepero.swift
@@ -7,31 +7,27 @@
 
 import Foundation
 
-//빼빼로 전체
-func makePePero(body: String, topping: String, bodyLength: Int, stickLength: Int) {
+func makePePero(body: String, topping: String, bodyLength: Int, stick: String, stickLength: Int) {
     print("길이 :\(bodyLength) \n몸통 : \(body) \n토핑 : \(topping) \n막대길이: \(stickLength)")
     
     makePeperoBody(length: bodyLength, body: body, topping: topping)
     
-    makePeperoStick(length: stickLength)
+    makePeperoStick(length: stickLength, stick: stick)
 }
 
-//빼빼로 몸통에 토핑 더하기
 func drawPepero(body: String, topping: String) {
     print(topping + body + topping, terminator: "\n")
 }
 
-//토핑이 더해진 몸통 길이 추가
 func makePeperoBody(length: Int, body: String, topping: String) {
     for _ in 1...length {
         drawPepero(body: body, topping: topping)
     }
 }
 
-//막대 길이 추가
-func makePeperoStick(length: Int) {
+func makePeperoStick(length: Int, stick: String) {
     for _ in 1...length {
-        print(" | |")
+        print(stick)
     }
 }
 
