08f1562801fc0d3640ec78b92494ee75d2f6ff97
tony-yun <lifeyun24@naver.com>
2023-08-21 17:53:50 +0900

    modified commit for step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,15 +1,14 @@
-func icecreamBody(x: String) -> String {
-    let str = String(repeating: x, count: 11)
-    return str
-}
-
-for row in 1...12 {
-    if row <= 8 {
-        let result = icecreamBody(x: "*")
-        print(result)
-        // 몸통 부분 그리기
-    } else {
-        print("    | |    ")
-        // 스틱 부분 그리기
+func drawIceCreamPart() {
+    for _ in 1...8 {
+        print("***********")
     }
 }
+
+func drawStick() {
+    for _ in 1...4 {
+        print("    | |    ")
+    }
+}
+
+drawIceCreamPart()
+drawStick()
