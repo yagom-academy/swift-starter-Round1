231d0808591b51b03dcd2ec4adefdaf179f4929a
jeongwook <jwpark916@gmail.com>
2023-08-22 00:48:37 +0900

    STEP 3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+/*
 // Week1 [STEP 2]
 // 아이스크림 부분을 그리는 함수
 func printIceCream() {
@@ -25,3 +25,21 @@ func printBar() {
 // 아이스크림, 막대기 함수 호출
 printIceCream()
 printBar()
+ */
+
+// Week1 [STEP 3]
+// printPepero.swift
+
+// 실행 예시 1 (일반 빼빼로 1)
+printPepero(length: Int(10), body: "***", topping: " ", barLength: Int(4))
+
+// 실행 예시 2 (일반 빼빼로 2)
+printPepero(length: Int(12), body: "***", topping: "&", barLength: Int(4))
+
+// 실행 예시 3 (일반 빼빼로 3)
+printPepero(length: Int(12), body: "***", topping: "#", barLength: Int(6))
+
+// 실행 예시 4 (누드 빼빼로)
+printPepero(length: Int(6), body: "|0|", topping: " ", barLength: Int(4))
+
+
