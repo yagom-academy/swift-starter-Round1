b8ce9219d1c93d135b9c542e9ee620e59264084f
lsm23456 <lsm23456@naver.com>
2023-08-22 16:22:22 +0900

    아이스크림 그리기 함수 작성 및 순서도 작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawHard(x : Int, y: Int){
+    for yCount in 1...y {
+        for xCount in 1...x{
+            print("*",terminator: "")
+        }
+        print("")
+    }
+}
+
+func drawBar(x : Int, y:Int){
+    for yCount in 1...y/2{
+        for xCount in 1...(x-3)/2{
+            print(" ",terminator:"")
+        }
+        print("| |",terminator:"")
+        for count in 1...(x-3)/2{
+            print(" ", terminator: "")
+        }
+        print("")
+    }
+    
+}
+
+drawHard(x: 11, y: 10)
+drawBar(x: 11, y: 10)
+
+
 
