efbccae2def9670e5aee256e76f65606199bd08f
doyeonjeong <debby_@kakao.com>
2023-08-23 05:54:12 +0900

    🐛[Fix]: 불필요한 매개변수 제거

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,28 +8,5 @@
 
 import Foundation
 
-func makeIceCream(width: Int) {
-    makeIceCreamBody(width: width)
-    let blank = (width / 2) - 1
-    makeIceCreamStick(blank: blank)
-}
-
-func makeIceCreamBody(width: Int) {
-    for _ in 0..<8 {
-        print(String(repeating: "*", count: width))
-    }
-}
-
-func makeIceCreamStick(blank: Int) {
-    let blank = String(repeating: " ", count: blank)
-    print(String(repeating: "\(blank)| |\n", count: 4))
-}
-
-print("====== 11만큼의 크기를 가진 아이스크림 만들기 ======\n")
-makeIceCream(width: 11)
-
-print("====== 14만큼의 크기를 가진 아이스크림 만들기 ======\n")
-makeIceCream(width: 14)
-
-print("====== 17만큼의 크기를 가진 아이스크림 만들기 ======\n")
-makeIceCream(width: 17)
+let maker = IceCreamMaker()
+maker.makeIceCream()
