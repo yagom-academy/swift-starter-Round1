bbda5ab8f67ecfc649f0f32d3dfe0824bddaf9a3
doyeonjeong <debby_@kakao.com>
2023-08-23 17:52:11 +0900

    [Update] STEP3: 커스텀 빼빼로 만들기
    - 불필요한 주석 삭제
    - stick 만들 때 앞에 필요한 공백 모두 넣기
      : 출력할 때에는 부가적인 과정이 들어가지 않도록 하기 위함

--- CodeStarterCamp_Week1/STEP3/PeperoMaker.swift
+++ CodeStarterCamp_Week1/STEP3/PeperoMaker.swift
@@ -12,14 +12,7 @@ import Foundation
  1. 길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
  2. 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
  3. 길이(Int)에 따라 몸통을 그리는 함수
- 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
- 다양한 빼빼로를 출력해봅시다.
- 실행 예시의 정보(길이, 몸통, 토핑, 막대길이)를 함께 출력합니다.
- 실행 예시의 빼빼로가 모두 출력되어야 합니다.
- 자신만의 토핑, 몸통, 막대 등을 만들어 출력해봅시다.
- 지금까지 만들었던 함수의 흐름을 생각하며 순서도를 작성해봅시다.
- 순서도의 각 칸을 작성할 땐 코드가 아닌 문장 형태로 작성합니다.
- 생성한 순서도를 폴더에 함께 첨부해주세요.
+ 4. 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
  */
 
 final class PeperoMaker {
@@ -53,10 +46,10 @@ final class PeperoMaker {
         let middleWhiteSpaceString = String(repeating: " ", count: width - 2 - bodyWhiteSpacingCount)
         
         /* 모든 공백을 제외한 만큼의 스틱 문자열 */
-        let stick = basicBodyWhiteSpaceString + "|" + middleWhiteSpaceString + "|"
+        let stick = basicBodyWhiteSpaceString + frontWhiteSpaceString + "|" + middleWhiteSpaceString + "|"
         
         for _ in 0 ..< height {
-            print(frontWhiteSpaceString + stick)
+            print(stick)
         }
     }
     
