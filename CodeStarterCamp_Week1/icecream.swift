9a01fe7310107cdba57566d8cc123e792915f5d5
DTS <rlgus621@naver.com>
2023-08-25 00:21:55 +0900

    step3 과제 제출

---
+++ CodeStarterCamp_Week1/icecream.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+func drawIceCream(x: Int, y: Int) {
+    for _ in 1...y {
+        for _ in 1...x {
+            print("*", terminator: "")
+        }
+        print()
+    }
+}
+
+func drawBarSpace(x: Int) {
+    for _ in 0...x/3 {
+        print(" ", terminator: "")
+    }
+    
+}
+
+func drawIceCreamBar(x: Int) {
+    for _ in 1...4 {
+        drawBarSpace(x: x)
+        print("| |")
+    }
+}
+
+//drawIceCream(x: 8, y: 10)
+//drawIceCreamBar(x: 8)
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,35 +1,77 @@
 //
 //  main.swift
-//  CodeStarterCamp_Week1
+//  Pepero
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by RammerSon on 2023/08/23.
 //
 
 import Foundation
 
-func drawIceCream(x: Int, y: Int) {
-    for _ in 1...y {
-        for _ in 1...x {
-            print("*", terminator: "")
-        }
-        print()
+func drawPepero(length: Int, topping: String) {
+    for _ in 1...length {
+        drawPeperoLine(topping: topping)
     }
 }
 
-func drawBarSpace(x: Int) {
-    for _ in 0...x/3 {
-        print(" ", terminator: "")
-    }
-    
+func drawPeperoLine(topping: String){
+    print(topping, terminator: "")
+    print("*", terminator: "")
+    print("*", terminator: "")
+    print("*", terminator: "")
+    print(topping)
 }
 
-func drawIceCreamBar(x: Int) {
-    for _ in 1...4 {
-        drawBarSpace(x: x)
-        print("| |")
+func drawNudePepero(length: Int) {
+    for _ in 1...length {
+        drawNudePeperoLine()
     }
 }
 
-drawIceCream(x: 11, y: 8)
-drawIceCreamBar(x: 11)
+func drawNudePeperoLine() {
+    print(" ", terminator: "")
+    print("|", terminator: "")
+    print("0", terminator: "")
+    print("|", terminator: "")
+    print()
+}
+
+
+func drawBottom(length: Int) {
+    for _ in 1...length {
+        drawBottomLine()
+    }
+}
+
+func drawBottomLine() {
+    print(" ", terminator: "")
+    print("|", terminator: "")
+    print(" ", terminator: "")
+    print("|", terminator: "")
+    print(" ")
+}
+
+//일반빼빼로
+drawPepero(length: 10, topping: " ")
+drawBottom(length: 4)
+print()
+
+//&빼빼로
+drawPepero(length: 12, topping: "&")
+drawBottom(length: 4)
+print()
+
+//#빼빼로
+drawPepero(length: 12, topping: "#")
+drawBottom(length: 6)
+print()
+
+//누드빼빼로
+drawNudePepero(length: 6)
+drawBottom(length: 4)
+print()
+
+//커스텀빼빼로
+drawPepero(length: 12, topping: "W")
+drawBottom(length: 4)
+print()
+
