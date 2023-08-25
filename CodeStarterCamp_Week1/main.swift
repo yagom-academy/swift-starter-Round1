e7d150884b56d46416f57cde43a88d7a26c1a213
bangMinji98 <whoru9806@gmail.com>
2023-08-26 01:41:21 +0900

    week1_step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,25 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+func icecream() {
+    for _ in 1...11 {
+        print("*", terminator: "")
+    }
+    print()
+}
 
-import Foundation
+func icebar() {
+    for _ in 1...4 {
+        print(" ", terminator: "")
+    }
+    print("| |")
+    for _ in 5...7 {
+        print(" ", terminator: "")
+    }
+    print()
+}
 
-print("Hello, World!")
+for _ in 1...8 {
+    icecream()
+}
 
+for _ in 1...4 {
+    icebar()
+}
