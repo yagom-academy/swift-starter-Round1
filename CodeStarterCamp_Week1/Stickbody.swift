48ce3cffc1e64b550f6099bb4b3694622f4986f6
jiwan-swfit <yoojiwan@icloud.com>
2023-08-27 12:14:54 +0900

    Week[STEP3]yoojiwan

---
+++ CodeStarterCamp_Week1/Stickbody.swift
@@ -0,0 +1,23 @@
+//
+//  Stickbody.swift
+//  CodeStarProject
+//
+//  Created by 유지완 on 2023/08/24.
+//
+
+import Foundation
+
+
+// 막대길이 4
+func createBodyStick4(stickbody : Int = 4){
+    for _ in 1...stickbody{
+        print(" | |")
+    }
+}
+
+// 막대길이 6
+func createBodyStick6(stickbody : Int = 6){
+    for _ in 1...stickbody{
+        print(" | |")
+    }
+}
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,24 +1,57 @@
 //
-//  ice.swift
+//  main.swift
 //  CodeStarProject
 //
-//  Created by 유지완 on 2023/08/24.
+//  Created by 유지완 on 2023/08/21.
 //
 
 import Foundation
 
 
-func createBodyIceCream(){
-    for _ in 1...8{
-        print("***********")
-    }
-}
-func createStick(){
-    for _ in 1...4{
-        print("    | |")
+// 실행 예시1
+func createBodyPepero1(length: Int = 10, body: String = "***"){
+    for _ in 1...length{
+        print(" \(body)")
     }
 }
 
-createBodyIceCream()
-createStick()
+// 실행 예시2
+func createBodyPepero2(length: Int = 12, body: String = "***",topping : String = "&"){
+    for _ in 1...length{
+        print("\(topping)\(body)\(topping)")
+    }
+}
 
+// 실행 예시3
+func createBodyPepero3(length: Int = 12, body: String = "***",topping : String = "#"){
+    for _ in 1...length{
+        print("\(topping)\(body)\(topping)")
+    }
+}
+
+// 실행 예시4
+func createBodyPepero4(length: Int = 6, body: String = "|0|"){
+    for _ in 1...length{
+        print(" \(body)")
+    }
+}
+
+// 실행 예시1 출력
+print("=exampleOne=")
+createBodyPepero1()
+createBodyStick4()
+
+// 실행 예시2 출력
+print("=exampleTwo=")
+createBodyPepero2()
+createBodyStick4()
+
+// 실행 예시3 출력
+print("=exampleThree=")
+createBodyPepero3()
+createBodyStick6()
+
+// 실행 예시4 출력
+print("=exampleFour=")
+createBodyPepero4()
+createBodyStick4()
