b867cc05d9d3482083be463d5304b8d3e62e13c8
bangMinji98 <whoru9806@gmail.com>
2023-08-28 20:08:11 +0900

    Fix:코멘트 받은 후 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,25 +1,31 @@
-func icecream() {
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 콩지.
+//  Copyright © yagom academy. All rights reserved.
+//
+import Foundation
+
+func drawIcecream() {
     for _ in 1...11 {
         print("*", terminator: "")
     }
-    print()
+    print("", terminator: "\n") 
 }
 
-func icebar() {
+func drawIcebar() {
     for _ in 1...4 {
         print(" ", terminator: "")
     }
     print("| |")
-    for _ in 5...7 {
-        print(" ", terminator: "")
-    }
-    print()
+    print("", terminator: "\n")
 }
 
 for _ in 1...8 {
-    icecream()
+    drawIcecream()
 }
 
 for _ in 1...4 {
-    icebar()
+    drawIcebar()
 }
