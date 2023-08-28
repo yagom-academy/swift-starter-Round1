3f9d1786828e094517a1770f67c24c69f550aca5
Synn <synn@Synnui-MacBookAir.local>
2023-08-28 23:00:31 +0900

    ROUND1 STEP3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,23 +8,29 @@
 
 import Foundation
 
-func printIceBar(rows: Int) {
-    for _ in 1...rows {
-        var star = ""
-        for _ in 1...11 {
-            star = star + "*"
-            //star += "*"
-        }
-        print(star)
+func printPeperoInfo(bodyLength: Int, topping: String, stickLength: Int) {
+    print("<정보>", "길이: \(bodyLength)", "몸통: \(peperoBody)", "토핑: \(topping)", "막대길이: \(stickLength)", separator: "\n")
+    print()
+}
+
+func drawPeperoStick(stickLength: Int) {
+    let PeperoStick = " | | "
+    for _ in 1...stickLength {
+        print(PeperoStick)
     }
 }
 
-func printIceStick() {
-    let bar = "    | |    "
-    for _ in 1...4 {
-        print(bar)
+func drawPeperoWithTopping(bodyLength: Int, topping: String) {
+    for _ in 1...bodyLength {
+        print(topping + peperoBody + topping)
     }
 }
 
-printIceBar(rows: 8)
-printIceStick()
+let bodyLength = 6
+let peperoBody = "|0|"
+let topping = " "
+let stickLength = 4
+
+printPeperoInfo(bodyLength: bodyLength, topping: topping, stickLength: stickLength)
+drawPeperoWithTopping(bodyLength: bodyLength, topping: topping)
+drawPeperoStick(stickLength: stickLength)
