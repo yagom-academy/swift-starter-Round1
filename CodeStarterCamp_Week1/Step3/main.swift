8648bdc03a811c283725221209407881d5201a01
black <oilpott12@naver.com>
2023-08-29 21:26:02 +0900

    fix: STEP3 피드백 반영

--- CodeStarterCamp_Week1/Step3/main.swift
+++ CodeStarterCamp_Week1/Step3/main.swift
@@ -1,10 +1,19 @@
-
-
 import Foundation
 
+func showInfo(peperoLength: Int, body: String, barLength: Int) {
+    print("""
+    < 빼빼로 정보 >
+    길이: \(peperoLength)
+    몸통: \(body)
+    토핑:
+    막대길이: \(barLength)\n
+    ------------\n
+    """)
+}
+
 func createPeperoBodyPart(body: String, topping: String) -> String {
-        let bodyPart = topping+body+topping
-        return bodyPart
+    var bodyPart = topping+body+topping
+    return bodyPart
 }
 
 func createPeperoBody(peperoLength: Int, body: String,topping: String) {
@@ -20,71 +29,13 @@ func createBar(barLength: Int) {
 }
 
 func createPepero(barLength: Int, peperoLength: Int, body: String, topping: String) {
-  
+    showInfo(peperoLength: peperoLength, body: body, barLength: barLength)
     createPeperoBody(peperoLength: peperoLength, body: body, topping: topping)
     createBar(barLength: barLength)
+    print("")
 }
 
-print("< 1번 빼빼로 정보 >")
-print("길이: 10")
-print("몸통: *** ")
-print("토핑: ")
-print("막대길이: 4")
-print("")
-var barLength = 4
-var peperoLength = 10
-var body = "***"
-var topping = " "
-
-createPepero(barLength: barLength, peperoLength: peperoLength, body: body, topping: topping)
-
-print("")
-print("----------------------")
-print("")
-
-print("< 2번 빼빼로 정보 >")
-print("길이: 12")
-print("몸통: *** ")
-print("토핑: & ")
-print("막대길이: 4")
-print("")
-
-barLength = 4
-peperoLength = 12
-body = "***"
-topping = "&"
-createPepero(barLength: barLength, peperoLength: peperoLength, body: body, topping: topping)
-
-print("")
-print("----------------------")
-print("")
-
-print("< 3번 빼빼로 정보 >")
-print("길이: 12")
-print("몸통: *** ")
-print("토핑: # ")
-print("막대길이: 6")
-print("")
-
-barLength = 6
-peperoLength = 12
-body = "***"
-topping = "#"
-createPepero(barLength: barLength, peperoLength: peperoLength, body: body, topping: topping)
-
-print("")
-print("----------------------")
-print("")
-
-print("< 4번 빼빼로 정보 >")
-print("길이: 6")
-print("몸통: |0| ")
-print("토핑: ")
-print("막대길이: 4")
-print("")
-
-barLength = 4
-peperoLength = 6
-body = "|0|"
-topping = " "
-createPepero(barLength: barLength, peperoLength: peperoLength, body: body, topping: topping)
+createPepero(barLength: 4, peperoLength: 10, body: "***", topping: " ")
+createPepero(barLength: 4, peperoLength: 12, body: "***", topping: "&")
+createPepero(barLength: 6, peperoLength: 12, body: "***", topping: "#")
+createPepero(barLength: 4, peperoLength: 6, body: "|0|", topping: " ")
