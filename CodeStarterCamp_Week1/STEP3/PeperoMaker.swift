ad4ea657d10cbb9080032ba3eadd1152b15d0dde
doyeonjeong <debby_@kakao.com>
2023-08-29 10:37:08 +0900

    🪄[Style]: 공백 & 네이밍 수정
    - 불필요한 공백 제거
    - 너무 긴 네이밍 짧게 수정
    - 상수와 함수 호출을 구분하기 위한 공백 추가

--- CodeStarterCamp_Week1/STEP3/PeperoMaker.swift
+++ CodeStarterCamp_Week1/STEP3/PeperoMaker.swift
@@ -17,16 +17,18 @@ import Foundation
 
 final class PeperoMaker {
     
-    func customizePePero(bodyHeight: Int, body: String, topping: String = "", stickHeight: Int) {
+    func customizePepero(bodyHeight: Int, body: String, topping: String = "", stickHeight: Int) {
+        
         print("""
         <정보>
         길이: \(bodyHeight)
         몸통: \(body)
         토핑: \(topping)
-        막대길이: \(stickHeight)
-        
+        막대길이: \(stickHeight)\n
         """)
+        
         let oneLineBodyString = makePeperoBodyString(body, with: topping)
+        
         makePeperoBody(bodyHeight, with: oneLineBodyString)
         makePeperoStick(body.count, stickHeight, bodyString: oneLineBodyString, withFrontWhitespace: topping.count)
     }
@@ -34,10 +36,11 @@ final class PeperoMaker {
     private func makePeperoStick(_ width: Int, _ height: Int, bodyString: String, withFrontWhitespace: Int) {
         
         // 토핑의 개수만큼 필요한 공백 문자열
-        let frontWhiteSpaceString = String(repeating: " ", count: withFrontWhitespace)
+        let frontWhiteSpace = String(repeating: " ", count: withFrontWhitespace)
         
         // 만들어진 빼빼로의 몸통의 앞에 공백이 몇개나 있는지 체크
         var bodyWhiteSpacingCount = 0
+        
         for character in bodyString {
             if !character.isWhitespace {
                 // 현재 문자가 공백이 아니라면 즉시 for문 종료
@@ -48,15 +51,15 @@ final class PeperoMaker {
         }
         
         // 빼빼로의 몸통 앞에 있는 공백만큼의 문자열
-        let basicBodyWhiteSpaceString = String(repeating: " ", count: bodyWhiteSpacingCount)
+        let basicWhiteSpace = String(repeating: " ", count: bodyWhiteSpacingCount)
         
         // 가운데에 필요한 공백 문자열
-        let middleWhiteSpaceString = String(repeating: " ", count: width - 2 - bodyWhiteSpacingCount)
+        let middleWhiteSpace = String(repeating: " ", count: width - 2 - bodyWhiteSpacingCount)
         
         // 모든 공백을 제외한 만큼의 스틱 문자열
-        let stick = basicBodyWhiteSpaceString + frontWhiteSpaceString + "|" + middleWhiteSpaceString + "|"
+        let stick = basicWhiteSpace + frontWhiteSpace + "|" + middleWhiteSpace + "|"
         
-        for _ in 0 ..< height {
+        for _ in 0..<height {
             print(stick)
         }
     }
@@ -66,9 +69,8 @@ final class PeperoMaker {
     }
     
     private func makePeperoBody(_ height: Int, with body: String) {
-        for _ in 0 ..< height {
+        for _ in 0..<height {
             print(body)
         }
     }
-    
 }
