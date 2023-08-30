c9c4ddc7800ca0af51d073726a91f32fc79bc938
Synn <synn@Synnui-MacBookAir.local>
2023-08-30 21:02:16 +0900

    WEEK1 STEP3_3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-func printPeperoInfo(bodyLength: Int, topping: String, stickLength: Int) {
+func printPeperoInfo(bodyLength: Int, peperoBody: String, topping: String, stickLength: Int) {
+    print()
     print("<정보>", "길이: \(bodyLength)", "몸통: \(peperoBody)", "토핑: \(topping)", "막대길이: \(stickLength)", separator: "\n")
     print()
 }
@@ -20,17 +21,28 @@ func drawPeperoStick(stickLength: Int) {
     }
 }
 
-func drawPeperoWithTopping(bodyLength: Int, topping: String) {
+func drawPeperoWithTopping(bodyLength: Int, peperoBody: String, topping: String) {
     for _ in 1...bodyLength {
         print(topping + peperoBody + topping)
     }
 }
-
-let bodyLength = 6
-let peperoBody = "|0|"
-let topping = " "
-let stickLength = 4
-
-printPeperoInfo(bodyLength: bodyLength, topping: topping, stickLength: stickLength)
-drawPeperoWithTopping(bodyLength: bodyLength, topping: topping)
-drawPeperoStick(stickLength: stickLength)
+// 실행 예시 1
+printPeperoInfo(bodyLength: 10, peperoBody: "***", topping: " ", stickLength: 4)
+drawPeperoWithTopping(bodyLength: 10, peperoBody: "***", topping: " ")
+drawPeperoStick(stickLength: 4)
+// 실행 예시 2
+printPeperoInfo(bodyLength: 12, peperoBody: "***", topping: "&", stickLength: 4)
+drawPeperoWithTopping(bodyLength: 12, peperoBody: "***", topping: "&")
+drawPeperoStick(stickLength: 4)
+// 실행 예시 3
+printPeperoInfo(bodyLength: 12, peperoBody: "***", topping: "#", stickLength: 6)
+drawPeperoWithTopping(bodyLength: 12, peperoBody: "***", topping: "#")
+drawPeperoStick(stickLength: 6)
+// 실행 예시 4
+printPeperoInfo(bodyLength: 6, peperoBody: "|0|", topping: " ", stickLength: 4)
+drawPeperoWithTopping(bodyLength: 6, peperoBody: "|0|", topping: " ")
+drawPeperoStick(stickLength: 4)
+// 임의 예시
+printPeperoInfo(bodyLength: 10, peperoBody: "***", topping: "s", stickLength: 5)
+drawPeperoWithTopping(bodyLength: 10, peperoBody: "***", topping: "s")
+drawPeperoStick(stickLength: 5)
