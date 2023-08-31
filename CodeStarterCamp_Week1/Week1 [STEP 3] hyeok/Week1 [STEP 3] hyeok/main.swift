05937f2a388edf092ce52406e08b0de5985addb4
SimJaeHyeok <jaehyeoking@gmail.com>
2023-08-31 17:43:39 +0900

    STEP3 UPDATE

--- CodeStarterCamp_Week1/Week1 [STEP 3] hyeok/Week1 [STEP 3] hyeok/main.swift
+++ CodeStarterCamp_Week1/Week1 [STEP 3] hyeok/Week1 [STEP 3] hyeok/main.swift
@@ -1,43 +1,63 @@
 import Foundation
 
-func makePeperoStick(stickLength: Int, body: String) {
-    var peperoStick = " | |"
-    if body.count > 3 {
-        for _ in 0..<body.count - 3 {
-            peperoStick.insert(" ", at: peperoStick.index(peperoStick.startIndex, offsetBy: 3))
-        }
-    }
-    for _ in 0..<stickLength {
-        print(peperoStick)
-    }
-}
+func makePeperoStick(length: Int, body: String) {
+    let leftBlank = String(repeating: " ", count: (body.count - 3) / 2)
+    let stick = leftBlank + " | |"
     
-
-func connectBodyTopping(body: String, topping: String) {
-    print("\(topping)\(body)\(topping)")
-    
+    for _ in 0..<length {
+        print(stick)
+    }
 }
 
-func makePeperoBody(bodyLength: Int, body: String, topping: String) {
-    for _ in 0..<bodyLength {
-        connectBodyTopping(body: body, topping: topping)
+
+func printPeperoBody(length: Int, body: String, topping: String) {
+    for _ in 0..<length {
+        print("\(topping)\(body)\(topping)")
 
     }
 }
 
-func makePepero(bodyLength: Int, body: String, topping: String = " ", stickLength: Int) {
+func informPeperoInfo(bodyLength: Int, body: String, topping: String, stickLength : Int) {
     print("""
           <정보>
           길이: \(bodyLength)
-          몸통: \(topping)\(body)\(topping)
+          몸통: \(body)
           토핑: \(topping)
           막대길이: \(stickLength)
-          
           """)
-    makePeperoBody(bodyLength: bodyLength , body: body, topping: topping)
-    makePeperoStick(stickLength: stickLength, body: body)
+}
+
+
+func makePepero(bodyLength: Int, body: String, topping: String = " ", stickLength: Int) {
+    informPeperoInfo(bodyLength: bodyLength, body: body, topping: topping, stickLength: stickLength)
+    printPeperoBody(length: bodyLength , body: body, topping: topping)
+    makePeperoStick(length: stickLength, body: body)
     
 }
 
-makePepero(bodyLength: 10, body: "*****", topping: "&", stickLength: 4)
+makePepero(bodyLength: 8, body: "***", topping: "&",stickLength: 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
