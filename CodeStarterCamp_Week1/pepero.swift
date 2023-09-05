4f331307eef8fb9cb1da32574ec8ba77f18368ff
lsm23456 <lsm23456@naver.com>
2023-09-05 19:37:29 +0900

    STEP3 빼빼로 만들기 커밋

---
+++ CodeStarterCamp_Week1/pepero.swift
@@ -0,0 +1,42 @@
+//
+//  pepero.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 임성묵 on 2023/09/05.
+//
+
+import Foundation
+
+
+func drawPeperoShape(peperoBody : String, peperoTopping :String) -> String {
+    return "\(peperoTopping)\(peperoBody)\(peperoTopping)"
+}
+
+func drawPeperoBody(peperoBodyLength : Int, peperoShape : String) {
+    
+    for _ in 1...peperoBodyLength {
+        print(peperoShape)
+    }
+}
+
+func drawPeperoBar(peperoBarLength : Int) {
+    for _ in 1...peperoBarLength {
+        print(" | | ")
+    }
+}
+
+func drawPepero(peperoBodyLength : Int, peperoBody : String = "***", peperoTopping : String = " ", peperoBarLength : Int) {
+    var peperoBodyFunction: (String, String) -> String = drawPeperoShape(peperoBody:peperoTopping:)
+    print("<정보>")
+    print("길이: \(peperoBodyLength)")
+    print("몸통: \(peperoBody)")
+    print("토핑: \(peperoTopping)")
+    print("막대길이: \(peperoBarLength)")
+    print("")
+    drawPeperoBody(peperoBodyLength: peperoBodyLength, peperoShape: peperoBodyFunction(peperoBody, peperoTopping))
+    drawPeperoBar(peperoBarLength: 4)
+    print("")
+}
+
+
+
