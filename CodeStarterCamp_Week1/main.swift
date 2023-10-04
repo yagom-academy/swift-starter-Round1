9f05907b504cdce45777a619935a42cd5fb42bc6
marukim <72730841+marukim365@users.noreply.github.com>
2023-10-04 11:02:16 +0900

    Week1 [STEP 2] 김마루 (#753)
    * STEP2.Draw ice cream & edit README.md
    
    * edit function, draw flowchart

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,12 +1,16 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
 
-import Foundation
+func drawIceCream(sizeIceCream: Int) {
+    for _ in 1...sizeIceCream {
+        print("***********")
+    }
+}
 
-print("Hello, World!")
+func drawHolder(sizeHolder: Int) {
+    for _ in 1...sizeHolder {
+        print("    | |    ")
+    }
+}
 
+// 사용자가 원하는 아이스크림과 홀더 사이즈 설정
+drawIceCream(sizeIceCream: 8)
+drawHolder(sizeHolder: 4)
