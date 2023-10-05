171f068acfbe9d4958d826d2cb427ae5335d3497
marukim <sjybext@naver.com>
2023-10-05 23:01:23 +0900

    feat: 빼빼로 그리기 코드 및 순서도 작성
    Week1 STEP3 주문에 따른  빼빼로 그리기
     - 요구사항에 맞게 총 4개의 함수로 코드 작성
     - 함수의 흐름에 따른 순서도 작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,16 +1,31 @@
 
-func drawIceCream(sizeIceCream: Int) {
-    for _ in 1...sizeIceCream {
-        print("***********")
+func drawHolder(length: Int) {
+    for _ in 1...length {
+        print(" | | ")
     }
 }
 
-func drawHolder(sizeHolder: Int) {
-    for _ in 1...sizeHolder {
-        print("    | |    ")
+func choiceBodyAndTopping(body: String, topping: String) -> String {
+    return topping + body + topping
+}
+
+func drawBody(body: String, length: Int) {
+    for _ in 1...length {
+        print(body)
     }
 }
 
-// 사용자가 원하는 아이스크림과 홀더 사이즈 설정
-drawIceCream(sizeIceCream: 8)
-drawHolder(sizeHolder: 4)
+func drawPepero(holderLength: Int, body: String, topping: String = " ", peperoLength: Int) {
+    print("""
+    <정보>
+    길이: \(peperoLength)
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(holderLength)
+    
+    """)
+    drawBody(body: choiceBodyAndTopping(body: body, topping: topping), length: peperoLength)
+    drawHolder(length: holderLength)
+}
+
+drawPepero(holderLength: 4, body: "|0|", topping: "#", peperoLength: 10)
