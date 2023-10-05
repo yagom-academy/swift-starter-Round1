1d9dbd6e2e5ad9974e67d8266b37e0cfb94b7908
shjang <algori6@naver.com>
2023-10-05 20:03:43 +0900

    ROUND 1 [STEP 2] 릴라
    @릴라
    만나뵙게 되서 반갑습니다.
    추석은 즐겁게 보내셨는지요?
    
    ## 고민했던 점
    ### 주제
    - for in 에서 반복문 배열에 접근하는 변수가 사용하지 않을때는
    Immutable value 'column' was never used; consider replacing with '_' or removing it
    위의 문구로 경고가 나는데 언더바(_)로 사용하는게 나을까요 아님 이름을 갖는게 나을지요?
    - 순서도를 처음 그려봤는데 디테일하게 피드백 부탁드려요.
    - 계행안하는 출력함수를 따로 정의해서 사용했습니다.

---
+++ CodeStarterCamp_Week1/Step2.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+import Foundation
+
+
+func drawIceCream(){
+    for column in 1...8 {
+        for row in 1...11 {
+            printNotNewLine(item: "*")
+        }
+        print("")
+    }
+}
+
+func printNotNewLine(item: String){
+    print(item, terminator: "")
+}
+
+func drawStick(){
+    for column in 1...4 {
+        for row in 1...7 {
+            if row == 5 || row == 7 {
+                printNotNewLine(item: "|")
+            } else {
+                printNotNewLine(item: " ")
+            }
+        }
+        print("")
+    }
+}
+
+func drawStickIceCream(){
+    print(drawIceCream() , drawStick())
+}
+
+
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,35 +7,4 @@
 //
 import Foundation
 
-
-func drawIceCream(){
-    for column in 1...8 {
-        for row in 1...11 {
-            printNotNewLine(item: "*")
-        }
-        print("")
-    }
-}
-
-func printNotNewLine(item: String){
-    print(item, terminator: "")
-}
-
-func drawStick(){
-    for column in 1...4 {
-        for row in 1...7 {
-            if row == 5 || row == 7 {
-                printNotNewLine(item: "|")
-            } else {
-                printNotNewLine(item: " ")
-            }
-        }
-        print("")
-    }
-}
-
-func drawStickIceCream(){
-    print(drawIceCream() , drawStick())
-}
-
 print(drawStickIceCream())
