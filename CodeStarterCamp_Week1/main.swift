11ce44795aede2d2df0ce29d782e24565d07eee0
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2023-10-05 15:57:40 +0900

    func 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeIcecream() {
+    for length in 1...8 {
+        for _ in 1...11
+        {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
+
+func makeStick() {
+    for _ in 1...4 {
+        for width in 1...7 {
+            if width == 5 || width == 7 {
+                print("|", terminator: "")
+                
+            }
+            else {
+                print(" ", terminator: "")
+            }
+        }
+        print("")
+    }
+}
+
+makeIcecream()
+makeStick()
 
