94e051b4a95c9e86814406f90202a68f1a289c84
jieun <jieun_0330@naver.com>
2023-10-05 23:04:08 +0900

    [Step 2] 아이스크림 그리기

---
+++ CodeStarterCamp_Week1/Step2_icecream.swift
@@ -0,0 +1,35 @@
+//
+//  Step2_icecream.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 박지은 on 2023/10/05.
+//
+
+import Foundation
+
+
+func icecream() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+func icecreamBar() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+icecream()
+icecreamBar()
+
+
+
+
+/*
+ 순서도
+ 
+ 1. *********** : 8
+ 2.     | | : 4
+ 
+ */
