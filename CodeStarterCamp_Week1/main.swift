28cdee42f0bec1d147c3c8782dc59b258e41c8a7
Chayu <chayou20@naver.com>
2023-10-07 14:16:31 +0900

    step3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-
+// STEP 2
 func drawIceCream(count: Int){
     for x in 1...count{
         print("***********")
@@ -23,3 +23,36 @@ func drawStick(count: Int){
 
 drawIceCream(count: 8)
 drawStick(count: 4)
+
+// STEP 3
+
+func information(length: Int, body: String, toping: String, lengthStick: Int){
+    print("<정보>")
+    print("길이: \(length)")
+    print("몸통: \(body)")
+    print("토핑: \(toping)")
+    print("막대길이: \(lengthStick)")
+    print("")
+}
+
+func drawSnack(length: Int, body: String, toping: String, lengthStick: Int){
+    for x in 1...length{
+        print("\(toping)\(body)\(toping)")
+    }
+    
+    for y in 1...lengthStick{
+        print(" | |")
+    }
+    print("")
+
+}
+
+func makeSnack(len: Int, bo: String, to: String, lenStick: Int){
+    information(length: len, body: bo, toping: to, lengthStick: lenStick)
+    drawSnack(length: len, body: bo, toping: to, lengthStick: lenStick)
+}
+
+makeSnack(len: 10, bo: "***", to: " ", lenStick: 4)
+makeSnack(len: 12, bo: "***", to: "&", lenStick: 4)
+makeSnack(len: 12, bo: "***", to: "#", lenStick: 6)
+makeSnack(len: 6, bo: "|0|", to: " ", lenStick: 4)
---
+++ CodeStarterCamp_Week1/step3.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 유진 on 2023/10/07.
+//
+
+import Foundation
+
+func information(length: Int, body: String, toping: String, lengthStick: Int){
+    print("<정보>")
+    print("길이: \(length)")
+    print("몸통: \(body)")
+    print("토핑: \(toping)")
+    print("막대길이: \(lengthStick)")
+}
+
+information(length: 10, body: "***", toping: "", lengthStick: 4)
