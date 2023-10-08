501e87e7e273533d228ddca06066bb8bf926fdf3
JEYUN <quandiyin@gmail.com>
2023-10-08 20:15:37 +0900

    bbeabbearo

---
+++ CodeStarterCamp_Week1/bbaebbaero.swift
@@ -0,0 +1,34 @@
+//
+//  bbaebbaero.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 전제윤 on 10/8/23.
+//
+
+
+//import Foundation
+//
+//print("<정보>")
+//print("길이: ", terminator: "")
+//let momtong_length = readLine()!
+//
+//print("몸통: ", terminator: "")
+//let momtong_shape = readLine()!
+//
+//print("토핑: ", terminator: "")
+//let toping = readLine()!
+//
+//print("막대길이: ", terminator: "")
+//let bar_length = readLine()!
+//
+//func bbaebbaero() {
+//    for i in 1...Int(momtong_length)! {
+//        print(toping+momtong_shape+toping)
+//    }
+//    
+//    for a in 1...Int(bar_length)! {
+//        print(" | | ")
+//    }
+//}
+//
+//bbaebbaero()
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,29 +8,69 @@
 
 import Foundation
 
-func plusTwo(x: Int) -> Int {
-    return x + 2
-}
+//func plusTwo(x: Int) -> Int {
+//    return x + 2
+//}
+//
+//func icecreamTop() {
+//    for i in 1...11{
+//        print("*", terminator: "")
+//    }
+//}
+//
+//func icecreamBottom() {
+//    print("    | |    ")
+//}
+//
+//for a in 1...8 {
+//    icecreamTop()
+//    print("")
+//}
+//
+//for b in 1...4 {
+//    icecreamBottom()
+//    
+//}
+//
+//var integer: Int = -100
+//var unsignedInteger: UInt = 50
+//
+//print("integer 값: \(integer), unsignedInteger 값: \(unsignedInteger)")
+//
+//var fValue: Float = 1234567890.1
+//
+//print(fValue)
 
-func icecreamTop() {
-    for i in 1...11{
-        print("*", terminator: "")
+
+import Foundation
+
+print("<정보>")
+
+
+print("길이: ", terminator: "")
+let momtong_length = readLine()!
+
+print("몸통: ", terminator: "")
+let momtong_shape = readLine()!
+
+print("토핑: ", terminator: "")
+let toping = readLine()!
+
+print("막대길이: ", terminator: "")
+let bar_length = readLine()!
+
+func bbaebbaero() {
+    for i in 1...Int(momtong_length)! {
+        print(toping+momtong_shape+toping)
+    }
+    
+    for a in 1...Int(bar_length)! {
+        print(" | | ")
     }
 }
 
-func icecreamBottom() {
-    print("    | |    ")
-}
+bbaebbaero()
 
-for a in 1...8 {
-    icecreamTop()
-    print("")
-}
-
-for b in 1...4 {
-    icecreamBottom()
-    
-}
 
 
 
