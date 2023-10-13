47bd1565342618a81a16c285f3172b45d11dffe1
Orderlee <nuncateolvidar3@gmail.com>
2023-10-14 01:00:26 +0900

    pepero 및 순서도 수정

--- CodeStarterCamp_Week1/pepero/pepero/main.swift
+++ CodeStarterCamp_Week1/pepero/pepero/main.swift
@@ -7,36 +7,31 @@ func peperoStick(length: Int) {
 }
 
 // 몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
-func peperoDraw(length: Int, body: String, topping: String) {
+func drawPepero(length: Int, body: String, topping: String) {
     for _ in 1...length {
         print("\(topping)\(body)\(topping)")
     }
 }
 
-// 길이(Int)에 따라 몸통을 그리는 함수
-func peperoBody(length: Int, body: String) {
-    for _ in 1...length {
-        print(body)
-    }
-}
+
 
 // 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
-func pepero(length: Int, body: String, topping: String, bottom: Int) {
+func pepero(length: Int, body: String, topping: String, handLength: Int) {
     print("<정보>")
     print("길이: \(length)")
     print("몸통: \(body)")
     print("토핑: \(topping)")
-    print("막대길이: \(bottom)")
+    print("막대길이: \(handLength)")
     
-    peperoDraw(length: length, body: body, topping: topping)
-    peperoStick(length: bottom)
+    drawPepero(length: length, body: body, topping: topping)
+    peperoStick(length: handLength)
 }
 
 
-pepero(length: 10, body: " ***", topping: "", bottom: 4)
+pepero(length: 10, body: " ***", topping: "", handLength: 4)
 print("\n")
-pepero(length: 12, body: "***", topping: "&", bottom: 4)
+pepero(length: 12, body: "***", topping: "&", handLength: 4)
 print("\n")
-pepero(length: 12, body: "***", topping: "#", bottom: 6)
+pepero(length: 12, body: "***", topping: "#", handLength: 6)
 print("\n")
-pepero(length: 6, body: " |0|", topping: "", bottom: 4)
+pepero(length: 6, body: " |0|", topping: "", handLength: 4)
