ce02a4e678d56b24c4f7cb8b11fde5c413afdb57
UDI <honran.dev.94@gmail.com>
2023-10-14 17:29:44 +0900

    Rename : move file
    프로젝트 과제별 파일 이동

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,18 +5,3 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
-func makeIcecream() {
-    for _ in 1...8 {
-        print ("***********")
-    }
-}
-
-func makeIcecreamBar() {
-    for _ in 1...4 {
-        print ("    | |    ")
-    }
-}
-
-makeIcecream()
-makeIcecreamBar()
