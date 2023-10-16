309dc1bb26c430a29526047011c5e9c319b2502e
lucie <146562581+lucidly-lucie@users.noreply.github.com>
2023-10-16 21:20:17 +0900

    Round 1 [Step 3] lucie (#774)
    * README.md 파일 추가
    
    * README.md 파일 추가
    
    * README.md 파일 추가
    
    * main.swift 파일추
    
    * printIce 함수수정
    
    * printStick 함수수정
    
    * 빼빼로를 그리는 main파일 생성
    
    * 빼빼로를 그리는 순서도 파일 추가
    
    * func printPepero 함수 내 개행 문자 사용으로 변경 및 func drawPeperoStick 내 공백 제거
    
    ---------
    
    Co-authored-by: DuDu <whqtkf12@naver.com>

---
+++ CodeStarterCamp_Week1/icecream.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+func printIce() {
+    for _ in 0..<8 {
+        print("***********")
+    }
+}
+
+func printStick() {
+    for _ in 0..<4 {
+        print("    | |")
+    }
+}
+
+printIce()
+printStick()
+
