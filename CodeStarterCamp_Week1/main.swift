7a92c26e56cfc9c05e283871279a8ac67170bebe
seokyung402 <105649543+seokyung402@users.noreply.github.com>
2023-10-16 18:41:52 +0900

    1주차 수정완료

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,21 +8,28 @@
 
 import Foundation
 
-func makeIceCream() {
-    for length in 1...8 {
-        print(" ", separator: " ", terminator:"")
-        for width in 1...11 {
+func iceCream() {
+    for _ in 1...8 {
+        print(" ", terminator:"")
+        for _ in 1...11 {
             print("*", terminator: "")
         }
         print("")
     }
 }
 
-func makeBar() {
-    for count in 1...4 {
-        print("     | |", separator: "")
+func stick() {
+    for _ in 1...4 {
+        print("     | |")
     }
 }
-makeIceCream()
-makeBar()
 
+//아이스크림만들기
+iceCream()
+stick()
+
+//빼빼로만들기
+pepero(length: 10, body: "***", toping:" ", barLength: 4)
+pepero(length: 12, body: "***", toping:"&", barLength: 4)
+pepero(length: 12, body: "***", toping:"#", barLength: 6)
+pepero(length: 6, body: "|0|", toping:" ", barLength: 4)
--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -6,7 +6,8 @@
 //
 
 import Foundation
-func makeIceCream(length: Int, body: String, toping: String, barLength: Int){
+func pepero(length: Int, body: String,
+                  toping: String, barLength: Int){
     print("<정보>")
     print("길이: \(length)")
     print("몸통: \(body)")
@@ -14,32 +15,23 @@ func makeIceCream(length: Int, body: String, toping: String, barLength: Int){
     print("막대길이: \(barLength)")
     print("")
     
-    drawBody(bodylength: length, body: body, toping: toping)
-    drawBar(barLength: barLength)
+    drawBody(length: length, body: body, toping: toping)
+    drawBar(length: barLength)
 }
 
-
-
-func drawBar(barLength : Int) {
-    for _ in 1...barLength {
+func drawBar(length : Int) {
+    for _ in 1...length {
         print(" | |", separator: "")
     }
     print("")
 }
 
-
-
-func drawBody(bodylength: Int, body: String, toping: String){
+func drawBody(length: Int, body: String, toping: String){
     func drawOneLine(body: String, toping: String) {
         print("\(toping)\(body)\(toping)")
     }
-    for _ in 1...bodylength{
+    for _ in 1...length{
         drawOneLine(body: body, toping: toping)
     }
 
 }
-
-makeIceCream(length: 10, body: "***", toping:" ", barLength: 4)
-makeIceCream(length: 12, body: "***", toping:"&", barLength: 4)
-makeIceCream(length: 12, body: "***", toping:"#", barLength: 6)
-makeIceCream(length: 6, body: "|0|", toping:" ", barLength: 4)
