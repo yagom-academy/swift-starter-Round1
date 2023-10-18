3890f4e9e2b2b9cf4f9a6745e56ab962484ead4d
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2023-10-18 13:56:20 +0900

    Pepero 함수 추가

--- CodeStarterCamp_Week1/Pepero.swift
+++ CodeStarterCamp_Week1/Pepero.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+
+// 빼빼로 손잡이를 그리는 함수
+func makeStick(height length: Int) {
+    for _ in 1...length {
+        for width in 1...4 {
+            if width % 2 == 0 {
+                print("|", terminator: "")
+                
+            } else {
+                print(" ", terminator: "")
+            }
+        }
+        print()
+    }
+}
+
+
+// 몸통을 그리는 함수
+func makeBody(_ body: String, _ topping: String) -> String {
+    if topping == "" {
+        return " " + body
+    } else {
+        return topping + body + topping
+    }
+}
+var bodyFunction: (String, String) -> String = makeBody
+
+
+// 길이에 따라 몸통을 그리는 함수
+func makePepero(height length: Int, _ function: (String, String) -> String, _ body: String, _ toping: String = "") -> Void {
+    for _ in 1...length {
+        print(function(body, toping))
+    }
+}
+
+func drawPepero(bodyHeight: Int, body: String, topping: String, stickHeight: Int) {
+    print("<정보>")
+    print("길이: \(bodyHeight)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이: \(stickHeight)")
+    print()
+    
+    makePepero(height: bodyHeight, bodyFunction, body, topping)
+    makeStick(height: stickHeight)
+}
