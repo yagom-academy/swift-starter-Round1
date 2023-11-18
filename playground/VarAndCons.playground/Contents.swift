cc0882e63fd5dafed6d374d3f70b83054233a1d5
yeahaluu <yeahaluu@gmail.com>
2023-11-18 18:13:09 +0900

    8 variable & Constant

---
+++ playground/VarAndCons.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+//MARK: - Variable
+
+var varNumber = 100
+varNumber = 200
+
+//MARK: - Constant
+
+let consNumber = 199
+//consNumber = 200  오류가 난다
+
+/* 스스로 해보기 */
+
+func JumpJump(number: Int) {
+    for count in 1...number {
+        print("\(name)이 \(count)번째 폴짝!")
+    }
+}
+
+var name = "길동"
+
+JumpJump(number: 3)
+
+name = "춘향"
+
+JumpJump(number: 3)
