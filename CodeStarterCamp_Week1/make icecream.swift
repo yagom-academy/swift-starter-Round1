fd3c5c13d6f83d4f3180a6ce5d80625c4a84bfaa
jmormalik <malikworkarm@gmail.com>
2023-11-20 15:08:57 +0900

    수정

---
+++ CodeStarterCamp_Week1/make icecream.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+<<<<<<< HEAD
+func makeIcecream(){
+    for _ in 0...7{
+        print("**********")
+    }
+}
+func drawStick(){
+    for _ in 0...3{
+        print("   | |")
+    }
+}
+makeIcecream()
+drawStick()
+=======
+func icecream(){
+    for _ in 0...7{
+        for _ in 0...10{
+            print(terminator:"*")
+        }
+        print(" ")
+    }
+}
+
+func stick(){
+    for _ in 0...3{
+        print("    "," ","   ", separator: "|")
+    }
+}
+icecream()
+stick()
+>>>>>>> step2
