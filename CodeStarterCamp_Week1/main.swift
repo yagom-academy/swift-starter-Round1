3dd81e474971e0228aa77a9bd8608886c12ae184
Nat <cestbonciel88@knou.ac.kr>
2023-11-23 04:08:38 +0900

    Remove: useless codes

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,38 +6,6 @@
 //
 
 import Foundation
-/*
- 길이: 10
- 몸통: ***
- 토핑:
- 막대길이: 4
- 
- */
-//let whiteSpace: String = " "
-//let peperoBody = String(repeating: "*", count: 3)
-////var peperoStick = String(repeating: "|", count: 2)
-//let peperoStick = "| |"
-//
-//var firstNumber = 0
-//func typeOfPepero(bodyLength: Int,topping: String, stickLength: Int) {
-//  while firstNumber < bodyLength {
-//    firstNumber += 1
-//    print("\(whiteSpace)\(topping)\(peperoBody)\(topping)", terminator: "\n")
-//  }
-//
-//  firstNumber = 0
-//
-//  for _ in 0..<stickLength {
-//    print(whiteSpace + peperoStick)
-//  }
-//
-//}
-//typeOfPepero(bodyLength: 10, topping: "", stickLength: 4)
-//typeOfPepero(bodyLength: 12, topping: "&", stickLength: 4)
-//typeOfPepero(bodyLength: 10, topping: "#", stickLength: 4)
-//typeOfPepero(bodyLength: 6, topping: "|0|", stickLength: 4)
-
-
 
 let whiteSpace: String = " "
 let peperoBody = String(repeating: "*", count: 3)
