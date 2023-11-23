6c0dfafd164722c2d5e6189299d902570af4a0c7
sooi <lucidstole@gmail.com>
2023-11-23 22:24:34 +0900

    변경사항 - 아이크스림이 아닌 빼빼로에 맞게 함수 및 변수 네이밍 변경 - for 반복문에서 while 반복문으로 변경 후 변수 loopCounter 추가 - 순서도 변경

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,41 +7,45 @@
 
 import Foundation
 
-//아이스크림의 크림 부분을 출력하는 함수
-func drawIceCream(creamHeight: Int, creamBody: String, creamTopping: String, stickHeight: Int ) {
-    for _ in 1...creamHeight {
-        drawBodyAndTopping(Body: creamBody, Topping: creamTopping)
-    }
-    for _ in 1...stickHeight {
-        drawIceCreamStick(stickHeight: stickHeight)
+//빼빼로에 대한 정보 (길이, 몸통, 토핑, 막대길이)를 입력받아서 빼빼로를 출력하는 함수
+func drawPepero(Height: Int, Body: String, Topping: String, stickHeight: Int ) {
+    drawBodyAndTopping(Height: Height, Body: Body, Topping: Topping)
+    drawPeperoStick(stickHeight: stickHeight)
+}
+
+//빼빼로의 길이, 몸통, 토핑을 매개변수로 입력받아 몸통과 토핑을 출력하는 함수
+func drawBodyAndTopping(Height: Int, Body: String, Topping: String) {
+    var loopCounter = 0;
+    while loopCounter < Height {
+        //토핑에 빈값을 입력했을때 몸통 옆쪽으로 한칸씩 띄워서 출력
+        if(Topping.isEmpty) {
+            print(" \(Body) ")
+        }
+        else {
+            print("\(Topping)\(Body)\(Topping)")
+        }
+        loopCounter = loopCounter + 1
     }
 }
 
-//아이스크림의 토핑과 몸통 매개변수로 입력받아 몸통과 토핑을 출력하는 함수
-func drawBodyAndTopping(Body: String, Topping: String) {
-    //토핑에 빈값을 입력했을때 몸통 옆쪽으로 한칸씩 띄워서 출력
-    if(Topping.isEmpty) {
-        print(" \(Body) ")
-    }
-    else {
-        print("\(Topping)\(Body)\(Topping)")
-    }
-}
-
-//아이스크림의 막대 부분을 출력하는 함수
-func drawIceCreamStick(stickHeight: Int) {
+//빼빼로의 막대 부분을 출력하는 함수
+func drawPeperoStick(stickHeight: Int) {
+    var loopCounter = 0;
+    while loopCounter < stickHeight {
         print(" | | ")
+        loopCounter = loopCounter + 1
+    }
 }
 
 print("<정보>")
 print("길이 : ", terminator: "")
-var iceCreamHeight = Int(readLine()!)! //아이스크림 크림 부분 단수 입력받기
+var peperoHeight = Int(readLine()!)! //빼빼로 부분 길이 입력받기
 print("몸통 : ", terminator: "")
-var iceCreamBody = readLine()! //아이스크림 몸통 입력받기
+var peperoBody = readLine()! //빼빼로 몸통 입력받기
 print("토핑 : ", terminator: "")
-var iceCreamTopping = readLine()! //아이스크림 토핑 입력받기
+var peperoTopping = readLine()! //빼빼로 토핑 입력받기
 print("막대길이 : ", terminator: "")
-var iceCreamStickHeight = Int(readLine()!)! //아이스크림 막대 부분 단수 입력받기
+var peperoStickHeight = Int(readLine()!)! //빼빼로 막대 부분 길이 입력받기
 
-drawIceCream(creamHeight: iceCreamHeight, creamBody: iceCreamBody, creamTopping: iceCreamTopping, stickHeight: iceCreamStickHeight)
+drawPepero(Height: peperoHeight, Body: peperoBody, Topping: peperoTopping, stickHeight: peperoStickHeight)
 
