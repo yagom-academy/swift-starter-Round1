c3efec6fde8ce717788a41d61a346b7f6c21f41d
manitw28 <manitw28@naver.com>
2023-11-24 22:17:48 +0900

    step2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,53 +8,19 @@
 
 import Foundation
 
-
-//var i : Int
-//var j : Int
-
-for i in 1...8 {
+for _ in 1...8 {
     print("")
-    for j in 1...11 {
+    for _ in 1...11 {
         print("*", terminator: "")
     }
 }
 
-for i in 1...4 {
+for _ in 1...4 {
     print("")
     for j in 1...10 {
         if (j==5 || j==7) {
             print("|", terminator: "")
-            continue
         }
         print(" ", terminator: "")
     }
 }
-
-print("")
-print("")
-
-
-
-func drawPepero(bodyLength:Int, main:String, topping:String, barLength:Int) {
-    drawBody(bodyLength:bodyLength, main: main, topping:topping);
-    drawBar(barLength:barLength);
-    print("");
-}
-
-func drawBody(bodyLength:Int, main:String, topping:String) {
-    for _ in 1...bodyLength {
-        print("\(topping)\(main)\(topping)")
-    }
-}
-
-func drawBar(barLength:Int) {
-    for _ in 1...barLength {
-        print(" | |")
-    }
-}
-
-drawPepero(bodyLength: 10, main: "***", topping: " ", barLength: 4)
-drawPepero(bodyLength: 12, main: "***", topping: "&", barLength: 4)
-drawPepero(bodyLength: 12, main: "***", topping: "#", barLength: 6)
-drawPepero(bodyLength: 6, main: "|0|", topping: " ", barLength: 4)
-
