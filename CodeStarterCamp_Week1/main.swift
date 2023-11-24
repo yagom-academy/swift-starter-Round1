b7477da0859d19b784938b395e498736f2119e30
manitw28 <manitw28@naver.com>
2023-11-24 22:24:48 +0900

    step3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -24,3 +24,32 @@ for _ in 1...4 {
         print(" ", terminator: "")
     }
 }
+
+print("")
+print("")
+
+
+
+func drawPepero(bodyLength:Int, main:String, topping:String, barLength:Int) {
+    drawBody(bodyLength:bodyLength, main: main, topping:topping);
+    drawBar(barLength:barLength);
+    print("");
+}
+
+func drawBody(bodyLength:Int, main:String, topping:String) {
+    for _ in 1...bodyLength {
+        print("\(topping)\(main)\(topping)")
+    }
+}
+
+func drawBar(barLength:Int) {
+    for _ in 1...barLength {
+        print(" | |")
+    }
+}
+
+drawPepero(bodyLength: 10, main: "***", topping: " ", barLength: 4)
+drawPepero(bodyLength: 12, main: "***", topping: "&", barLength: 4)
+drawPepero(bodyLength: 12, main: "***", topping: "#", barLength: 6)
+drawPepero(bodyLength: 6, main: "|0|", topping: " ", barLength: 4)
+
