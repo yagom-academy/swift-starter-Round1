51e6ffa7c40fc65f3cab79b7bb91566e15f56ee5
ss-12-csp <bg_style@naver.com>
2023-11-26 11:36:38 +0900

    Feat: Add a function to draw the ice cream.

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+let iceCreamWidith: Int = 11
+let iceCreamHeight: Int = 8
+let stickHeight: Int = 4
 
+let iceCreamSymbol: String = "*"
+let stickSymbol: String = "    | |"
+
+func drawIceCream(height: Int, width: Int) {
+    for _ in 1...height {
+        for _ in 1...width {
+            print(iceCreamSymbol, terminator: "")
+        }
+        print()
+    }
+}
+
+func drawStick(height: Int) {
+    for _ in 1...stickHeight {
+        print(stickSymbol)
+    }
+}
+
+drawIceCream(height: iceCreamHeight, width: iceCreamWidith)
+drawStick(height: stickHeight)
