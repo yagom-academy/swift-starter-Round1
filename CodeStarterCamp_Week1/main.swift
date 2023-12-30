35942d47365ad223179d53cf8d69922eea02b4c4
Gwei <eogns815@naver.com>
2023-12-30 17:10:11 +0900

    test

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,3 +10,41 @@ import Foundation
 
 print("Hello, World!")
 
+func plusTWo (X:Int) -> Int{
+    return X+2
+}
+
+func printHelloWorld()
+{
+    print("Hello, World!")
+}
+
+
+for count in 1...6{
+    앞으로이동()
+}
+
+
+func 앞으로이동(){
+    //code
+}
+
+
+for count in 1...3{
+    print("헤")
+}
+
+for count in 0...4{
+    print(count,"번째 무야호!!")
+}
+
+
+func printMuYaHo(count : Int){
+    print(count,"번째 무야호")
+}
+
+printMuYaHo(count: 2)
+
+for count in 0...4{
+    printMuYaHo(count: count)
+}
