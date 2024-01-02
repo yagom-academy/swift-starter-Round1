4ffbed6aa7fb122eceb8d1a622e199efaa6dd1cd
cty7574 <cty7574@gmail.com>
2024-01-02 21:51:06 +0900

    코드수정

---
+++ CodeStarterCamp_Week1/Step3.swift
@@ -0,0 +1,36 @@
+//
+//  Step2.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 맨태 on 12/29/23.
+//
+
+import Foundation
+
+func drawStick(length: Int) {
+    
+    for _ in 1...length {
+        print(" | |")
+    }
+    
+    print()
+}
+
+func drawBody(length: Int, body: String, topping: String) {
+    
+    for _ in 1...length {
+        print(topping + body + topping)
+    }
+}
+
+func printInfo(bodyLength: Int, body: String, topping: String, stickLength: Int) {
+    
+    print("<정보>")
+    print("길이: \(bodyLength)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이: \(stickLength)", terminator: "\n\n")
+    
+    drawBody(length: bodyLength, body: body, topping: topping)
+    drawStick(length: stickLength)
+}
---
+++ CodeStarterCamp_Week1/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+func drawBody() {
+    for _ in 1...8 {
+        print("**********")
+    }
+}
+
+func drawStick() {
+    for _ in 1...4 {
+        print("    | |   ")
+    }
+}
+
+//drawBody()
+//drawStick()
+
+// #1
+printInfo(bodyLength: 10, body: "***", topping: " ", stickLength: 4)
+
+// #2
+printInfo(bodyLength: 12, body: "***", topping: "&", stickLength: 4)
+
+// #3
+printInfo(bodyLength: 12, body: "***", topping: "#", stickLength: 6)
+
+// #4
+printInfo(bodyLength: 6, body: "|0|", topping: " ", stickLength: 4)
+
+// custom
+printInfo(bodyLength: 8, body: "$$$", topping: "~", stickLength: 4)
