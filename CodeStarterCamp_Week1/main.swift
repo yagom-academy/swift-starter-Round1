9b2a29ee9ca23b6bb992b781889bbf15a56e7d43
Gwei <eogns815@naver.com>
2024-01-02 22:12:17 +0900

    step3 뺴빼로 그리기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,54 +8,22 @@
 
 import Foundation
 
-//func printStar() {
-//    for _ in 1...8 {
-//        print("***********")
-//    }
-//}
-//
-//
-//func printBar() {
-//    for _ in 1...4 {
-//        print("    | |")
-//    }
-//}
-//
-//printStar()
-//printBar()
-//
-
-
-func drawStick(length: Int, stickLength: Int) {
-    for _ in 1...stickLength {
-        print(" | |")
+func printStar() {
+    for _ in 1...8 {
+        print("***********")
     }
 }
 
-func drawChocolateBar(body: String, topping: String, length: Int) {
-    for _ in 1...length {
-        print(" \(topping)\(body)\(topping)")
+
+func printBar() {
+    for _ in 1...4 {
+        print("    | |")
     }
 }
 
-func drawPocky(length: Int, stickLength: Int, body: String, topping: String) {
-    print("<정보>")
-    print("길이: \(length)")
-    print("몸통: \(body)")
-    print("토핑: \(topping)")
-    print("막대길이: \(stickLength)")
+printStar()
+printBar()
 
-    drawChocolateBar(body: body, topping: topping, length: length)
-    drawStick(length: length, stickLength: stickLength)
-}
 
-drawPocky(length: 10, stickLength: 4, body: "***", topping: "")
-print("\n----------------------\n")
 
-drawPocky(length: 12, stickLength: 4, body: "***", topping: "&")
-print("\n----------------------\n")
 
-drawPocky(length: 12, stickLength: 6, body: "***", topping: "#")
-print("\n----------------------\n")
-
-drawPocky(length: 6, stickLength: 4, body: "|0|", topping: "")
