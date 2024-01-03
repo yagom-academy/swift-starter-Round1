78b090a2295bae80f82f4e8b278bc7678680ab5c
serena <101619749+serena0720@users.noreply.github.com>
2024-01-03 19:13:27 +0900

    Revert " Round1[Step1],[Step2],[Step3] - gwei"

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,22 +8,5 @@
 
 import Foundation
 
-func printStar() {
-    for _ in 1...8 {
-        print("***********")
-    }
-}
-
-
-func printBar() {
-    for _ in 1...4 {
-        print("    | |")
-    }
-}
-
-printStar()
-printBar()
-
-
-
+print("Hello, World!")
 
