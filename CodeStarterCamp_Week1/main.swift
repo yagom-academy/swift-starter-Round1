9f7606f77cc8ff40b36c7010a5e87bac00104b84
jinsuwee <amglt2@naver.com>
2024-01-08 16:11:04 +0900

    Round1[Step2] 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,18 +1,22 @@
-func iceCreamHead(starCount: Int, repeatCount: Int) {
-    for _ in 1...repeatCount {
-        let stars = String(repeating: "*", count: starCount)
-        print(stars)
+
+//iceCream
+
+
+
+func iceCreamMake(iceCreamHead: String, iceCreamHeadRepeat: Int, repeatCount: Int){
+    for _ in 1...repeatCount{
+        let iceCream = String(repeating: iceCreamHead, count: iceCreamHeadRepeat)
+        print(iceCream)
     }
 }
 
-func iceCreamBody(number: Int) {
-    for _ in 1...number {
-        print("    | |    ")
+func iceCreamBody(iceCreamBodyStick: String, stickRepeatCount: Int){
+    for _ in 1...stickRepeatCount {
+        print(iceCreamBodyStick)
     }
 }
-iceCreamHead(starCount: 11, repeatCount: 8)
-iceCreamBody(number: 4)
 
 
-
+iceCreamMake(iceCreamHead: "*", iceCreamHeadRepeat: 11, repeatCount: 8)
+iceCreamBody(iceCreamBodyStick:"    | |    ", stickRepeatCount: 4)
 
