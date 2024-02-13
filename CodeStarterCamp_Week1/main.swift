ec661c4c7b1f1e0ef0c34e572e5abee4e3efb02e
kof99athena <kof99athena@gmail.com>
2024-02-13 23:33:17 +0900

    add makeIceCream function

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,24 +1,20 @@
-
 import Foundation
 
 func makeIceCream(bodyWidth : Int){
-    for bodyHeight in 1...8 {
+    for _ in 1...8 {
         print("*",terminator: "")
     }
     print("")
 } //아이스크림을 만드는 함수
 
-for bodyWidth in 1...10 {
-    makeIceCream(bodyWidth: bodyWidth)
-}
-
 func makeBar() {
-    for barHeight in 1...4 {
+    for _ in 1...4 {
         print("  | |  ")
     }
 }//막대기를 만드는 함수
-
-
 makeBar()
 
+for bodyWidth in 1...10 {
+    makeIceCream(bodyWidth: bodyWidth)
+}
 
