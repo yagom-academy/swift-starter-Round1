0fe25dfb1e27bdd5b4a436fe6224cf130cd75040
kof99athena <kof99athena@gmail.com>
2024-02-17 19:43:04 +0900

    Update func

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,24 +1,19 @@
-import Foundation
-
-var iceWidth : Int = 11
-var iceHeight : Int = 8
-var barWidth : Double = Double(iceWidth) / 2.0
-var barHeight : Double = Double(iceHeight) / 2.0
+let iceWidth: Int = 11
+let iceHeight: Int = 8
+let barWidth: Int = iceWidth / 2
+let barHeight: Int = iceHeight / 2
 
 makeIceBody(iceWidth: iceWidth, iceHeight: iceHeight)
-makeBar(barWidth: Int(barWidth), barHeight: Int(barHeight))
+makeBar(barWidth: barWidth, barHeight: barHeight)
 
-func makeIceBody(iceWidth : Int, iceHeight : Int) {
+func makeIceBody(iceWidth: Int, iceHeight: Int) {
     for _ in 1...iceHeight{
-        for _ in 1...iceWidth {
-            print("*",terminator: "")
-        }
-        print("")
+        print(String(repeating: "*", count: iceWidth))
     }
 }
 
-func makeBar (barWidth : Int, barHeight : Int) {
+func makeBar (barWidth: Int, barHeight: Int) {
     for _ in 1...barHeight{
-        print(String(repeating: " ", count: barWidth-1)+"| |")
+        print(String(repeating: " ", count: barWidth - 1) + "| |")
     }
 }
