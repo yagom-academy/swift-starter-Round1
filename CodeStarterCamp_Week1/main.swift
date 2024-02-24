860d5d4a8e1cbdf366dc66d589db5da3c57ed9be
o2k <o2k@o2kui-MacBookAir.local>
2024-02-24 16:44:59 +0900

    Round1 [Step3] - o2k

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,20 +1,53 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+var topping: String = ""
+var body: String = ""
+var bodyLength: Int = 0
+var barLength: Int = 0
+var stickGap: Int = 0
 
-import Foundation
+takeOrder()
 
-for _ in 1...8 {
-    for _ in 1...11 {
-        print("*", terminator: "")
-    }
+func takeOrder() {
+    print("빼빼로의 길이를 입력해주세요")
+    bodyLength = Int(readLine()!)!
+    
+    print("빼빼로의 몸통(?)을 입력해주세요")
+    body = readLine()!
+    
+    print("뺴뺴로의 토핑을 입력해주세요")
+    topping = readLine()!
+    
+    print("막대 길이를 입력해주세요")
+    barLength = Int(readLine()!)!
+    
+    stickGap = (topping.count + body.count) / 2
+    
+    print("""
+    <정보>
+    길이: \(bodyLength)
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(barLength)
+    """)
     print("")
+    
+    makeBody(bodyLength: bodyLength)
+    makeBar(stickGap: stickGap, barLength: barLength)
 }
 
-for _ in 1...4 {
-    print("    | |    ")
+
+
+func items(topping : String, body: String){
+    print("\(topping)\(body)\(topping)")
+}
+
+func makeBody(bodyLength: Int) {
+    for _ in 1...bodyLength {
+        items(topping: topping, body: body)
+    }
+}
+
+func makeBar(stickGap: Int, barLength: Int) {
+    for _ in 1...barLength {
+        print(String(repeating: " ", count: stickGap - 1) + "| |")
+    }
 }
