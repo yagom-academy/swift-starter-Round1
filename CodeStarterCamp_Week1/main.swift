f7d402cf62d4cca31da7cd24d66d7c503e60a13b
nangnang2 <nayon7821@naver.com>
2024-02-26 23:20:43 +0900

    step3

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,33 +8,7 @@
 
 //import Foundation
 
-
-// let icecreamHead = "***********"
-// let icecreamStick = "    | |    "
-// for _ in 1...8 {
-//    print(icecreamHead)
-// }
-// for _ in 1...4 {
-//    print(icecreamStick)
-// }
-
-
-/*
-func draw(iceCreamHead: String) {
-    for _ in 1...8 {
-        print(iceCreamHead)
-    }
- }
-
-func draw(iceCreamStick : String) {
-    for _ in 1...4 {
-        print(iceCreamStick)
-    }
-}
-draw(iceCreamHead: "***********")
-draw(iceCreamStick: "    | |")
-*/
-
+/* step2 수정했습니다.
 
 func drawIceCreamHead() {
     for _ in 1...8 {
@@ -55,4 +29,118 @@ func drawIceCreamStick() {
 
 drawIceCreamHead()
 drawIceCreamStick()
+
+*/
+
+// step3
+
+print(
+"""
+<실행 예시 1>
+
+길이: 10
+몸통: ***
+토핑:
+막대길이: 4
+
+""")
+
+func drawBasicPepero(body: String, stick: String, bodyLength: Int, stickLength: Int) {
+    for _ in 1...bodyLength {
+        print(body)
+    }
+    for _ in 1...stickLength {
+        print(stick)
+    }
+}
+drawBasicPepero(body: " ***", stick: " | |", bodyLength: 10, stickLength: 4)
+print(
+"""
+
+
+<실행 예시 2>
+
+ 길이: 12
+ 몸통: ***
+ 토핑: &
+ 막대길이: 4
+
+""")
+
+
+func drawSecondPepero(body: String, topping: String, stick: String, bodyLength: Int, stickLength: Int) {
+    for _ in 1...bodyLength {
+        print(topping, body, topping, separator: "")
+    }
+    for _ in 1...stickLength {
+        print(stick)
+    }
+}
+drawSecondPepero(body: "***", topping: "&", stick: " | |", bodyLength: 12, stickLength: 4)
+print(
+"""
+
+
+<실행 예시 3>
+
+길이: 12
+몸통: ***
+토핑: #
+막대길이: 6
+
+""")
+
+func drawThirdPepero(body: String, topping: String, stick: String, bodyLength: Int, stickLength: Int) {
+    for _ in 1...bodyLength {
+        print(topping, body, topping, separator: "")
+    }
+    for _ in 1...stickLength {
+        print(stick)
+    }
+}
+drawThirdPepero(body: "***", topping: "#", stick: " | |", bodyLength: 12, stickLength: 6)
+print(
+"""
+
+
+<실행 예시 4>
+
+길이: 6
+몸통: |0|
+토핑:
+막대길이: 4
+
+""")
+
+func drawNudePepero(body: String, stick: String, bodyLength: Int, stickLength: Int) {
+    for _ in 1...bodyLength {
+        print(body)
+    }
+    for _ in 1...stickLength {
+        print(stick)
+    }
+}
+drawNudePepero(body: " |0|", stick: " | |", bodyLength: 6, stickLength: 4)
+print(
+"""
+
+
+<나만의 빼빼로 만들기>
+
+길이: 12
+몸통: ooo
+토핑: @
+막대길이: 6
+
+""")
+
+func drawMyPepero(body: String, topping: String, stick: String, bodyLength: Int, stickLength: Int) {
+    for _ in 1...bodyLength {
+        print(topping, body, topping, separator: "")
+    }
+    for _ in 1...stickLength {
+        print(stick)
+    }
+}
+drawMyPepero(body: "ooo", topping: "@", stick: " |||", bodyLength: 12, stickLength: 6)
 
