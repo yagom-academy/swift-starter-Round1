97a944bd9c4875addfb25139486062b4cbb7cb8f
o2k <o2k@o2kui-MacBookAir.local>
2024-02-28 03:04:40 +0900

    Roundt1 [Step3] - o2k

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,6 +1,6 @@
 var topping: String = ""
 var body: String = ""
-var bodyLength: Int = 0
+var peperoLength: Int = 0
 var barLength: Int = 0
 var stickGap: Int = 0
 
@@ -8,7 +8,7 @@ takeOrder()
 
 func takeOrder() {
     print("빼빼로의 길이를 입력해주세요")
-    bodyLength = Int(readLine()!)!
+    peperoLength = Int(readLine()!)!
     
     print("빼빼로의 몸통(?)을 입력해주세요")
     body = readLine()!
@@ -23,30 +23,30 @@ func takeOrder() {
     
     print("""
     <정보>
-    길이: \(bodyLength)
+    길이: \(peperoLength)
     몸통: \(body)
     토핑: \(topping)
     막대길이: \(barLength)
     """)
     print("")
     
-    makeBody(bodyLength: bodyLength)
-    makeBar(stickGap: stickGap, barLength: barLength)
+    makePeperoBody(peperoLength: peperoLength)
+    makePeperoStick(stickGap: stickGap, barLength: barLength)
 }
 
 
 
-func items(topping : String, body: String){
+func selectPeperoItems(topping : String, body: String){
     print("\(topping)\(body)\(topping)")
 }
 
-func makeBody(bodyLength: Int) {
-    for _ in 1...bodyLength {
-        items(topping: topping, body: body)
+func makePeperoBody(peperoLength: Int) {
+    for _ in 1...peperoLength {
+        selectPeperoItems(topping: topping, body: body)
     }
 }
 
-func makeBar(stickGap: Int, barLength: Int) {
+func makePeperoStick(stickGap: Int, barLength: Int) {
     for _ in 1...barLength {
         print(String(repeating: " ", count: stickGap - 1) + "| |")
     }
