692269bcf2c65a3cf68f4b8fa4e3dfe6a30d0fdf
최승준 <a1@choeseunghuiui-MacBookPro.local>
2024-03-24 21:52:40 +0900

    [Step3] 빼빼로만들기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,3 +9,8 @@
 import Foundation
 
 makeIceCream(creamSize: 11, stickSize: 4)
+
+makePepero(peperoSize: 10, pepero: "***", stickLength: 4)
+makePepero(peperoSize: 12, pepero: "***", toping: "&", stickLength: 4)
+makePepero(peperoSize: 12, pepero: "***", toping: "#", stickLength: 6)
+makePepero(peperoSize: 6, pepero: "|0|", stickLength: 4)
