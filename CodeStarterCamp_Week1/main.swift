3c8e0e28f5e898229cedb79210d58f6e552511c9
dsungc1111 <eotjd9808@naver.com>
2024-03-27 03:25:23 +0900

    step2 update

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,26 +8,24 @@
 
 import Foundation
 
-//아이스크림 부분
-func iceCream(){
-    for _ in 1...8{
-        for _ in 1...11{
-            print("*", terminator: "")
+func iceCream(length: Int, width: Int){
+    for _ in 1...length{
+        print(String(repeating: "*", count: width))
+    }
+    if width % 2 == 0 {
+        for _ in 1...length/2{
+            for _ in 1..<width/2-1{
+                print(" ", terminator: "")
+            }
+            print("|  |")
         }
-        print()
+    } else {
+        for _ in 1...length/2{
+            for _ in 1...width/2-1{
+                print(" ", terminator: "")
+            }
+            print("| |")
+        }
     }
 }
-//아이스크림 바 부분
-func iceCreamBar(){
-    for _ in 1...4{
-        for _ in 1...4{
-            print(" ", terminator: "")
-        }
-        print("|", terminator: " ")
-        print("|")
-        
-    }
-}
-
-iceCream()
-iceCreamBar()
+iceCream(length: 6, width: 10)
