c9e16603b839fe05ffe2a21484af66f04d73c10a
dsungc1111 <eotjd9808@naver.com>
2024-03-28 01:23:23 +0900

    step2 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,24 +8,41 @@
 
 import Foundation
 
-func iceCream(length: Int, width: Int){
-    for _ in 1...length{
-        print(String(repeating: "*", count: width))
+var shape: String = String()
+var blank: String = String()
+var boxOfLength: Int = 0
+var boxOfWidth: Int = 0
+
+func iceCream(length: Int, width: Int) {
+    
+    for _ in 1...width {
+        shape.append("*")
     }
+    
+    for _ in 1...length {
+        print("\(shape)")
+    }
+    boxOfLength = length
+    boxOfWidth = width
+}
+
+
+func iceCreamBar(length: Int, width: Int) {
+    
+    for _ in 1...width/2 - 2 {
+        blank.append(" ")
+    }
+    
     if width % 2 == 0 {
-        for _ in 1...length/2{
-            for _ in 1..<width/2-1{
-                print(" ", terminator: "")
-            }
-            print("|  |")
+        for _ in 1...length/2 {
+            print("\(blank)|  |")
         }
     } else {
-        for _ in 1...length/2{
-            for _ in 1...width/2-1{
-                print(" ", terminator: "")
-            }
-            print("| |")
+        for _ in 1...length/2 {
+            print("\(blank) | |")
         }
     }
+    
 }
-iceCream(length: 6, width: 10)
+iceCream(length: 6, width: 7)
+iceCreamBar(length: boxOfLength, width: boxOfWidth)
