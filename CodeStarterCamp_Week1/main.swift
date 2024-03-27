4570037e776643ab4724f4a1bce380419f8b54d2
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2024-03-27 21:25:51 +0900

    Rount1[Step2] : 아이스크림 만들기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,19 +1,19 @@
 // Step 2: 아이스크림 그리기
 
-func iceCream() {
-    for icecreamCulumn in 1...8 {
-        for icecreamRow in 1...11 {
+func iceCream(icecreamCulumn culumn : Int, icecreamRow row : Int) {
+    for _ in 1...culumn {
+        for _ in 1...row {
             print("*", terminator: "")
         }
         print("")
     }
 }
 
-func iceCreamStick() {
-    for stickCulumn in 1...4 {
+func iceCreamStick(stickLength : Int) {
+    for _ in 1...stickLength {
         print("    | |    ")
     }
 }
 
-iceCream()
-iceCreamStick()
+iceCream(icecreamCulumn : 8, icecreamRow : 11)
+iceCreamStick(stickLength : 4)
